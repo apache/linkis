fd7b9ddc0f8d8ea332130a44d95897a40431933a
leisore <leisore@gmail.com>
2019-08-02 18:13:41 +0800

    Ref publicService： 1. 默认用户工作根目录在本地和HDFS上保持一致，均为/tmp/linkis/username 2、代码中的默认值和配置文件conf/linkis.properties中hdfs前后缀不一致      代码中默认前缀为hdfs:///tmp/，后缀为/linkis/      配置中默认前缀为hdfs:///tmp/linkis/，后缀为/     配置中的合理，和local是一致的，修改代码和配置保持一致

--- publicService/workspace/src/main/java/com/webank/wedatasphere/linkis/filesystem/conf/WorkSpaceConfiguration.java
+++ publicService/workspace/src/main/java/com/webank/wedatasphere/linkis/filesystem/conf/WorkSpaceConfiguration.java
@@ -24,6 +24,6 @@ import com.webank.wedatasphere.linkis.common.conf.CommonVars$;
  */
 public class WorkSpaceConfiguration {
     public static final CommonVars LOCAL_USER_ROOT_PATH = CommonVars$.MODULE$.apply("wds.linkis.workspace.filesystem.localuserrootpath","file:///tmp/linkis/");
-    public static final CommonVars HDFS_USER_ROOT_PATH_PREFIX = CommonVars$.MODULE$.apply("wds.linkis.workspace.filesystem.hdfsuserrootpath.prefix","hdfs:///tmp/");
-    public static final CommonVars HDFS_USER_ROOT_PATH_SUFFIX = CommonVars$.MODULE$.apply("wds.linkis.workspace.filesystem.hdfsuserrootpath.suffix","/linkis/");
+    public static final CommonVars HDFS_USER_ROOT_PATH_PREFIX = CommonVars$.MODULE$.apply("wds.linkis.workspace.filesystem.hdfsuserrootpath.prefix","hdfs:///tmp/linkis/");
+    public static final CommonVars HDFS_USER_ROOT_PATH_SUFFIX = CommonVars$.MODULE$.apply("wds.linkis.workspace.filesystem.hdfsuserrootpath.suffix","/");
 }

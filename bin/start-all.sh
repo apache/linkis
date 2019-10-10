b12f9f23ee44ebea2cbba14727faccb28abbc3cd
peacewong <wpeace1212@gmail.com>
2019-10-10 17:09:58 +0800

    Fix this issue error log does not print Closes #73

--- bin/start-all.sh
+++ bin/start-all.sh
@@ -63,7 +63,7 @@ echo "<-------------------------------->"
 echo "Begin to start Eureka Server"
 EUREKA_NAME="eureka"
 EUREKA_BIN=${LINKIS_INSTALL_HOME}/${EUREKA_NAME}/bin
-EUREKA_START_CMD="cd ${EUREKA_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-${EUREKA_NAME}.sh > /dev/null"
+EUREKA_START_CMD="cd ${EUREKA_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-${EUREKA_NAME}.sh"
 if [ -n "${EUREKA_INSTALL_IP}" ];then
     ssh ${EUREKA_INSTALL_IP} "${EUREKA_START_CMD}"
 
@@ -79,7 +79,7 @@ echo "<-------------------------------->"
 echo "Begin to start Gateway"
 GATEWAY_NAME="gateway"
 GATEWAY_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${GATEWAY_NAME}/bin
-GATEWAY_START_CMD="cd ${GATEWAY_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-${GATEWAY_NAME}.sh > /dev/null"
+GATEWAY_START_CMD="cd ${GATEWAY_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-${GATEWAY_NAME}.sh"
 if [ -n "${GATEWAY_INSTALL_IP}"  ];then
     ssh ${GATEWAY_INSTALL_IP} "${GATEWAY_START_CMD}"
 else
@@ -94,7 +94,7 @@ echo "<-------------------------------->"
 echo "Begin to start Public Service"
 PUB_SERVICE_NAME="publicservice"
 PUB_SERVICE_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${PUB_SERVICE_NAME}/bin
-PUB_SERVICE_START_CMD="cd ${PUB_SERVICE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-${PUB_SERVICE_NAME}.sh > /dev/null"
+PUB_SERVICE_START_CMD="cd ${PUB_SERVICE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-${PUB_SERVICE_NAME}.sh"
 if [ -n "${PUBLICSERVICE_INSTALL_IP}" ];then
     ssh ${PUBLICSERVICE_INSTALL_IP} "${PUB_SERVICE_START_CMD}"
 else
@@ -109,7 +109,7 @@ echo "<-------------------------------->"
 echo "Begin to start metadata"
 METADATA_NAME="metadata"
 METADATA_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${METADATA_NAME}/bin
-METADATA_START_CMD="if [ -d ${METADATA_BIN} ];then cd ${METADATA_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-metadata.sh > /dev/null;else echo 'WARNING:Metadata will not start';fi"
+METADATA_START_CMD="if [ -d ${METADATA_BIN} ];then cd ${METADATA_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-metadata.sh;else echo 'WARNING:Metadata will not start';fi"
 if [ -n "${METADATA_INSTALL_IP}" ];then
     ssh ${METADATA_INSTALL_IP} "${METADATA_START_CMD}"
 else
@@ -124,7 +124,7 @@ echo "<-------------------------------->"
 echo "Begin to start Resource Manager"
 RM_NAME="resourcemanager"
 RM_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${RM_NAME}/bin
-RM_START_CMD="cd ${RM_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-${RM_NAME}.sh > /dev/null"
+RM_START_CMD="cd ${RM_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-${RM_NAME}.sh"
 if [ -n "${RESOURCEMANAGER_INSTALL_IP}" ];then
     ssh ${RESOURCEMANAGER_INSTALL_IP} "${RM_START_CMD}"
 else
@@ -141,7 +141,7 @@ echo "<-------------------------------->"
 echo "Begin to start Spark Entrance"
 SPARK_ENTRANCE_NAME="ujes-spark-entrance"
 SPARK_ENTRANCE_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${SPARK_ENTRANCE_NAME}/bin
-SPARK_ENTRANCE_START_CMD="if [ -d ${SPARK_ENTRANCE_BIN} ];then cd ${SPARK_ENTRANCE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-sparkentrance.sh > /dev/null;else echo 'WARNING:Spark Entrance will not start';fi"
+SPARK_ENTRANCE_START_CMD="if [ -d ${SPARK_ENTRANCE_BIN} ];then cd ${SPARK_ENTRANCE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-sparkentrance.sh;else echo 'WARNING:Spark Entrance will not start';fi"
 if [ -n "${SPARK_INSTALL_IP}" ];then
     ssh ${SPARK_INSTALL_IP} "${SPARK_ENTRANCE_START_CMD}"
 else
@@ -155,7 +155,7 @@ echo "<-------------------------------->"
 echo "Begin to Spark Engine Manager"
 SPARK_EM_NAME="ujes-spark-enginemanager"
 SPARK_EM_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${SPARK_EM_NAME}/bin
-SPARK_EM_START_CMD="if [ -d ${SPARK_EM_BIN} ];then cd ${SPARK_EM_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-sparkenginemanager.sh > /dev/null;else echo 'WARNING:Spark EM will not start';fi"
+SPARK_EM_START_CMD="if [ -d ${SPARK_EM_BIN} ];then cd ${SPARK_EM_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-sparkenginemanager.sh;else echo 'WARNING:Spark EM will not start';fi"
 if [ -n "${SPARK_INSTALL_IP}" ];then
     ssh ${SPARK_INSTALL_IP} "${SPARK_EM_START_CMD}"
 else
@@ -169,7 +169,7 @@ echo "<-------------------------------->"
 echo "Begin to start Hive Entrance"
 HIVE_ENTRANCE_NAME="ujes-hive-entrance"
 HIVE_ENTRANCE_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${HIVE_ENTRANCE_NAME}/bin
-HIVE_ENTRANCE_START_CMD="if [ -d ${HIVE_ENTRANCE_BIN} ];then cd ${HIVE_ENTRANCE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-hiveentrance.sh > /dev/null;else echo 'WARNING:Hive Entrance will not start';fi"
+HIVE_ENTRANCE_START_CMD="if [ -d ${HIVE_ENTRANCE_BIN} ];then cd ${HIVE_ENTRANCE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-hiveentrance.sh;else echo 'WARNING:Hive Entrance will not start';fi"
 if [ -n "${HIVE_INSTALL_IP}" ];then
     ssh ${HIVE_INSTALL_IP} "${HIVE_ENTRANCE_START_CMD}"
 else
@@ -202,7 +202,7 @@ echo "<-------------------------------->"
 echo "Begin to start Python Entrance"
 PYTHON_ENTRANCE_NAME="ujes-python-entrance"
 PYTHON_ENTRANCE_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${PYTHON_ENTRANCE_NAME}/bin
-PYTHON_ENTRANCE_START_CMD="if [ -d ${PYTHON_ENTRANCE_BIN} ];then cd ${PYTHON_ENTRANCE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-pythonentrance.sh > /dev/null;else echo 'WARNING:Python Entrance will not start';fi"
+PYTHON_ENTRANCE_START_CMD="if [ -d ${PYTHON_ENTRANCE_BIN} ];then cd ${PYTHON_ENTRANCE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-pythonentrance.sh;else echo 'WARNING:Python Entrance will not start';fi"
 if [ -n "${PYTHON_INSTALL_IP}" ];then
     ssh ${PYTHON_INSTALL_IP} "${PYTHON_ENTRANCE_START_CMD}"
 else
@@ -218,7 +218,7 @@ echo "<-------------------------------->"
 echo "Begin to start Python Engine Manager"
 PYTHON_EM_NAME="ujes-python-enginemanager"
 PYTHON_EM_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${PYTHON_EM_NAME}/bin
-PYTHON_EM_START_CMD="if [ -d ${PYTHON_EM_BIN} ];then cd ${PYTHON_EM_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-pythonenginemanager.sh > /dev/null;else echo 'WARNING:Python EM will not start';fi"
+PYTHON_EM_START_CMD="if [ -d ${PYTHON_EM_BIN} ];then cd ${PYTHON_EM_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-pythonenginemanager.sh;else echo 'WARNING:Python EM will not start';fi"
 if [ -n "${PYTHON_INSTALL_IP}" ];then
     ssh ${PYTHON_INSTALL_IP} "${PYTHON_EM_START_CMD}"
 else
@@ -236,7 +236,7 @@ echo "<-------------------------------->"
 echo "Begin to start JDBC Entrance"
 JDBC_ENTRANCE_NAME="ujes-jdbc-entrance"
 JDBC_ENTRANCE_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${JDBC_ENTRANCE_NAME}/bin
-JDBC_ENTRANCE_START_CMD="if [ -d ${JDBC_ENTRANCE_BIN} ];then cd ${JDBC_ENTRANCE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-jdbcentrance.sh > /dev/null;else echo 'WARNING:JDBC Entrance will not start';fi"
+JDBC_ENTRANCE_START_CMD="if [ -d ${JDBC_ENTRANCE_BIN} ];then cd ${JDBC_ENTRANCE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-jdbcentrance.sh;else echo 'WARNING:JDBC Entrance will not start';fi"
 if [ -n "${JDBC_INSTALL_IP}" ];then
     ssh ${JDBC_INSTALL_IP} "${JDBC_ENTRANCE_START_CMD}"
 else
@@ -253,7 +253,7 @@ sleep 3
 #echo "Pipeline Entrance is Starting"
 #PIPELINE_ENTRANCE_NAME="ujes-pipeline-entrance"
 #PIPELINE_ENTRANCE_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${PIPELINE_ENTRANCE_NAME}/bin
-#PIPELINE_ENTRANCE_START_CMD="cd ${PIPELINE_ENTRANCE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-pipelineentrance.sh > /dev/null"
+#PIPELINE_ENTRANCE_START_CMD="cd ${PIPELINE_ENTRANCE_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-pipelineentrance.sh"
 #if [ -n "${PIPELINE_INSTALL_IP}" ];then
 #    ssh ${PIPELINE_INSTALL_IP} "${PIPELINE_ENTRANCE_START_CMD}"
 #else
@@ -265,7 +265,7 @@ sleep 3
 #echo "Pipeline Engine Manager is Starting"
 #PIPELINE_EM_NAME="ujes-pipeline-enginemanager"
 #PIPELINE_EM_BIN=${LINKIS_INSTALL_HOME}/${APP_PREFIX}${PIPELINE_EM_NAME}/bin
-#PIPELINE_EM_START_CMD="cd ${PIPELINE_EM_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-pipelineenginemanager.sh > /dev/null"
+#PIPELINE_EM_START_CMD="cd ${PIPELINE_EM_BIN}; dos2unix ./* > /dev/null 2>&1; dos2unix ../conf/* > /dev/null 2>&1; sh start-pipelineenginemanager.sh"
 #if [ -n "${PIPELINE_INSTALL_IP}" ];then
 #    ssh ${PIPELINE_INSTALL_IP} "${PIPELINE_EM_START_CMD}"
 #else

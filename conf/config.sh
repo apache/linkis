f052617abc3663abd2ca13058563e6a2204c286c
yangzhiyue <904666286@qq.com>
2019-11-26 10:54:32 +0800

    add bml module to linkis assembly

--- conf/config.sh
+++ conf/config.sh
@@ -88,6 +88,12 @@ JDBC_ENTRANCE_PORT=9888
 MLSQL_INSTALL_IP=127.0.0.1
 MLSQL_ENTRANCE_PORT=9889
 
+
+### BML
+### This service is used to provide BML capability.
+BML_INSTALL_IP=127.0.0.1
+BML_PORT=9999
+
 ########################################################################################
 
 ## LDAP is for enterprise authorization, if you just want to have a try, ignore it.

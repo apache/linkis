a7903515cbe182ba6f1c038edd7f2b210045151a
yangzhiyue <904666286@qq.com>
2019-11-29 20:55:00 +0800

    add bml engineHook adapted to engine

---
+++ ujes/engine/src/main/scala/com/webank/wedatasphere/linkis/engine/ResourceExecuteRequest.scala
@@ -0,0 +1,9 @@
+package com.webank.wedatasphere.linkis.engine
+
+/**
+  * created by cooperyang on 2019/11/29
+  * Description:
+  */
+trait ResourceExecuteRequest {
+  val resources:java.util.List[Object]
+}

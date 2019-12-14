351e073fe76fd9fda388b0d1492b5d8e32822ef2
peacewong <wpeace1212@gmail.com>
2019-12-14 23:49:34 +0800

    unify the start script of microservices Closes #199

--- eurekaServer/bin/start-eureka.sh
+++ eurekaServer/bin/start-eureka.sh
@@ -10,12 +10,12 @@ if [[ -f "${EUREKA_SERVER_PID}" ]]; then
     pid=$(cat ${EUREKA_SERVER_PID})
     if kill -0 ${pid} >/dev/null 2>&1; then
       echo "EUREKA_SERVER Remote Server is already running."
-      return 0;
+      exit 1
     fi
 fi
 
 export EUREKA_SERVER_LOG_PATH=$HOME/logs
-export EUREKA_SERVER_HEAP_SIZE="1G"
+export EUREKA_SERVER_HEAP_SIZE="512M"
 export EUREKA_SERVER_CLASS=${EUREKA_SERVER_CLASS:-com.webank.wedatasphere.linkis.eureka.SpringCloudEurekaApplication}
 
 profiles='eureka'
@@ -23,7 +23,7 @@ if [ $1 ];then
         type=$1
 fi
 
-export EUREKA_SERVER_JAVA_OPTS="-Xms$EUREKA_SERVER_HEAP_SIZE -Xmx$EUREKA_SERVER_HEAP_SIZE -XX:+UseG1GC -XX:MaxPermSize=500m -Xloggc:$HOME/logs/$type-gc.log"
+export EUREKA_SERVER_JAVA_OPTS=" -Xmx$EUREKA_SERVER_HEAP_SIZE -XX:+UseG1GC  -Xloggc:$HOME/logs/linkis-gc.log"
 
 java $EUREKA_SERVER_JAVA_OPTS -cp $HOME/conf:$HOME/lib/* $EUREKA_SERVER_CLASS --spring.profiles.active=$profiles  2>&1 > $EUREKA_SERVER_LOG_PATH/linkis.out &
 pid=$!
--- ujes/definedEngines/hive/entrance/bin/start-hive-entrance.sh
+++ ujes/definedEngines/hive/entrance/bin/start-hive-entrance.sh
@@ -3,29 +3,36 @@
 cd `dirname $0`
 cd ..
 HOME=`pwd`
-export DWS_ENGINE_MANAGER_HOME=$HOME
 
-export DWS_ENGINE_MANAGER_PID=$HOME/bin/linkis.pid
+export SERVER_PID=$HOME/bin/linkis.pid
+export SERVER_LOG_PATH=$HOME/logs
+export SERVER_CLASS=com.webank.wedatasphere.linkis.DataWorkCloudApplication
 
-if [[ -f "${DWS_ENGINE_MANAGER_PID}" ]]; then
-    pid=$(cat ${DWS_ENGINE_MANAGER_PID})
+if test -z "$SERVER_HEAP_SIZE"
+then
+  export SERVER_HEAP_SIZE="512M"
+fi
+
+if test -z "$SERVER_JAVA_OPTS"
+then
+  export SERVER_JAVA_OPTS=" -Xmx$SERVER_HEAP_SIZE -XX:+UseG1GC -Xloggc:$HOME/logs/linkis-gc.log"
+fi
+
+if [[ -f "${SERVER_PID}" ]]; then
+    pid=$(cat ${SERVER_PID})
     if kill -0 ${pid} >/dev/null 2>&1; then
-      echo "Hive Entrance is already running."
-      return 0;
+      echo "Server is already running."
+      exit 1
     fi
 fi
 
-export DWS_ENGINE_MANAGER_LOG_PATH=$HOME/logs
-export DWS_ENGINE_MANAGER_HEAP_SIZE="1G"
-export DWS_ENGINE_MANAGER_JAVA_OPTS="-Xms$DWS_ENGINE_MANAGER_HEAP_SIZE -Xmx$DWS_ENGINE_MANAGER_HEAP_SIZE -XX:+UseG1GC -XX:MaxPermSize=500m"
-
-nohup java $DWS_ENGINE_MANAGER_JAVA_OPTS -cp $HOME/conf:$HOME/lib/* com.webank.wedatasphere.linkis.DataWorkCloudApplication 2>&1 > $DWS_ENGINE_MANAGER_LOG_PATH/linkis.out &
+nohup java $SERVER_JAVA_OPTS -cp $HOME/conf:$HOME/lib/* $SERVER_CLASS 2>&1 > $SERVER_LOG_PATH/linkis.out &
 pid=$!
 if [[ -z "${pid}" ]]; then
-    echo "Hive Entrance start failed!"
+    echo "server $SERVER_NAME start failed!"
     exit 1
 else
-    echo "Hive Entrance start succeeded!"
-    echo $pid > $DWS_ENGINE_MANAGER_PID
+    echo "server $SERVER_NAME start succeeded!"
+    echo $pid > $SERVER_PID
     sleep 1
 fi

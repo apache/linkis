26f347a4deea780585c3485e11fcd0538d3517d1
peacewong <wpeace1212@gmail.com>
2020-01-17 21:06:35 +0800

    Specify the hive meta information as required in the install script closes #271

--- conf/config.sh
+++ conf/config.sh
@@ -25,9 +25,9 @@ HDFS_USER_ROOT_PATH=hdfs:///tmp/linkis ##hdfs:// required
 RESULT_SET_ROOT_PATH=hdfs:///tmp/linkis
 
 ### Provide the DB information of Hive metadata database.
-#HIVE_META_URL=
-#HIVE_META_USER=
-#HIVE_META_PASSWORD=
+HIVE_META_URL=
+HIVE_META_USER=
+HIVE_META_PASSWORD=
 
 ###HADOOP CONF DIR
 HADOOP_CONF_DIR=/appcom/config/hadoop-config

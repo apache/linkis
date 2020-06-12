8dccf88715c4d4c951b27fc200d1ed4edeecea83
Yogafire <yogafirew@gmail.com>
2020-06-12 18:09:29 +0800

    update presto-entrance: add presto resource & fix some bugs

--- ujes/definedEngines/presto/entrance/src/main/scala/com/webank/wedatasphere/linkis/entrance/configuration/PrestoConfiguration.scala
+++ ujes/definedEngines/presto/entrance/src/main/scala/com/webank/wedatasphere/linkis/entrance/configuration/PrestoConfiguration.scala
@@ -9,20 +9,21 @@ import com.webank.wedatasphere.linkis.common.conf.{ByteType, CommonVars}
  */
 object PrestoConfiguration {
 
+  val ENTRANCE_MAX_JOB_INSTANCE = CommonVars[Int]("wds.linkis.entrance.max.job.instance", 100)
+  val ENTRANCE_PROTECTED_JOB_INSTANCE = CommonVars[Int]("wds.linkis.entrance.protected.job.instance", 0)
+  val ENTRANCE_RESULTS_MAX_CACHE = CommonVars("wds.linkis.presto.resultSet.cache.max", new ByteType("512k"))
+
   val PRESTO_HTTP_CONNECT_TIME_OUT = CommonVars[java.lang.Long]("wds.linkis.presto.http.connectTimeout", new lang.Long(60)) //单位秒
   val PRESTO_HTTP_READ_TIME_OUT = CommonVars[java.lang.Long]("wds.linkis.presto.http.readTimeout", new lang.Long(60))
 
 
   val PRESTO_DEFAULT_LIMIT = CommonVars("wds.linkis.presto.default.limit", 5000)
   val PRESTO_URL = CommonVars("wds.linkis.presto.url", "http://127.0.0.1:8080")
+  val PRESTO_RESOURCE_CONFIG_PATH = CommonVars("wds.linkis.presto.resource.config", "");
   val PRESTO_USER_NAME = CommonVars("wds.linkis.presto.username", "default")
   val PRESTO_PASSWORD = CommonVars("wds.linkis.presto.password", "")
   val PRESTO_CATALOG = CommonVars("wds.linkis.presto.catalog", "system")
   val PRESTO_SCHEMA = CommonVars("wds.linkis.presto.schema", "")
-  val PRESTO_RESOURCE = CommonVars("wds.linkis.presto.source", "global")
-  val PRESTO_RESULTS_MAX_CACHE = CommonVars("wds.linkis.presto.resultSet.cache.max", new ByteType("512k"))
-
-
-  val PRESTO_ENGINE_REQUEST_MEMORY = CommonVars[Int]("query_max_total_memory", 4) //单位GB
-  val PRESTO_ENGINE_QUEUE_NAME = CommonVars[String]("resource_group", "global")
+  val PRESTO_SOURCE = CommonVars("wds.linkis.presto.source", "global")
+  val PRESTO_REQUEST_MEMORY = CommonVars("presto.session.query_max_total_memory", "8GB")
 }

f534d696c399073fce85e0acfa3babdb74aa628a
wushengyeyouya <690574002@qq.com>
2021-03-10 20:40:24 +0800

    ### What is the purpose of the change
    Related issues: #576
    Add a new module EngineConnPlugin for the Linkis1.0 architecture. This module is defined to simplify the user's implementation of a new Linkis underlying computation storage engine.
    
    ### Brief change log
    
    1. A separate service that provides interfaces for LinkisManager to  obtain the resources when creating an EngineConn.
    
    2. Provides interfaces for EngineConnManager to prepare the start-up argument information of EngineConn after ECM receives the EngineConnBuilder's requests from LinkisManager.
    
    3. Be able to load the dependency jars required by the computation storage engine in real time, so those engines, which have been integreted to Linkis don't need to be pre-installed in EngineConnManager machines.
    
    4. Be able to load the corresponding EngineConnPlugin on demand.

---
+++ linkis-engineconn-plugins/linkis-engineconn-plugin-framework/linkis-engineconn-plugin-core/src/main/scala/com/webank/wedatasphere/linkis/manager/engineplugin/common/launch/entity/EngineConnBuildRequest.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 WeBank
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.webank.wedatasphere.linkis.manager.engineplugin.common.launch.entity
+
+import java.util
+
+import com.webank.wedatasphere.linkis.manager.common.entity.resource.NodeResource
+import com.webank.wedatasphere.linkis.manager.common.protocol.bml.BmlResource
+import com.webank.wedatasphere.linkis.manager.label.entity.Label
+import com.webank.wedatasphere.linkis.protocol.message.RequestProtocol
+
+
+trait EngineConnBuildRequest extends RequestProtocol {
+  val ticketId: String
+  val labels: util.List[Label[_]]
+  val engineResource: NodeResource
+  val engineConnCreationDesc: EngineConnCreationDesc
+}
+
+
+case class EngineConnBuildRequestImpl(ticketId: String,
+                                      labels: util.List[Label[_]],
+                                      engineResource: NodeResource,
+                                      engineConnCreationDesc: EngineConnCreationDesc) extends EngineConnBuildRequest
+
+
+trait RicherEngineConnBuildRequest extends EngineConnBuildRequest {
+
+  def getBmlResources: Array[BmlResource]
+
+  def getStartupConfigs: util.Map[String, Object]
+
+}
+
+
+
+
+
+

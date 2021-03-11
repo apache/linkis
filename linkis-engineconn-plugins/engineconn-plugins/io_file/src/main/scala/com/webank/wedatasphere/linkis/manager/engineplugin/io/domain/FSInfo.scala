def67cef531f754e4f681d8a5bcd16da83ad2962
Alex <382080314@qq.com>
2021-03-11 14:45:49 +0800

    remove author msg from source

--- linkis-engineconn-plugins/engineconn-plugins/io_file/src/main/scala/com/webank/wedatasphere/linkis/manager/engineplugin/io/domain/FSInfo.scala
+++ linkis-engineconn-plugins/engineconn-plugins/io_file/src/main/scala/com/webank/wedatasphere/linkis/manager/engineplugin/io/domain/FSInfo.scala
@@ -21,7 +21,6 @@ import com.webank.wedatasphere.linkis.storage.utils.StorageConfiguration
 
 /**
   * FS信息记录，包括Entrance的ID信息
-  * Created by johnnwang on 2018/10/31.
   */
 class FSInfo(val id: Long, val fs: Fs, var lastAccessTime: Long = System.currentTimeMillis()) {
   def timeout = System.currentTimeMillis() - lastAccessTime > (StorageConfiguration.IO_FS_EXPIRE_TIME.getValue + 60 * 1000)

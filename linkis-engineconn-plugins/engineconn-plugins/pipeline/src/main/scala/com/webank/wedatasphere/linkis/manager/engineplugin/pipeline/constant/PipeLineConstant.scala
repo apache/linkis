ea6aa20f0f5311246819e8a389a276e673351b0b
xyqiao@vip.qq.com <georgeqiao@webank.com>
2021-03-11 16:08:24 +0800

    PipelineEngine adapts to the Linkis1.0's new architecture, Implement the PipelineEngine.

---
+++ linkis-engineconn-plugins/engineconn-plugins/pipeline/src/main/scala/com/webank/wedatasphere/linkis/manager/engineplugin/pipeline/constant/PipeLineConstant.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2019 WeBank
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.webank.wedatasphere.linkis.manager.engineplugin.pipeline.constant
+
+object PipeLineConstant {
+  val DEFAULTC_HARSET = "utf-8"
+  val DEFAULT_SHEETNAME = "result"
+  val DEFAULT_DATEFORMATE = "yyyy-MM-dd HH:mm:ss"
+  val PIPELINE_OUTPUT_ISOVERWRITE = "wds.linkis.engine.pipeline.output.isoverwtite"
+  val PIPELINE_OUTPUT_SHUFFLE_NULL_TYPE = "wds.linkis.engine.pipeline.output.shuffle.null.type"
+  val PIPELINE_OUTPUT_CHARSET = "wds.linkis.engine.pipeline.output.charset"
+  val PIPELINE_FIELD_SPLIT = "wds.linkis.engine.pipeline.field.split"
+  val BLANK = "BLANK"
+}

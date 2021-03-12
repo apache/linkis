ae0a1ebc3a44bf2be61e9c6905f8801da17cbca3
leeebai <120190695@qq.com>
2021-03-12 11:31:00 +0800

    Enhance the adaptability of the Linkis Configuration module to the new Linkis1.0 architecture

--- linkis-computation-governance/linkis-computation-client/src/main/scala/com/webank/wedatasphere/linkis/ujes/client/response/JobLogResult.scala
+++ linkis-computation-governance/linkis-computation-client/src/main/scala/com/webank/wedatasphere/linkis/ujes/client/response/JobLogResult.scala
@@ -14,10 +14,6 @@
  * limitations under the License.
  */
 
-/*
- * created by cooperyang on 2019/07/24.
- */
-
 package com.webank.wedatasphere.linkis.ujes.client.response
 
 import java.util
@@ -26,9 +22,6 @@ import com.webank.wedatasphere.linkis.httpclient.dws.annotation.DWSHttpMessageRe
 
 import scala.beans.BeanProperty
 
-/**
-  * created by cooperyang on 2019/5/23.
-  */
 @DWSHttpMessageResult("/api/rest_j/v\\d+/entrance/(\\S+)/log")
 class JobLogResult extends UJESJobResult {
 

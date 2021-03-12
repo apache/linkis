44fac0aa5fc878f09506b2c2e711f27b05d3237a
peacewong <wpeace1212@gmail.com>
2021-03-12 16:59:46 +0800

    add license and delete the created by annotation.

--- linkis-commons/linkis-protocol/src/main/scala/com/webank/wedatasphere/linkis/protocol/RetryableProtocol.scala
+++ linkis-commons/linkis-protocol/src/main/scala/com/webank/wedatasphere/linkis/protocol/RetryableProtocol.scala
@@ -16,9 +16,7 @@
 
 package com.webank.wedatasphere.linkis.protocol
 
-/**
-  * Created by enjoyyin on 2019/1/7.
-  */
+
 trait RetryableProtocol extends Protocol {
   def retryNum: Int = 2
   def period: Long = 1000l

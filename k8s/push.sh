821797406ddb0cd42ca47830feb5fcf599f858ed
peacewong <wpeace1212@gmail.com>
2021-03-13 15:59:19 +0800

    Optimize directories and files, update version to 1.0.0-RC1 close #675

--- k8s/push.sh
+++ k8s/push.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-docker push $HARBOR_LINKIS/linkis:linkis-bml-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-bml-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-dsm-server-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-dsm-server-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-mdm-server-0.11.0
-docker push $HARBOR_LINKIS/linkis:linkis-mdm-service-mysql-0.11.0
-docker push $HARBOR_LINKIS/linkis:linkis-mdm-service-es-0.11.0
-docker push $HARBOR_LINKIS/linkis:linkis-mdm-service-hive-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-mdm-server-1.0.0-RC1
+docker push $HARBOR_LINKIS/linkis:linkis-mdm-service-mysql-1.0.0-RC1
+docker push $HARBOR_LINKIS/linkis:linkis-mdm-service-es-1.0.0-RC1
+docker push $HARBOR_LINKIS/linkis:linkis-mdm-service-hive-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-gateway-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-gateway-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-resourcemanager-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-resourcemanager-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-cs-server-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-cs-server-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-metadata-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-metadata-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-publicservice-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-publicservice-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-spark-enginemanager-0.11.0
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-spark-entrance-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-spark-enginemanager-1.0.0-RC1
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-spark-entrance-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-hive-enginemanager-0.11.0
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-hive-entrance-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-hive-enginemanager-1.0.0-RC1
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-hive-entrance-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-python-enginemanager-0.11.0
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-python-entrance-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-python-enginemanager-1.0.0-RC1
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-python-entrance-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-pipeline-enginemanager-0.11.0
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-pipeline-entrance-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-pipeline-enginemanager-1.0.0-RC1
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-pipeline-entrance-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-jdbc-enginemanager-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-jdbc-enginemanager-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-mlsql-entrance-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-mlsql-entrance-1.0.0-RC1
 
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-shell-enginemanager-0.11.0
-docker push $HARBOR_LINKIS/linkis:linkis-ujes-shell-entrance-0.11.0
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-shell-enginemanager-1.0.0-RC1
+docker push $HARBOR_LINKIS/linkis:linkis-ujes-shell-entrance-1.0.0-RC1
 
 

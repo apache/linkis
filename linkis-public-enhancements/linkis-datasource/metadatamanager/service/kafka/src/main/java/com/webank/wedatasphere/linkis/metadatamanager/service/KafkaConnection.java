d95531aaa5b64616fbd48720a392b092878b93b4
xutao <xutao_ustc@163.com>
2021-04-28 21:28:41 +0800

    add kafka metadata

---
+++ linkis-public-enhancements/linkis-datasource/metadatamanager/service/kafka/src/main/java/com/webank/wedatasphere/linkis/metadatamanager/service/KafkaConnection.java
@@ -0,0 +1,34 @@
+package com.webank.wedatasphere.linkis.metadatamanager.service;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Properties;
+
+public class KafkaConnection implements Closeable {
+    private AdminClient adminClient;
+
+    public KafkaConnection(String uris) throws Exception{
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, uris);
+        adminClient = KafkaAdminClient.create(props);
+    }
+
+    public KafkaConnection(String uris, String principle, String keytabFilePath) throws Exception {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, uris);
+        adminClient = KafkaAdminClient.create(props);
+    }
+
+    public AdminClient getClient(){
+        return adminClient;
+    }
+
+    @Override
+    public void close() throws IOException {
+        adminClient.close();
+    }
+}

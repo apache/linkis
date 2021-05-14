db1923ffb4cc31b732bfb7550f2881851c54ef05
wushengyeyouya <690574002@qq.com>
2021-05-14 15:23:46 +0800

    Optimize HttpClient module.

---
+++ linkis-commons/linkis-httpclient/src/main/scala/com/webank/wedatasphere/linkis/httpclient/GenericHttpClient.scala
@@ -0,0 +1,27 @@
+package com.webank.wedatasphere.linkis.httpclient
+
+import com.webank.wedatasphere.linkis.common.io.{Fs, FsPath}
+import com.webank.wedatasphere.linkis.httpclient.config.ClientConfig
+import com.webank.wedatasphere.linkis.httpclient.discovery.Discovery
+import com.webank.wedatasphere.linkis.httpclient.exception.HttpMethodNotSupportException
+import com.webank.wedatasphere.linkis.httpclient.request.HttpAction
+import com.webank.wedatasphere.linkis.httpclient.response.{HashMapHttpResult, Result}
+import org.apache.http.HttpResponse
+
+/**
+  * Created by enjoyyin on 2020/9/9.
+  */
+class GenericHttpClient(clientConfig: ClientConfig, clientName: String) extends AbstractHttpClient(clientConfig, clientName) {
+
+  override protected def createDiscovery(): Discovery = throw new HttpMethodNotSupportException("GenericHttpClient not support discovery.")
+
+  override protected def httpResponseToResult(response: HttpResponse, requestAction: HttpAction, responseBody: String): Option[Result] = {
+    val result = new HashMapHttpResult
+    result.set(responseBody, response.getStatusLine.getStatusCode, requestAction.getURL, response.getEntity.getContentType.getValue)
+    Some(result)
+  }
+
+  override protected def getFsByUser(user: String, path: FsPath): Fs = {
+    null
+  }
+}

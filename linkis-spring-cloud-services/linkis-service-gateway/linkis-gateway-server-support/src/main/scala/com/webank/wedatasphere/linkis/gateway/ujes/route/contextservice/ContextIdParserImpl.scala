e6a0fc443b5ae0db40915d268143333b5338ecc6
alexkun <xuanyu710@qq.com>
2021-05-28 15:14:13 +0800

    Optimize linkis spring cloud services module. 1. change versions from 1.0.0-RC1 to 1.0.0 in pom files 2. refactor HaContextGatewayRouter in linkis-gateway-server-support 3. remove resources files under resources folder in all modules 4. add redirect switch for old dss version in linkis-gateway-core close #750

--- linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-server-support/src/main/scala/com/webank/wedatasphere/linkis/gateway/ujes/route/contextservice/ContextIdParserImpl.scala
+++ linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-server-support/src/main/scala/com/webank/wedatasphere/linkis/gateway/ujes/route/contextservice/ContextIdParserImpl.scala
@@ -16,14 +16,20 @@
 
 package com.webank.wedatasphere.linkis.gateway.ujes.route.contextservice
 
-import java.util
-
-import com.webank.wedatasphere.linkis.common.utils.Logging
+import com.webank.wedatasphere.linkis.common.ServiceInstance
+import com.webank.wedatasphere.linkis.common.utils.{Logging, Utils}
 import com.webank.wedatasphere.linkis.cs.common.entity.source.ContextIDParser
 import com.webank.wedatasphere.linkis.cs.common.utils.CSHighAvailableUtils
-import com.webank.wedatasphere.linkis.rpc.instancealias.InstanceAliasManager
-import org.springframework.beans.factory.annotation.Autowired
+import com.webank.wedatasphere.linkis.instance.label.service.InsLabelServiceAdapter
+import com.webank.wedatasphere.linkis.manager.label.builder.factory.LabelBuilderFactoryContext
+import com.webank.wedatasphere.linkis.manager.label.constant.LabelKeyConstant
+import com.webank.wedatasphere.linkis.manager.label.entity.Label
+import com.webank.wedatasphere.linkis.protocol.label.LabelInsQueryRequest
+import org.apache.commons.lang.StringUtils
 import org.springframework.stereotype.Component
+
+import java.util
+import javax.annotation.Resource
 
 
 /**
@@ -32,21 +38,23 @@ import org.springframework.stereotype.Component
 @Component
 class ContextIdParserImpl extends ContextIDParser with Logging {
 
-  @Autowired
-  var instanceAliasManager : InstanceAliasManager = _
+  @Resource
+  private var insLabelService: InsLabelServiceAdapter = _
 
   override def parse(contextId: String): util.List[String] = {
 
     if (CSHighAvailableUtils.checkHAIDBasicFormat(contextId)) {
       val instances = new util.ArrayList[String](2)
       val haContextID = CSHighAvailableUtils.decodeHAID(contextId)
-      if (instanceAliasManager.isInstanceAliasValid(haContextID.getInstance)) {
-        instances.add(instanceAliasManager.getInstanceByAlias(haContextID.getInstance).getInstance)
+      val mainInstance = getInstanceByAlias(haContextID.getInstance())
+      if (null != mainInstance) {
+        instances.add(mainInstance.getInstance)
       } else {
         error(s"parse HAID instance invalid. haIDKey : " + contextId)
       }
-      if (instanceAliasManager.isInstanceAliasValid(haContextID.getBackupInstance)) {
-        instances.add(instanceAliasManager.getInstanceByAlias(haContextID.getBackupInstance).getInstance)
+      val backupInstance = getInstanceByAlias(haContextID.getInstance())
+      if (null != backupInstance) {
+        instances.add(backupInstance.getInstance)
       } else {
         error("parse HAID backupInstance invalid. haIDKey : " + contextId)
       }
@@ -61,6 +69,28 @@ class ContextIdParserImpl extends ContextIDParser with Logging {
       c => if (c < 48 || c >57) return false
     }
     true
+  }
+
+  // todo same as that in RouteLabelInstanceAliasConverter
+  private def getInstanceByAlias(alias: String): ServiceInstance = {
+    if (StringUtils.isNotBlank(alias)) {
+      Utils.tryAndError {
+        val request = new LabelInsQueryRequest()
+        val labelMap = new util.HashMap[String, Any]()
+        labelMap.put(LabelKeyConstant.ROUTE_KEY, alias)
+        request.setLabels(labelMap.asInstanceOf[util.HashMap[String, Object]])
+        var serviceInstance: ServiceInstance = null
+        val labels = new util.ArrayList[Label[_]]()
+        labels.add(LabelBuilderFactoryContext.getLabelBuilderFactory.createLabel[Label[_]](LabelKeyConstant.ROUTE_KEY, alias))
+        val insList = insLabelService.searchInstancesByLabels(labels)
+        if (null != insList && !insList.isEmpty) {
+          serviceInstance = insList.get(0)
+        }
+        serviceInstance
+      }
+    } else {
+      null
+    }
   }
 
 }

9fd63da8ae34eae548cf39b000c404d1f004d883
owenlovemika <owenlovemika@gmail.com>
2021-05-31 11:13:34 +0800

    Add license and annotations

--- linkis-public-enhancements/linkis-publicservice/linkis-script-dev/linkis-storage-script-dev-client/src/main/scala/com/webank/wedatasphere/linkis/filesystem/request/WorkspaceHttpConf.scala
+++ linkis-public-enhancements/linkis-publicservice/linkis-script-dev/linkis-storage-script-dev-client/src/main/scala/com/webank/wedatasphere/linkis/filesystem/request/WorkspaceHttpConf.scala
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.webank.wedatasphere.linkis.filesystem.request
 
 import com.webank.wedatasphere.linkis.httpclient.authentication.AuthenticationStrategy

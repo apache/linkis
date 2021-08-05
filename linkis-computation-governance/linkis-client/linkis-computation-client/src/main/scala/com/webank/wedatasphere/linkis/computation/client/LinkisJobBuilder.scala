e4c15f7b254f9f3d244cd2d7894547ed40420161
cooperyang <cooperyang@webank.com>
2021-08-05 11:38:47 +0800

    添加once引擎client的相关的license

--- linkis-computation-governance/linkis-client/linkis-computation-client/src/main/scala/com/webank/wedatasphere/linkis/computation/client/LinkisJobBuilder.scala
+++ linkis-computation-governance/linkis-client/linkis-computation-client/src/main/scala/com/webank/wedatasphere/linkis/computation/client/LinkisJobBuilder.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.webank.wedatasphere.linkis.computation.client
 
 import java.util
@@ -14,9 +31,7 @@ import com.webank.wedatasphere.linkis.ujes.client.{UJESClient, UJESClientImpl}
 import com.webank.wedatasphere.linkis.ujes.client.request.JobSubmitAction
 import org.apache.commons.lang.StringUtils
 
-/**
-  * Created by enjoyyin on 2021/6/1.
-  */
+
 trait LinkisJobBuilder[Job <: LinkisJob] {
 
   protected var executeUser: String = _

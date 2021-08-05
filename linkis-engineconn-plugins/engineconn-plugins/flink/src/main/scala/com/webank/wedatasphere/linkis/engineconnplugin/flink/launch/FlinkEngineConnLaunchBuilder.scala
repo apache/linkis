6cf424655d50f6b802167135806f9c84b928cb6c
cooperyang <cooperyang@webank.com>
2021-08-05 11:09:18 +0800

    添加flink引擎的相关的license

--- linkis-engineconn-plugins/engineconn-plugins/flink/src/main/scala/com/webank/wedatasphere/linkis/engineconnplugin/flink/launch/FlinkEngineConnLaunchBuilder.scala
+++ linkis-engineconn-plugins/engineconn-plugins/flink/src/main/scala/com/webank/wedatasphere/linkis/engineconnplugin/flink/launch/FlinkEngineConnLaunchBuilder.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.webank.wedatasphere.linkis.engineconnplugin.flink.launch
 
 import com.webank.wedatasphere.linkis.engineconnplugin.flink.config.FlinkEnvConfiguration._
@@ -6,9 +23,7 @@ import com.webank.wedatasphere.linkis.manager.engineplugin.common.conf.EnvConfig
 import com.webank.wedatasphere.linkis.manager.engineplugin.common.launch.entity.EngineConnBuildRequest
 import com.webank.wedatasphere.linkis.manager.engineplugin.common.launch.process.JavaProcessEngineConnLaunchBuilder
 
-/**
-  * Created by enjoyyin on 2021/4/7.
-  */
+
 class FlinkEngineConnLaunchBuilder extends JavaProcessEngineConnLaunchBuilder {
 
   override protected def getCommands(implicit engineConnBuildRequest: EngineConnBuildRequest): Array[String] = {

93f0da53116c9d503cf887b1ca3b4eee5627ad85
yuxin.yuan <yuanyuxin@chinatelecom.cn>
2021-09-24 14:48:47 +0800

    fix GetAllDataSourceTypesAction url

--- linkis-public-enhancements/linkis-datasource/datasource-client/src/main/scala/com/webank/wedatasphere/linkis/datasource/client/response/GetAllDataSourceTypesResult.scala
+++ linkis-public-enhancements/linkis-datasource/datasource-client/src/main/scala/com/webank/wedatasphere/linkis/datasource/client/response/GetAllDataSourceTypesResult.scala
@@ -1,15 +1,16 @@
 package com.webank.wedatasphere.linkis.datasource.client.response
 
+import java.util
+
 import com.webank.wedatasphere.linkis.datasourcemanager.common.domain.DataSourceType
 import com.webank.wedatasphere.linkis.httpclient.dws.DWSHttpClient
 import com.webank.wedatasphere.linkis.httpclient.dws.annotation.DWSHttpMessageResult
 import com.webank.wedatasphere.linkis.httpclient.dws.response.DWSResult
 
-import java.util
 import scala.beans.BeanProperty
 
-@DWSHttpMessageResult("/api/rest_j/v\\d+/datasource/type/all")
-class GetAllDataSourceTypesResult extends DWSResult{
+@DWSHttpMessageResult("/api/rest_j/v\\d+/datasources/type/all")
+class GetAllDataSourceTypesResult extends DWSResult {
   @BeanProperty var type_list: java.util.List[java.util.Map[String, Any]] = _
 
   def getAllDataSourceType: util.List[DataSourceType]={

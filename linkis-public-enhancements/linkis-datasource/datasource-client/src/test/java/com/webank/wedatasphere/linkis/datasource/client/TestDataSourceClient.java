91b8afb407be93477d0217328e61488f0c407c85
Dlimeng <77954309@qq.com>
2021-11-16 17:46:55 +0800

    hive client

---
+++ linkis-public-enhancements/linkis-datasource/datasource-client/src/test/java/com/webank/wedatasphere/linkis/datasource/client/TestDataSourceClient.java
@@ -0,0 +1,9 @@
+package com.webank.wedatasphere.linkis.datasource.client;
+
+import com.webank.wedatasphere.linkis.httpclient.dws.config.DWSClientConfigBuilder;
+
+public class TestDataSourceClient {
+    public static void main(String[] args) {
+
+    }
+}

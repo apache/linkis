f5f577242e0199d7bd0b9ec4abdad08070b388db
leo_jie <leojie1314@gmail.com>
2021-11-24 22:31:28 +0800

    add kerberos auth type for jdbc

--- linkis-engineconn-plugins/engineconn-plugins/jdbc/src/main/java/org/apache/linkis/manager/engineplugin/jdbc/ConnectionManager.java
+++ linkis-engineconn-plugins/engineconn-plugins/jdbc/src/main/java/org/apache/linkis/manager/engineplugin/jdbc/ConnectionManager.java
@@ -17,19 +17,22 @@
  
 package org.apache.linkis.manager.engineplugin.jdbc;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.linkis.manager.engineplugin.jdbc.conf.JDBCConfiguration;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.dbcp.BasicDataSourceFactory;
 import org.apache.commons.lang.StringUtils;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.IOException;
+import java.sql.*;
 import java.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.apache.hadoop.fs.CommonConfigurationKeysPublic.HADOOP_SECURITY_AUTHENTICATION;
+import static org.apache.hadoop.security.UserGroupInformation.AuthenticationMethod.KERBEROS;
 
 public  class  ConnectionManager {
 
@@ -95,7 +98,6 @@ public  class  ConnectionManager {
         return url.substring(0, index);
     }
 
-
     protected DataSource createDataSources(Map<String, String> properties) throws SQLException {
         String url = properties.get("jdbc.url");
         String username = properties.get("jdbc.username").trim();
@@ -140,24 +142,38 @@ public  class  ConnectionManager {
     }
 
     public Connection getConnection(Map<String, String> properties) throws SQLException {
-        String url = properties.get("jdbc.url");
-        if(StringUtils.isEmpty(properties.get("jdbc.username"))) {
-            throw new NullPointerException("jdbc.username cannot be null.");
-        }
-        url = getRealURL(url);
-        //这里通过URL+username识别
-        // String key = url + "/" + properties.get("jdbc.username").trim();
-        String key = url;
-        DataSource dataSource = databaseToDataSources.get(key);
-        if(dataSource == null) {
-            synchronized (databaseToDataSources) {
-                if(dataSource == null) {
-                    dataSource = createDataSources(properties);
-                    databaseToDataSources.put(key, dataSource);
+        final String url = clearUrl(properties.get("jdbc.url"));
+        if (!isKerberosEnabled(properties)) {
+            if(StringUtils.isEmpty(properties.get("jdbc.username"))) {
+                throw new NullPointerException("jdbc.username cannot be null.");
+            }
+            String key = getRealURL(url);
+            //这里通过URL+username识别
+            // String key = url + "/" + properties.get("jdbc.username").trim();
+            // String key = url;
+            DataSource dataSource = databaseToDataSources.get(key);
+            if(dataSource == null) {
+                synchronized (databaseToDataSources) {
+                    if(dataSource == null) {
+                        dataSource = createDataSources(properties);
+                        databaseToDataSources.put(key, dataSource);
+                    }
                 }
             }
+            return dataSource.getConnection();
         }
-        return dataSource.getConnection();
+        String user = properties.get("jdbc.user");
+        logger.info("The user {} has selected auth type of jdbc is kerberos.", user);
+        String proxyUserProperty = properties.get("jdbc.proxy.user.property");
+        String connectionUrl = url;
+
+        if (StringUtils.isNotBlank(proxyUserProperty)) {
+            connectionUrl = url.concat(";").concat(proxyUserProperty + "=" + user);
+        }
+
+        logger.info("JDBC url is {}", connectionUrl);
+        createKerberosSecureConfiguration(properties);
+        return DriverManager.getConnection(connectionUrl);
     }
 
     public void close() {
@@ -167,6 +183,41 @@ public  class  ConnectionManager {
                 ((BasicDataSource)dataSource).close();
             } catch (SQLException e) {}
         }
+    }
+
+    private boolean isKerberosEnabled(Map<String, String> properties) {
+        return "KERBEROS".equals(properties.getOrDefault("jdbc.auth.type", "USERNAME").trim());
+    }
+
+    private void createKerberosSecureConfiguration(Map<String, String> properties) {
+        Configuration conf = new Configuration();
+        conf.set(HADOOP_SECURITY_AUTHENTICATION, KERBEROS.toString());
+        UserGroupInformation.setConfiguration(conf);
+        try {
+            if (!UserGroupInformation.isSecurityEnabled()
+                    || UserGroupInformation.getCurrentUser().getAuthenticationMethod() != KERBEROS
+                    || !UserGroupInformation.isLoginKeytabBased()) {
+                String keytab = properties.get("jdbc.keytab.location");
+                String principal = properties.get("jdbc.principal");
+                UserGroupInformation.loginUserFromKeytab(principal, keytab);
+                logger.info("Login successfully with keytab: {} and principal: {}", keytab, principal);
+            } else {
+                logger.info("The user has already logged in using keytab and principal, " +
+                        "no action required");
+            }
+        } catch (IOException e) {
+            logger.error("Failed to get either keytab location or principal name in the " +
+                    "jdbc executor", e);
+        }
+
+    }
+
+    private String clearUrl(String url) {
+        if (url.startsWith("\"") && url.endsWith("\"")) {
+            url = url.trim();
+            return url.substring(1, url.length() - 1);
+        }
+        return url;
     }
 
     public static void main(String[] args) throws Exception {

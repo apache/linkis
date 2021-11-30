eee37e19877215fdc609f5da1d035f14ec272598
leo_jie <leojie1314@gmail.com>
2021-11-30 21:31:08 +0800

    Move the class of JdbcAuthType to jdbc package.

--- linkis-engineconn-plugins/engineconn-plugins/jdbc/src/main/java/org/apache/linkis/manager/engineplugin/jdbc/JdbcAuthType.java
+++ linkis-engineconn-plugins/engineconn-plugins/jdbc/src/main/java/org/apache/linkis/manager/engineplugin/jdbc/JdbcAuthType.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.linkis.hadoop.common.jdbc;
+package org.apache.linkis.manager.engineplugin.jdbc;
 
 public enum JdbcAuthType {
     /**

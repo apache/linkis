9b5e2d9bb5e52e7aee7609b2e0164a986cf86f26
wushengyeyouya <690574002@qq.com>
2021-12-28 15:12:52 +0800

    Since both ECM and EC need Operator module, so the Operator module has been moved to linkis-manager-common.

--- linkis-computation-governance/linkis-engineconn/linkis-engineconn-executor/accessible-executor/src/main/scala/org/apache/linkis/engineconn/acessible/executor/service/DefaultOperateService.scala
+++ linkis-computation-governance/linkis-engineconn/linkis-engineconn-executor/accessible-executor/src/main/scala/org/apache/linkis/engineconn/acessible/executor/service/DefaultOperateService.scala
@@ -18,22 +18,25 @@
 package org.apache.linkis.engineconn.acessible.executor.service
 import org.apache.commons.lang.exception.ExceptionUtils
 import org.apache.linkis.common.utils.{Logging, Utils}
-import org.apache.linkis.engineconn.acessible.executor.operator.OperatorFactory
+import org.apache.linkis.manager.common.operator.OperatorFactory
 import org.apache.linkis.manager.common.protocol.engine.{EngineOperateRequest, EngineOperateResponse}
 import org.apache.linkis.message.annotation.Receiver
 import org.springframework.stereotype.Service
+
+import scala.collection.JavaConverters.mapAsScalaMapConverter
 
 @Service
 class DefaultOperateService extends OperateService with Logging {
 
   @Receiver
   override def executeOperation(engineOperateRequest: EngineOperateRequest): EngineOperateResponse = {
-    val operator = Utils.tryCatch(OperatorFactory().getOperatorRequest(engineOperateRequest)){ t =>
+    val parameters = engineOperateRequest.parameters.asScala.toMap
+    val operator = Utils.tryCatch(OperatorFactory().getOperatorRequest(parameters)){ t =>
       error(s"Get operator failed, parameters is ${engineOperateRequest.parameters}.", t)
       return EngineOperateResponse(Map.empty, true, ExceptionUtils.getRootCauseMessage(t))
     }
     info(s"Try to execute operator ${operator.getClass.getSimpleName} with parameters ${engineOperateRequest.parameters}.")
-    val result = Utils.tryCatch(operator(engineOperateRequest.parameters)) {t =>
+    val result = Utils.tryCatch(operator(parameters)) {t =>
       error(s"Execute ${operator.getClass.getSimpleName} failed.", t)
       return EngineOperateResponse(Map.empty, true, ExceptionUtils.getRootCauseMessage(t))
     }
--- linkis-computation-governance/linkis-manager/linkis-manager-commons/linkis-manager-common/src/main/scala/org/apache/linkis/manager/common/operator/OperatorFactory.scala
+++ linkis-computation-governance/linkis-manager/linkis-manager-commons/linkis-manager-common/src/main/scala/org/apache/linkis/manager/common/operator/OperatorFactory.scala
@@ -5,27 +5,27 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-package org.apache.linkis.engineconn.acessible.executor.operator
 
-import org.apache.linkis.common.exception.WarnException
+package org.apache.linkis.manager.common.operator
+
 import org.apache.linkis.common.utils.{ClassUtils, Logging}
-import org.apache.linkis.manager.common.protocol.engine.EngineOperateRequest
+import org.apache.linkis.governance.common.exception.GovernanceErrorException
+import org.apache.linkis.manager.common.protocol.OperateRequest
 
 trait OperatorFactory {
 
   def getOperatorName(parameters: Map[String, Any]): String
 
-  def getOperatorRequest(request: EngineOperateRequest): Operator
+  def getOperatorRequest(parameters: Map[String, Any]): Operator
 
 }
 
@@ -42,19 +42,18 @@ class OperatorFactoryImpl extends OperatorFactory with Logging {
 
   private val operators: Map[String, _ <: Operator] = ClassUtils.reflections.getSubTypesOf(classOf[Operator])
     .filterNot(ClassUtils.isInterfaceOrAbstract).flatMap { clazz =>
-      val operator = clazz.newInstance()
-      operator.getNames.map(name => name -> operator)
-    }.toMap
+    val operator = clazz.newInstance()
+    operator.getNames.map(name => name -> operator)
+  }.toMap
   info("Launched operators list => " + operators)
 
   override def getOperatorName(parameters: Map[String, Any]): String =
-    parameters.getOrElse(EngineOperateRequest.OPERATOR_NAME_KEY,
-      throw new WarnException(20031, s"${EngineOperateRequest.OPERATOR_NAME_KEY} is not exists.")).asInstanceOf[String]
+    OperateRequest.getOperationName(parameters)
 
-  override def getOperatorRequest(request: EngineOperateRequest): Operator = {
-    val operatorName = getOperatorName(request.parameters)
+  override def getOperatorRequest(parameters: Map[String, Any]): Operator = {
+    val operatorName = getOperatorName(parameters)
     if (operators.contains(operatorName)) operators(operatorName)
-    else throw new WarnException(20030, s"Cannot find operator named $operatorName.")
+    else throw new GovernanceErrorException(20030, s"Cannot find operator named $operatorName.")
   }
 
 }

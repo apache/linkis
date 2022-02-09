3bcb8d05b9869105a19325842288d90cbb1449d9
xiaojie19852006 <xiaojie19852006@163.com>
2022-02-09 15:45:38 +0800

    revise define mdq receiver

--- linkis-public-enhancements/linkis-datasource/linkis-metadata/src/main/scala/org/apache/linkis/metadata/receiver/MDQReceiver.scala
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata/src/main/scala/org/apache/linkis/metadata/receiver/MDQReceiver.scala
@@ -5,19 +5,17 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-package org.apache.linkis.metadata.receiver
 
-import java.util
+package org.apache.linkis.metadata.receiver
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.linkis.common.utils.{Logging, Utils}
@@ -30,6 +28,7 @@ import org.apache.linkis.rpc.{Receiver, Sender}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
+import java.util
 import scala.concurrent.duration.Duration
 
 

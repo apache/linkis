541bbbcb1bafca5c1ebfc628b0ba040c1796bf3d
peacewong <wpeace1212@gmail.com>
2022-02-11 19:51:19 +0800

    Cooperate with UDF to support version management optimization close #1422

--- linkis-computation-governance/linkis-engineconn/linkis-engineconn-core/src/main/scala/org/apache/linkis/engineconn/core/hook/ExecutorHook.scala
+++ linkis-computation-governance/linkis-engineconn/linkis-engineconn-core/src/main/scala/org/apache/linkis/engineconn/core/hook/ExecutorHook.scala
@@ -5,47 +5,55 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-package org.apache.linkis.engineconn.computation.executor.hook
+
+package org.apache.linkis.engineconn.core.hook
 
 import org.apache.linkis.common.utils.{ClassUtils, Logging, Utils}
-import org.apache.linkis.engineconn.common.creation.EngineCreationContext
-import org.apache.linkis.engineconn.computation.executor.execute.EngineExecutionContext
+import org.apache.linkis.engineconn.executor.entity.Executor
 
 import scala.collection.JavaConverters.asScalaSetConverter
 import scala.collection.mutable.ArrayBuffer
 
-
-trait ComputationExecutorHook {
+trait ExecutorHook extends Logging {
 
   def getOrder(): Int = 1
 
-  def getHookName(): String
+  def getHookName(): String = getClass.getName
 
-  def beforeExecutorExecute(engineExecutionContext: EngineExecutionContext, engineCreationContext: EngineCreationContext, codeBeforeHook: String): String = codeBeforeHook
+  def isAccepted(codeType: String): Boolean
+
+  /*
+  Will be called before executor inited, and only be called once.
+   */
+  def beforeExecutorInit(executor: Executor): Unit = {}
+
+  /*
+  Will be called after executor inited, and only be called once.
+   */
+  def afterExecutorInit(executor: Executor): Unit = {}
 
 }
 
-object ComputationExecutorHook extends Logging {
+object ExecutorHook extends Logging {
 
-  private lazy val computationExecutorHooks: Array[ComputationExecutorHook] = initComputationExecutorHook
+  private lazy val executorHooks: Array[ExecutorHook] = initExecutorHooks
 
-  private def initComputationExecutorHook: Array[ComputationExecutorHook] = {
-    val hooks = new ArrayBuffer[ComputationExecutorHook]
+  private def initExecutorHooks(): Array[ExecutorHook] = {
+    val hooks = new ArrayBuffer[ExecutorHook]
     Utils.tryCatch {
       val reflections = ClassUtils.reflections
-      val allSubClass = reflections.getSubTypesOf(classOf[ComputationExecutorHook])
+      val allSubClass = reflections.getSubTypesOf(classOf[ExecutorHook])
       allSubClass.asScala.filter(!ClassUtils.isInterfaceOrAbstract(_)).foreach(l => {
-        hooks += l.newInstance
+        hooks += l.newInstance()
       })
     } {
       t: Throwable =>
@@ -54,5 +62,6 @@ object ComputationExecutorHook extends Logging {
     hooks.sortWith((a, b) => a.getOrder() <= b.getOrder()).toArray
   }
 
-  def getComputationExecutorHooks: Array[ComputationExecutorHook] = computationExecutorHooks
+  def getAllExecutorHooks(): Array[ExecutorHook] = executorHooks
+
 }

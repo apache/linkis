44e4e200c7a31d46834a2c2e02ab312e2b8f22a3
peacewong <wpeace1212@gmail.com>
2022-02-11 14:35:55 +0800

    add gateway-authentication module

---
+++ linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-authentication/src/main/java/org/apache/linkis/gateway/authentication/dao/TokenDao.java
@@ -0,0 +1,17 @@
+package org.apache.linkis.gateway.authentication.dao;
+
+import org.apache.linkis.gateway.authentication.entity.TokenEntity;
+
+import java.util.List;
+
+public interface TokenDao {
+    Boolean insertToken(TokenEntity token); // TODO
+
+    Boolean updateToken(TokenEntity token); // TODO
+
+    Boolean removeToken(TokenEntity token); // TODO
+
+    List<TokenEntity> selectTokenByName(String tokenName);
+
+    List<TokenEntity> getAllTokens();
+}

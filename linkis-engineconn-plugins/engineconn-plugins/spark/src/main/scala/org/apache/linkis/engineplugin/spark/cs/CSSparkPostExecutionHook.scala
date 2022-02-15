a322bc4e0964de7ab23aace1fcbcb4bbdfc0de64
HmhWz <934178858@qq.com>
2022-02-15 10:16:52 +0800

    optimize spark engineplugin

--- linkis-engineconn-plugins/engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/cs/CSSparkPostExecutionHook.scala
+++ linkis-engineconn-plugins/engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/cs/CSSparkPostExecutionHook.scala
@@ -5,52 +5,48 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package org.apache.linkis.engineplugin.spark.cs
 
 import org.apache.linkis.common.utils.Logging
 import org.apache.linkis.cs.client.utils.ContextServiceUtils
 import org.apache.linkis.engineconn.computation.executor.execute.EngineExecutionContext
-import org.apache.linkis.engineplugin.spark.extension.SparkPreExecutionHook
+import org.apache.linkis.engineplugin.spark.extension.SparkPostExecutionHook
+import org.apache.linkis.scheduler.executer.ExecuteResponse
 import javax.annotation.PostConstruct
+import org.apache.commons.lang.StringUtils
 import org.springframework.stereotype.Component
 
-
 @Component
-class CSSparkPreExecutionHook extends SparkPreExecutionHook with Logging{
+class CSSparkPostExecutionHook extends SparkPostExecutionHook with Logging{
 
   @PostConstruct
-  def  init(): Unit ={
-    SparkPreExecutionHook.register(this)
+  def  init(): Unit = {
+    SparkPostExecutionHook.register(this)
   }
 
+  override def hookName: String = {
+    "CSSparkPostExecutionHook"
+  }
 
+  override def callPostExecutionHook(engineExecutorContext: EngineExecutionContext, executeResponse: ExecuteResponse, code: String): Unit = {
+    val contextIDValueStr = ContextServiceUtils.getContextIDStrByMap(engineExecutorContext.getProperties)
+    val nodeNameStr = ContextServiceUtils.getNodeNameStrByMap(engineExecutorContext.getProperties)
 
-  override def hookName: String = "CSSparkPreExecutionHook"
-
-  override def callPreExecutionHook(engineExecutionContext: EngineExecutionContext, code: String): String = {
-
-    var parsedCode = code
-    val contextIDValueStr = ContextServiceUtils.getContextIDStrByMap(engineExecutionContext.getProperties)
-    val nodeNameStr = ContextServiceUtils.getNodeNameStrByMap(engineExecutionContext.getProperties)
-    info(s"Start to call CSSparkPreExecutionHook,contextID is $contextIDValueStr, nodeNameStr is $nodeNameStr")
-    parsedCode = try {
-      CSTableParser.parse(engineExecutionContext, parsedCode, contextIDValueStr, nodeNameStr)
-    } catch {
-      case t: Throwable =>
-        info("Failed to parser cs table", t)
-        parsedCode
+    if (StringUtils.isNotEmpty(contextIDValueStr) && StringUtils.isNotEmpty(nodeNameStr)) {
+      info(s"Start to call CSSparkPostExecutionHook,contextID is $contextIDValueStr, nodeNameStr is $nodeNameStr")
+      CSTableParser.clearCSTmpView(code, contextIDValueStr, nodeNameStr)
+      info(s"Finished to call CSSparkPostExecutionHook,contextID is $contextIDValueStr, nodeNameStr is $nodeNameStr")
     }
-    info(s"Finished to call CSSparkPreExecutionHook,contextID is $contextIDValueStr, nodeNameStr is $nodeNameStr")
-    parsedCode
+
   }
 }

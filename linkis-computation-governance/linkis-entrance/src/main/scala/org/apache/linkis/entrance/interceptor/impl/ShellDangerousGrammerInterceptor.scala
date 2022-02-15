5c968ed731ad318e7f11f5a4a1bb140f7e056246
peacewong <wpeace1212@gmail.com>
2022-02-15 15:51:06 +0800

    Optimize code logic

--- linkis-computation-governance/linkis-entrance/src/main/scala/org/apache/linkis/entrance/interceptor/impl/ShellDangerousGrammerInterceptor.scala
+++ linkis-computation-governance/linkis-entrance/src/main/scala/org/apache/linkis/entrance/interceptor/impl/ShellDangerousGrammerInterceptor.scala
@@ -14,12 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package org.apache.linkis.entrance.interceptor.impl
 
 import org.apache.linkis.common.utils.Logging
 import org.apache.linkis.entrance.conf.EntranceConfiguration
-import org.apache.linkis.entrance.exception.{DangerousGramsCheckException, SensitiveTablesCheckException}
+import org.apache.linkis.entrance.exception.DangerousGramsCheckException
 import org.apache.linkis.entrance.interceptor.EntranceInterceptor
 import org.apache.linkis.governance.common.entity.job.JobRequest
 import org.apache.linkis.governance.common.paser.CodeType
@@ -31,71 +31,80 @@ import java.lang
 class ShellDangerousGrammerInterceptor extends EntranceInterceptor with Logging {
 
   private val shellDangerousGrammerCheckSwitch = EntranceConfiguration.SHELL_DANGER_CHECK_SWITCH.getValue
-  private val shellDangerUsage = EntranceConfiguration.SHELL_DANGER_USAGE.getValue
-  private val shellWhiteUsage = EntranceConfiguration.SHELL_WHITE_USAGE.getValue
+  private val shellDangerCode = EntranceConfiguration.SHELL_DANGER_USAGE.getValue.split(",")
+  private val shellWhiteCodes = EntranceConfiguration.SHELL_WHITE_USAGE.getValue
 
   info(s"shellDangerousGrammerCheckSwitch : ${shellDangerousGrammerCheckSwitch}")
   if (shellDangerousGrammerCheckSwitch) {
-    info(s"SHELL DANGER USAGE ${shellDangerUsage}")
-    info(s"SHELL White USAGE ${shellWhiteUsage}")
+    info(s"SHELL DANGER USAGE ${shellDangerCode}")
+    info(s"SHELL White USAGE ${shellWhiteCodes}")
   }
 
 
   def shellWhiteUsage(shellContent: String): Boolean = {
     val shellLines = shellContent.split("\n")
     var signature: Boolean = false
-    shellLines foreach {
-      shellLine =>
-       shellLine.split(";").foreach(inner => {
-         val shellCommand: String = inner.trim.split(" ")(0)
-         if (shellWhiteUsage.split(",").contains(shellCommand)) {
-           signature = true
-         }
-       })
+    shellLines foreach { shellLine =>
+      shellLine.split(";").foreach(inner => {
+        val shellCommand: String = inner.trim.split(" ")(0)
+        if (shellWhiteCodes.split(",").contains(shellCommand)) {
+          signature = true
+        }
+      })
     }
     signature
   }
 
+  /**
+   * check dander shell usage
+   *
+   * @param shellContent
+   * @return
+   */
   def shellContainDangerUsage(shellContent: String): Boolean = {
     val shellLines = shellContent.split("\n")
     var signature: Boolean = false
-    shellLines synchronized {
-      shellLines foreach {
-        shellLine =>
-          if (shellLine.trim.endsWith(".sh")) { //禁止执行shell命令
-            signature = true
-          } else {
-            val shellCommands = shellLine.trim.split(" ")
-            shellCommands foreach {
-              shellCommand =>
-                shellDangerUsage.split(",").contains(shellCommand) match {
-                  case true => signature = true
-                  case _ =>
-                }
+    shellLines foreach { shellLine =>
+      if (shellLine.trim.endsWith(".sh")) {
+        signature = true
+      } else {
+        val shellCommands = shellLine.trim.split(" ")
+        shellCommands foreach {
+          shellCommand =>
+            if (shellDangerCode.contains(shellCommand)) {
+              signature = true
             }
-          }
+        }
       }
     }
     signature
   }
 
   /**
-    * The apply function is to supplement the information of the incoming parameter task, making the content of this task more complete.
-    *   * Additional information includes: database information supplement, custom variable substitution, code check, limit limit, etc.
-    * apply函数是对传入参数task进行信息的补充，使得这个task的内容更加完整。
-    * 补充的信息包括: 数据库信息补充、自定义变量替换、代码检查、limit限制等
-    *
-    * @param logAppender Used to cache the necessary reminder logs and pass them to the upper layer(用于缓存必要的提醒日志，传给上层)
-    * @return
-    */
+   * The apply function is to supplement the information of the incoming parameter task, making the content of this task more complete.
+   *   * Additional information includes: database information supplement, custom variable substitution, code check, limit limit, etc.
+   * apply函数是对传入参数task进行信息的补充，使得这个task的内容更加完整。
+   * 补充的信息包括: 数据库信息补充、自定义变量替换、代码检查、limit限制等
+   *
+   * @param jobRequest
+   * @param logAppender Used to cache the necessary reminder logs and pass them to the upper layer(用于缓存必要的提醒日志，传给上层)
+   * @return
+   */
   override def apply(jobRequest: JobRequest, logAppender: lang.StringBuilder): JobRequest = {
+    if (!shellDangerousGrammerCheckSwitch) return jobRequest
+
     val codeType = LabelUtil.getCodeType(jobRequest.getLabels)
     val engineType = LabelUtil.getEngineType(jobRequest.getLabels)
-    // todo check enum equals
-    if (CodeType.Shell.equals(CodeType.getType(codeType))
-      || EngineType.SHELL.equals(EngineType.mapStringToEngineType(engineType))) {
+    if (CodeType.Shell.equals(CodeType.getType(codeType)) || EngineType.SHELL.equals(EngineType.mapStringToEngineType(engineType))) {
       info(s"GET REQUEST CODE_TYPE ${codeType} and ENGINE_TYPE ${EngineType}")
-    jobRequest
-    } else jobRequest
+      if (shellContainDangerUsage(jobRequest.getExecutionCode)) {
+        throw  DangerousGramsCheckException("Shell code contains blacklisted code(shell中包含黑名单代码)")
+      } /*else if (!shellWhiteUsage(jobRequest.getExecutionCode)) {
+        throw  DangerousGramsCheckException("The shell code is not in the whitelist code(shell代码不在白名单中)")
+      }*/
+      jobRequest
+    } else {
+      jobRequest
     }
   }
+}

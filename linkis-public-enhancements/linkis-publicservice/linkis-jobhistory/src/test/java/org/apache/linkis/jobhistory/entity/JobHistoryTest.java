0f4e7fe73db0602f66020f36cec7f6d34ee5e821
casionone <casionone@gmail.com>
2022-02-22 15:47:56 +0800

    Avoid time zone issues

--- linkis-public-enhancements/linkis-publicservice/linkis-jobhistory/src/test/java/org/apache/linkis/jobhistory/entity/JobHistoryTest.java
+++ linkis-public-enhancements/linkis-publicservice/linkis-jobhistory/src/test/java/org/apache/linkis/jobhistory/entity/JobHistoryTest.java
@@ -20,6 +20,7 @@ package org.apache.linkis.jobhistory.entity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,12 +32,15 @@ public class JobHistoryTest {
     @DisplayName("Method description: ...")
     public void testGetUpdateTimeMills() throws Exception {
         long time = 1643738522000L; // 2022-02-02 02:02:02.000
-        String timestr = "2022-02-02 02:02:02.000";
+        //String timestr = "2022-02-02 02:02:02.000";
 
         JobHistory jobHistory = new JobHistory();
         Date date = new Date(time);
         jobHistory.setCreated_time(date);
         jobHistory.setUpdated_time(date);
-        assertEquals(timestr, jobHistory.getUpdateTimeMills());
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String expectedStr=dateFormat.format(date);
+        assertEquals(expectedStr, jobHistory.getUpdateTimeMills());
     }
 }

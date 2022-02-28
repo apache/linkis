bb33a93ae2137449ea6b60cf99de1fa45d7c8205
peacewong <wpeace1212@gmail.com>
2022-02-28 16:45:14 +0800

    Added DataSourceUtils tool class for creating datasource #1574

--- linkis-public-enhancements/linkis-datasource/linkis-metadata/src/main/java/org/apache/linkis/metadata/hive/config/LinkisMybatisConfig.java
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata/src/main/java/org/apache/linkis/metadata/hive/config/LinkisMybatisConfig.java
@@ -18,55 +18,43 @@
 package org.apache.linkis.metadata.hive.config;
 
 import org.apache.linkis.hadoop.common.conf.HadoopConf;
+import org.apache.linkis.metadata.condition.DataSourceCondition;
 import org.apache.linkis.metadata.util.DWSConfig;
 import org.apache.linkis.metadata.util.HiveUtils;
+import org.apache.linkis.mybatis.DataSourceUtils;
 
 import org.apache.commons.lang.StringUtils;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.alibaba.druid.pool.DruidDataSource;
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.SQLException;
 import java.util.HashMap;
 
 @Configuration
 @EnableTransactionManagement(order = 2)
 public class LinkisMybatisConfig {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final Logger logger = LoggerFactory.getLogger(LinkisMybatisConfig.class);
 
-    private DruidDataSource hiveDataSource() {
-        DruidDataSource datasource = new DruidDataSource();
-        int initialSize = 1;
-        int minIdle = 1;
-        int maxActive = 20;
-        int maxWait = 6000;
-        int timeBetweenEvictionRunsMillis = 60000;
-        int minEvictableIdleTimeMillis = 300000;
-        String validationQuery = "SELECT 1";
-        boolean testWhileIdle = true;
-        boolean testOnBorrow = true;
-        boolean testOnReturn = true;
+    private DataSource hiveDataSource() {
 
         String url = DWSConfig.HIVE_META_URL.getValue();
         String username = DWSConfig.HIVE_META_USER.getValue();
         String password = DWSConfig.HIVE_META_PASSWORD.getValue();
 
-        org.apache.hadoop.conf.Configuration hiveConf = null;
         if (StringUtils.isBlank(url)
                 || StringUtils.isBlank(username)
                 || StringUtils.isBlank(password)) {
-            hiveConf = HiveUtils.getDefaultConf(HadoopConf.HADOOP_ROOT_USER().getValue());
-            logger.info("从配置文件中读取hive数据库连接地址");
+            org.apache.hadoop.conf.Configuration hiveConf =
+                    HiveUtils.getDefaultConf(HadoopConf.HADOOP_ROOT_USER().getValue());
+            logger.info("from hive conf to pares meta store JDBC url");
             url = hiveConf.get("javax.jdo.option.ConnectionURL");
             username = hiveConf.get("javax.jdo.option.ConnectionUserName");
             password = hiveConf.get("javax.jdo.option.ConnectionPassword");
@@ -76,75 +64,18 @@ public class LinkisMybatisConfig {
             }
         }
 
-        logger.info("数据库连接地址信息=" + url);
-        datasource.setUrl(url);
-        datasource.setUsername(username);
-        datasource.setPassword(password);
-        datasource.setInitialSize(initialSize);
-        datasource.setMinIdle(minIdle);
-        datasource.setMaxActive(maxActive);
-        datasource.setMaxWait(maxWait);
-        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
-        datasource.setValidationQuery(validationQuery);
-        datasource.setTestWhileIdle(testWhileIdle);
-        datasource.setTestOnBorrow(testOnBorrow);
-        datasource.setTestOnReturn(testOnReturn);
-        return datasource;
+        return DataSourceUtils.buildDataSource(url, username, password);
     }
 
-    private DruidDataSource mysqlDataSource() {
-        DruidDataSource datasource = new DruidDataSource();
-        String dbUrl = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_URL.getValue();
-        String username = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_USERNAME.getValue();
-        String password = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_PASSWORD.getValue();
-        String driverClassName =
-                DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_DRIVER_CLASS_NAME.getValue();
-        int initialSize = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_INITIALSIZE.getValue();
-        int minIdle = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_MINIDLE.getValue();
-        int maxActive = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_MAXACTIVE.getValue();
-        int maxWait = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_MAXWAIT.getValue();
-        int timeBetweenEvictionRunsMillis =
-                DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_TBERM.getValue();
-        int minEvictableIdleTimeMillis = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_MEITM.getValue();
-        String validationQuery = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_VALIDATIONQUERY.getValue();
-        boolean testWhileIdle = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_TESTWHILEIDLE.getValue();
-        boolean testOnBorrow = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_TESTONBORROW.getValue();
-        boolean testOnReturn = DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_TESTONRETURN.getValue();
-        boolean poolPreparedStatements =
-                DWSConfig.BDP_SERVER_MYBATIS_DATASOURCE_POOLPREPAREDSTATEMENTS.getValue();
-        datasource.setUrl(dbUrl);
-        datasource.setUsername(username);
-        datasource.setPassword(password);
-        datasource.setDriverClassName(driverClassName);
-        datasource.setInitialSize(initialSize);
-        datasource.setMinIdle(minIdle);
-        datasource.setMaxActive(maxActive);
-        datasource.setMaxWait(maxWait);
-        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
-        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
-        datasource.setValidationQuery(validationQuery);
-        datasource.setTestWhileIdle(testWhileIdle);
-        datasource.setTestOnBorrow(testOnBorrow);
-        datasource.setTestOnReturn(testOnReturn);
-        datasource.setPoolPreparedStatements(poolPreparedStatements);
-        logger.info("Database connection address information(数据库连接地址信息)=" + dbUrl);
-        return datasource;
+    private DataSource mysqlDataSource() {
+        return DataSourceUtils.buildDataSource(null, null, null);
     }
 
     @Bean(name = "dataSource")
+    @Conditional(DataSourceCondition.class)
     public DynamicDataSource mutiDataSource() {
-
-        DruidDataSource hiveDataSource = hiveDataSource();
-        DruidDataSource mysqlDataSource = mysqlDataSource();
-
-        try {
-            hiveDataSource.init();
-            mysqlDataSource.init();
-        } catch (SQLException sql) {
-            logger.error("连接数据库失败：", sql);
-        }
-
+        DataSource hiveDataSource = hiveDataSource();
+        DataSource mysqlDataSource = mysqlDataSource();
         DynamicDataSource dynamicDataSource = new DynamicDataSource();
         HashMap<Object, Object> hashMap = new HashMap<>();
         hashMap.put(DSEnum.FIRST_DATA_SOURCE, hiveDataSource);
@@ -152,11 +83,5 @@ public class LinkisMybatisConfig {
         dynamicDataSource.setTargetDataSources(hashMap);
         dynamicDataSource.setDefaultTargetDataSource(mysqlDataSource);
         return dynamicDataSource;
-    }
-
-    @Primary
-    public PlatformTransactionManager annotationDrivenTransactionManager(
-            @Qualifier("dataSource") DynamicDataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
     }
 }

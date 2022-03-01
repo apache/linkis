6fbf00b4267655ebb43a0896ffc2859860864e2e
peacewong <wpeace1212@gmail.com>
2022-03-01 11:00:47 +0800

    Merge code from upstream master branch

--- linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-authentication/src/main/java/org/apache/linkis/gateway/authentication/exception/TokenAuthException.java
+++ linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-authentication/src/main/java/org/apache/linkis/gateway/authentication/exception/TokenAuthException.java
@@ -15,20 +15,22 @@
  * limitations under the License.
  */
 
--- ----------------------------
--- Table structure for linkis_mg_gateway_auth_token
--- ----------------------------
-DROP TABLE IF EXISTS `linkis_mg_gateway_auth_token`;
-CREATE TABLE `linkis_mg_gateway_auth_token` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `token_name` varchar(128) NOT NULL,
-  `legal_users` text,
-  `legal_hosts` text,
-  `business_owner` varchar(32),
-  `create_time` DATE DEFAULT NULL,
-  `update_time` DATE DEFAULT NULL,
-  `elapse_day` BIGINT DEFAULT NULL,
-  `update_by` varchar(32),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `token_name` (`token_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+package org.apache.linkis.gateway.authentication.exception;
+
+import org.apache.linkis.common.exception.ErrorException;
+import org.apache.linkis.common.exception.ExceptionLevel;
+
+public class TokenAuthException extends ErrorException {
+    public TokenAuthException(int errCode, String desc) {
+        super(errCode, desc);
+    }
+
+    public TokenAuthException(int errCode, String desc, String ip, int port, String serviceKind) {
+        super(errCode, desc, ip, port, serviceKind);
+    }
+
+    @Override
+    public ExceptionLevel getLevel() {
+        return super.getLevel();
+    }
+}

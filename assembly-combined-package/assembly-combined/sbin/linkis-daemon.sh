526c5b32b302f0ec1469aa2269b6a58f672b680f
peacewong <wpeace1212@gmail.com>
2022-03-05 16:22:12 +0800

    optimize license

--- assembly-combined-package/assembly-combined/sbin/linkis-daemon.sh
+++ assembly-combined-package/assembly-combined/sbin/linkis-daemon.sh
@@ -114,7 +114,7 @@ function stop()
       if [[ -z "${pid}" ]]; then
         echo "server $SERVER_NAME is not running"
       else
-        wait_for_server_to_die $pid 300
+        wait_for_server_to_die $pid 40
         $(rm -f ${SERVER_PID})
         echo "server $SERVER_NAME is stopped."
       fi

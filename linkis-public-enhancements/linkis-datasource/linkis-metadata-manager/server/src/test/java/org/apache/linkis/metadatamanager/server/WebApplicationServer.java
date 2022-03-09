216d0eccb40fd5d72959513e8abb6da386007472
xiaojie19852006 <xiaojie19852006@163.com>
2022-03-09 10:36:54 +0800

    add unit test for datasource metadata server

---
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata-manager/server/src/test/java/org/apache/linkis/metadatamanager/server/WebApplicationServer.java
@@ -0,0 +1,11 @@
+package org.apache.linkis.metadatamanager.server;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ServletComponentScan
+@ComponentScan
+public class WebApplicationServer {
+}

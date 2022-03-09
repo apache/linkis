9c7252581a8965702743b479dccf69f11da51271
casionone <casionone@gmail.com>
2022-03-09 17:53:50 +0800

    optimize datasource client unnecessary parameters

--- linkis-public-enhancements/linkis-datasource/linkis-datasource-client/src/main/scala/org/apache/linkis/datasource/client/AbstractRemoteClient.scala
+++ linkis-public-enhancements/linkis-datasource/linkis-datasource-client/src/main/scala/org/apache/linkis/datasource/client/AbstractRemoteClient.scala
@@ -22,7 +22,7 @@ import org.apache.linkis.httpclient.request.Action
 import org.apache.linkis.httpclient.response.Result
 
 abstract class AbstractRemoteClient extends RemoteClient {
-  protected val dwsHttpClient:DWSHttpClient
+  protected val dwsHttpClient: DWSHttpClient
 
   override def execute(action: Action): Result = action match {
     case action: Action => dwsHttpClient.execute(action)

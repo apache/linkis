dd2c0e588f1eb298712ceb5827035e53e95b6d07
Casion <casionone@gmail.com>
2022-03-24 10:55:05 +0800

    [Web] FAQ page redirect to "https://linkis.apache.org/zh-CN/faq/main/ (#1786)
    * FAQ page redirect to "https://linkis.apache.org/zh-CN/faq/main/
    
    * code format

--- web/src/apps/linkis/module/FAQ/index.vue
+++ web/src/apps/linkis/module/FAQ/index.vue
@@ -5,16 +5,16 @@
   ~ The ASF licenses this file to You under the Apache License, Version 2.0
   ~ (the "License"); you may not use this file except in compliance with
   ~ the License.  You may obtain a copy of the License at
-  ~ 
+  ~
   ~   http://www.apache.org/licenses/LICENSE-2.0
-  ~ 
+  ~
   ~ Unless required by applicable law or agreed to in writing, software
   ~ distributed under the License is distributed on an "AS IS" BASIS,
   ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   ~ See the License for the specific language governing permissions and
   ~ limitations under the License.
   -->
-  
+
 <template>
   <div>
     <iframe
@@ -64,7 +64,8 @@ export default {
       this.height = this.$parent.$el.clientHeight - 168
     },
     linkTo() {
-      util.windowOpen(this.getFAQUrl())
+      const faqUrl="https://linkis.apache.org/zh-CN/faq/main/"
+      util.windowOpen(faqUrl)
     },
   },
 };

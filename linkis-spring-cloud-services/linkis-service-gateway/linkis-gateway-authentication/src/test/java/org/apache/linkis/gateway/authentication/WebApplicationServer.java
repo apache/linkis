ae48f113c86883f36d8a06bd6446c7ad680ea6fd
Casion <casionone@gmail.com>
2022-04-08 14:34:28 +0800

    add junit test for gateway-authentication (#1922)
    * optimize of gateway token dao
    
    * add junit test for gateway-authentication
    
    * code format

--- linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-authentication/src/test/java/org/apache/linkis/gateway/authentication/WebApplicationServer.java
+++ linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-authentication/src/test/java/org/apache/linkis/gateway/authentication/WebApplicationServer.java
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,22 +15,20 @@
  * limitations under the License.
  */
 
-package org.apache.linkis.gateway.authentication.dao;
+package org.apache.linkis.gateway.authentication;
 
-import org.apache.linkis.gateway.authentication.entity.TokenEntity;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
-import org.apache.ibatis.annotations.Param;
+@EnableAutoConfiguration
+@ServletComponentScan
+@ComponentScan
+public class WebApplicationServer extends SpringBootServletInitializer {
 
-import java.util.List;
-
-public interface TokenDao {
-    Boolean insertToken(TokenEntity token); // TODO
-
-    Boolean updateToken(TokenEntity token); // TODO
-
-    Boolean removeToken(TokenEntity token); // TODO
-
-    TokenEntity selectTokenByName(@Param("tokenName") String tokenName);
-
-    List<TokenEntity> getAllTokens();
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(WebApplicationServer.class).run(args);
+    }
 }

c64d91e3e0c2d785369045995c4c2ad055caab16
peacewong <wpeace1212@gmail.com>
2022-04-12 20:21:53 +0800

    add cs-client module log

--- linkis-public-enhancements/linkis-context-service/linkis-cs-client/src/main/java/org/apache/linkis/cs/client/service/CSVariableService.java
+++ linkis-public-enhancements/linkis-context-service/linkis-cs-client/src/main/java/org/apache/linkis/cs/client/service/CSVariableService.java
@@ -52,6 +52,7 @@ public class CSVariableService implements VariableService {
             throws CSErrorException {
         List<LinkisVariable> rsList = new ArrayList<>();
         if (StringUtils.isBlank(contextIDStr) || StringUtils.isBlank(nodeName)) {
+            logger.warn("contextIDStr or nodeName cannot null");
             return rsList;
         }
         try {
@@ -60,6 +61,11 @@ public class CSVariableService implements VariableService {
                 rsList =
                         searchService.searchUpstreamContext(
                                 contextID, nodeName, Integer.MAX_VALUE, LinkisVariable.class);
+                logger.info(
+                        "contextID {} and nodeName {} succeed to getUpstreamVariables size {}",
+                        contextID.getContextId(),
+                        nodeName,
+                        rsList.size());
             }
             return rsList;
         } catch (Throwable e) {
@@ -81,6 +87,11 @@ public class CSVariableService implements VariableService {
             ContextValue contextValue = new CommonContextValue();
             contextValue.setValue(linkisVariable);
             contextClient.update(contextID, contextKey, contextValue);
+            logger.info(
+                    "contextID {} and contextKeyStr {} succeed to putVariable {}",
+                    contextID.getContextId(),
+                    contextKeyStr,
+                    linkisVariable.getValue());
         } catch (ErrorException e) {
             logger.error("Deserialize error. e ");
             throw new CSErrorException(ErrorCode.DESERIALIZE_ERROR, "Deserialize error. e : ", e);

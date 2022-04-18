58293e274218f39a99d7136cce04cbbf1b212d5b
peacewong <wpeace1212@gmail.com>
2022-04-18 17:03:02 +0800

    optimize import

--- linkis-public-enhancements/linkis-publicservice/linkis-udf/linkis-udf-service/src/main/java/org/apache/linkis/udf/service/impl/UDFTreeServiceImpl.java
+++ linkis-public-enhancements/linkis-publicservice/linkis-udf/linkis-udf-service/src/main/java/org/apache/linkis/udf/service/impl/UDFTreeServiceImpl.java
@@ -35,16 +35,9 @@ import com.google.common.collect.Iterables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import static org.apache.linkis.udf.utils.ConstantVar.BDP_USER;
-import static org.apache.linkis.udf.utils.ConstantVar.EXPIRE_USER;
-import static org.apache.linkis.udf.utils.ConstantVar.SHARE_USER;
-import static org.apache.linkis.udf.utils.ConstantVar.SYS_USER;
+import static org.apache.linkis.udf.utils.ConstantVar.*;
 
 @Service
 public class UDFTreeServiceImpl implements UDFTreeService {

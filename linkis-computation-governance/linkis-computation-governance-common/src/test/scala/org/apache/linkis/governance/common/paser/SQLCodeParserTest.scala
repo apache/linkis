a564988d8e328af1d27aba84c98fd287efb0eba9
leo_jie <weixiao.me@aliyun.com>
2022-04-27 17:34:32 +0800

    [Feature-2043] Cancel useless tests.

--- linkis-computation-governance/linkis-computation-governance-common/src/test/scala/org/apache/linkis/governance/common/paser/SQLCodeParserTest.scala
+++ linkis-computation-governance/linkis-computation-governance-common/src/test/scala/org/apache/linkis/governance/common/paser/SQLCodeParserTest.scala
@@ -20,8 +20,6 @@ package org.apache.linkis.governance.common.paser
 import org.junit.jupiter.api.{DisplayName, Test}
 import org.junit.jupiter.api.Assertions.assertTrue
 
-import java.util.regex.Pattern
-
 class SQLCodeParserTest {
 
   @Test
@@ -39,33 +37,4 @@ class SQLCodeParserTest {
     assertTrue(strings.length == 2)
   }
 
-  @Test
-  @DisplayName("testParseSql")
-  def testParseSql(): Unit = {
-    val parser = new SQLCodeParser
-    val sqlString: String =
-      """
-        |-- select device_id, client_id, stats_uv_day,
-        |-- stats_uv_week,
-        |-- stats_uv_month
-        |-- from kbase.test1 limit 10;
-        |-- select device_id,
-        |-- stats_uv
-        |-- from kbase.test1 limit 10;
-        |select *
-        |-- 这是一条注释
-        |from test.test_table3
-        |limit 10;
-        |select * from test.leo_test
-        |-- 这是注释;
-        |/** 这也是注释 **/
-        |limit 10
-        |""".stripMargin
-    // sqlString = "select * from test.leo_test"
-    val p = Pattern.compile("(?ms)('(?:''|[^'])*')|--.*?$|/\\*.*?\\*/")
-    // val p = Pattern.compile("(?ms)('(?:''|[^'])*')|--.*?$|//.*?$|/\\*.*?\\*/|#.*?$|")
-    // val result = p.matcher(sqlString).replaceAll("$1")
-    val strings = parser.parse(sqlString)
-    assertTrue(strings.length == 2)
-  }
 }

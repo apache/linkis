b209094634530d00225d15532bd043354fbf2fd2
weixiao <leojie1314@gmail.com>
2022-05-24 22:00:17 +0800

    [feture-2167] File type variable configuration in FileSource.scala (#2168)
    * [feture-2167] File type variable configuration in FileSource.scala
    
    * [feture-2167] File type variable configuration in FileSource.scala

--- linkis-commons/linkis-storage/src/main/scala/org/apache/linkis/storage/conf/LinkisStorageConf.scala
+++ linkis-commons/linkis-storage/src/main/scala/org/apache/linkis/storage/conf/LinkisStorageConf.scala
@@ -17,10 +17,13 @@
 
 package org.apache.linkis.storage.conf
 
+import org.apache.commons.lang.StringUtils
 import org.apache.linkis.common.conf.{ByteType, CommonVars}
 import org.apache.linkis.common.utils.ByteTimeUtils
 
 object LinkisStorageConf {
+  private val CONF_LOCK = new Object()
+
   val HDFS_FILE_SYSTEM_REST_ERRS: String =
     CommonVars.apply(
       "wds.linkis.hdfs.rest.errs",
@@ -30,5 +33,25 @@ object LinkisStorageConf {
   val ROW_BYTE_MAX_LEN_STR = CommonVars("wds.linkis.resultset.row.max.str", "10m").getValue
 
   val ROW_BYTE_MAX_LEN = ByteTimeUtils.byteStringAsBytes(ROW_BYTE_MAX_LEN_STR)
+
+  val FILE_TYPE = CommonVars("wds.linkis.storage.file.type", "dolphin,sql,scala,py,hql,python,out,log,text,sh,jdbc,ngql,psql,fql").getValue
+
+  private var fileTypeArr: Array[String] = null
+
+  private def fileTypeArrParser(fileType: String): Array[String] = {
+    if (StringUtils.isBlank(fileType)) Array()
+    else fileType.split(",")
+  }
+
+  def getFileTypeArr: Array[String] = {
+    if (fileTypeArr == null) {
+      CONF_LOCK.synchronized {
+        if (fileTypeArr == null) {
+          fileTypeArr = fileTypeArrParser(FILE_TYPE)
+        }
+      }
+    }
+    fileTypeArr
+  }
 
 }

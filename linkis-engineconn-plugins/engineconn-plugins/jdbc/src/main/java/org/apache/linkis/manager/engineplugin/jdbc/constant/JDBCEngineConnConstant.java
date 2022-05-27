107a7b1dbfc0d5d9184a3ba6924290e5db052ddd
CCweixiao <leojie1314@gmail.com>
2022-05-27 19:16:48 +0800

    [feture-2140] Unify database connection parameters in JDBC engine and extract variables.

---
+++ linkis-engineconn-plugins/engineconn-plugins/jdbc/src/main/java/org/apache/linkis/manager/engineplugin/jdbc/constant/JDBCEngineConnConstant.java
@@ -0,0 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.linkis.manager.engineplugin.jdbc.constant;
+
+public class JDBCEngineConnConstant {
+    public static final String JDBC_URL = "wds.linkis.jdbc.connect.url";
+    public static final String JDBC_USERNAME = "wds.linkis.jdbc.username";
+    public static final String JDBC_PASSWORD = "wds.linkis.jdbc.password";
+    public static final String JDBC_AUTH_TYPE = "wds.linkis.jdbc.auth.type";
+    public static final String JDBC_KERBEROS_AUTH_TYPE_PRINCIPAL = "wds.linkis.jdbc.principal";
+    public static final String JDBC_KERBEROS_AUTH_TYPE_KEYTAB_LOCATION = "wds.linkis.jdbc.keytab.location";
+    public static final String JDBC_PROXY_USER_PROPERTY = "wds.linkis.jdbc.proxy.user.property";
+    public static final String JDBC_PROXY_USER = "wds.linkis.jdbc.proxy.user";
+}

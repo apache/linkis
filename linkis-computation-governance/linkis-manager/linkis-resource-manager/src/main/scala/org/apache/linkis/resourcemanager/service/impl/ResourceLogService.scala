6ef9fc9c1189ee59584ea317c1369e2e77249647
peacewong <wpeace1212@gmail.com>
2022-05-31 11:33:12 +0800

    LinkisManager record table records ec information  (#2151)
    * add ec info record feature
    
    * optimize ec log from tail to read
    
    * add ec resource info action
    
    * use spotless reformat code
    
    * optimize code
    
    * optimize code
    
    * add watermark switch
    
    * update ec info ddl
    
    * update get page size
    
    * remove reset creator
    
    * optimize read log operator support from tail read
    
    * modify ddl

--- linkis-computation-governance/linkis-manager/linkis-resource-manager/src/main/scala/org/apache/linkis/resourcemanager/service/impl/ResourceLogService.scala
+++ linkis-computation-governance/linkis-manager/linkis-resource-manager/src/main/scala/org/apache/linkis/resourcemanager/service/impl/ResourceLogService.scala
@@ -14,114 +14,105 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package org.apache.linkis.resourcemanager.service.impl
 
+import org.apache.commons.lang.time.DateFormatUtils
 import org.apache.linkis.common.utils.{Logging, Utils}
-import org.apache.linkis.manager.common.entity.persistence.PersistenceResourceActionRecord
-import org.apache.linkis.manager.common.entity.resource.{Resource, ResourceActionRecord, ResourceType}
-import org.apache.linkis.manager.common.exception.ResourceWarnException
-import org.apache.linkis.manager.common.utils.ResourceUtils
-import org.apache.linkis.manager.label.entity.{CombinedLabel, Label}
+import org.apache.linkis.governance.common.utils.ECPathUtils
+import org.apache.linkis.manager.common.entity.persistence.ECResourceInfoRecord
+import org.apache.linkis.manager.common.entity.resource.Resource
+import org.apache.linkis.manager.dao.ECResourceRecordMapper
+import org.apache.linkis.manager.label.entity.CombinedLabel
 import org.apache.linkis.manager.label.entity.em.EMInstanceLabel
 import org.apache.linkis.manager.label.entity.engine.EngineInstanceLabel
-import org.apache.linkis.manager.persistence.ResourceManagerPersistence
-import org.apache.linkis.resourcemanager.service.LabelResourceService
+import org.apache.linkis.resourcemanager.domain.RMLabelContainer
 import org.apache.linkis.resourcemanager.utils.RMUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
+import java.io.File
+import java.util.Date
+
 @Component
-case class ResourceLogService() extends Logging{
+class ResourceLogService extends Logging {
+
 
   @Autowired
-  var labelResourceService: LabelResourceService = _
-
-  @Autowired
-  var resourceManagerPersistence: ResourceManagerPersistence = _
+  private var ecResourceRecordMapper: ECResourceRecordMapper = _
 
 
-
-  private def printLog(changeType: String, status: String, engineLabel: EngineInstanceLabel = null, ecmLabel: EMInstanceLabel = null): String = {
+  private def printLog(changeType: String, resource: Resource, status: String, engineLabel: EngineInstanceLabel = null, ecmLabel: EMInstanceLabel = null): String = {
     val logString = new StringBuilder(changeType + " ")
     logString ++= (status + ", ")
-    if (engineLabel != null) {
-      val engineResource = labelResourceService.getLabelResource(engineLabel)
-      var usedResource = Resource.initResource(ResourceType.Default)
-      if (engineResource != null && engineResource.getUsedResource != null) {
-        usedResource = engineResource.getUsedResource
-      }
+    if (engineLabel != null && resource != null) {
       logString ++= ("engine current resource:")
       logString ++= (engineLabel.getServiceInstance.getInstance)
-      logString ++= (usedResource.toJson + " ")
+      logString ++= (resource.toJson + " ")
     }
-    if (ecmLabel != null) {
-      val ecmResource = labelResourceService.getLabelResource(ecmLabel)
-      var usedResource = Resource.initResource(ResourceType.Default)
-      if (ecmResource != null && ecmResource.getUsedResource != null) {
-        usedResource = ecmResource.getUsedResource
-      }
+    if (ecmLabel != null & resource != null) {
       logString ++= ("ecm current resource:")
       logString ++= (ecmLabel.getServiceInstance.getInstance)
-      logString ++= (usedResource.toJson + " ")
+      logString ++= (resource.toJson + " ")
     }
     logString.toString()
   }
 
-  def failed(changeType: String, engineLabel: EngineInstanceLabel = null, ecmLabel: EMInstanceLabel = null, exception: Exception = null): Unit = {
+  def failed(changeType: String, resource: Resource, engineLabel: EngineInstanceLabel = null, ecmLabel: EMInstanceLabel = null, exception: Exception = null): Unit = Utils.tryQuietly {
     if (changeType != null) {
       val log: String = changeType match {
         case ChangeType.ENGINE_INIT => {
-          printLog(changeType, ChangeType.FAILED, engineLabel, ecmLabel)
+          printLog(changeType, resource, ChangeType.FAILED, engineLabel, ecmLabel)
         }
         case ChangeType.ENGINE_CLEAR => {
-          printLog(changeType, ChangeType.FAILED, engineLabel, ecmLabel)
+          printLog(changeType, resource, ChangeType.FAILED, engineLabel, ecmLabel)
         }
         case ChangeType.ECM_INIT => {
-          printLog(changeType, ChangeType.FAILED, null, ecmLabel)
+          printLog(changeType, resource, ChangeType.FAILED, null, ecmLabel)
         }
         case ChangeType.ECM_CLEAR => {
-          printLog(changeType, ChangeType.FAILED, null, ecmLabel)
+          printLog(changeType, resource, ChangeType.FAILED, null, ecmLabel)
         }
         case ChangeType.ECM_RESOURCE_ADD => {
-          printLog(changeType, ChangeType.FAILED, engineLabel, ecmLabel)
+          printLog(changeType, resource, ChangeType.FAILED, engineLabel, ecmLabel)
         }
         case ChangeType.ECM_Resource_MINUS => {
-          printLog(changeType, ChangeType.FAILED, engineLabel, ecmLabel)
+          printLog(changeType, resource, ChangeType.FAILED, engineLabel, ecmLabel)
         }
         case _ => " "
       }
       if (exception != null) {
-        error(log, exception)
+        logger.error(log, exception)
       } else {
-        error(log)
+        logger.error(log)
       }
     }
   }
-  def success(changeType: String, engineLabel: EngineInstanceLabel = null, ecmLabel: EMInstanceLabel = null): Unit = {
+
+  def success(changeType: String, resource: Resource, engineLabel: EngineInstanceLabel = null, ecmLabel: EMInstanceLabel = null): Unit = Utils.tryQuietly {
     if (changeType != null) {
       val log: String = changeType match {
         case ChangeType.ENGINE_INIT => {
-          printLog(changeType, ChangeType.SUCCESS, engineLabel, ecmLabel)
+          printLog(changeType, resource, ChangeType.SUCCESS, engineLabel, ecmLabel)
         }
         case ChangeType.ENGINE_CLEAR => {
-          printLog(changeType, ChangeType.SUCCESS, engineLabel, ecmLabel)
+          printLog(changeType, resource, ChangeType.SUCCESS, engineLabel, ecmLabel)
         }
         case ChangeType.ECM_INIT => {
-          printLog(changeType, ChangeType.SUCCESS, null, ecmLabel)
+          printLog(changeType, resource, ChangeType.SUCCESS, null, ecmLabel)
         }
         case ChangeType.ECM_CLEAR => {
-          printLog(changeType, ChangeType.SUCCESS, null, ecmLabel)
+          printLog(changeType, resource, ChangeType.SUCCESS, null, ecmLabel)
         }
         case ChangeType.ECM_RESOURCE_ADD => {
-          printLog(changeType, ChangeType.SUCCESS, engineLabel, ecmLabel)
+          printLog(changeType, resource, ChangeType.SUCCESS, engineLabel, ecmLabel)
         }
         case ChangeType.ECM_Resource_MINUS => {
-          printLog(changeType, ChangeType.SUCCESS, engineLabel, ecmLabel)
+          printLog(changeType, resource, ChangeType.SUCCESS, engineLabel, ecmLabel)
         }
         case _ => " "
       }
-      info(log)
+      logger.info(log)
     }
   }
 
@@ -135,80 +126,57 @@ case class ResourceLogService() extends Logging{
 
   def printNode(nodeLabel: EngineInstanceLabel, source: CombinedLabel): Unit = {
     val log = s"${nodeLabel.getInstance()}\t${source.getStringValue}"
-    info(log)
+    logger.info(log)
   }
 
-  def deserialize(persistenceResourceActionRecord: PersistenceResourceActionRecord): ResourceActionRecord = {
-    val resourceActionRecord = new ResourceActionRecord
-    resourceActionRecord.setId(persistenceResourceActionRecord.getId)
-    resourceActionRecord.setLabelValue(persistenceResourceActionRecord.getLabelValue)
-    resourceActionRecord.setTicketId(persistenceResourceActionRecord.getTicketId)
-    resourceActionRecord.setRequestTimes(persistenceResourceActionRecord.getRequestTimes)
-    resourceActionRecord.setRequestResourceAll(ResourceUtils.deserializeResource(persistenceResourceActionRecord.getRequestResourceAll))
-    resourceActionRecord.setUsedTimes(persistenceResourceActionRecord.getUsedTimes)
-    resourceActionRecord.setUsedResourceAll(ResourceUtils.deserializeResource(persistenceResourceActionRecord.getUsedResourceAll))
-    resourceActionRecord.setReleaseTimes(persistenceResourceActionRecord.getReleaseTimes)
-    resourceActionRecord.setReleaseResourceAll(ResourceUtils.deserializeResource(persistenceResourceActionRecord.getReleaseResourceAll))
-    resourceActionRecord.setCreateTime(persistenceResourceActionRecord.getCreateTime)
-    resourceActionRecord.setUpdateTime(persistenceResourceActionRecord.getUpdateTime)
-    resourceActionRecord
-  }
-
-
-  def serialize(resourceActionRecord: ResourceActionRecord): PersistenceResourceActionRecord = {
-    val persistenceResourceActionRecord = new PersistenceResourceActionRecord
-    persistenceResourceActionRecord.setId(resourceActionRecord.getId)
-    persistenceResourceActionRecord.setLabelValue(resourceActionRecord.getLabelValue)
-    persistenceResourceActionRecord.setTicketId(resourceActionRecord.getTicketId)
-    persistenceResourceActionRecord.setRequestTimes(resourceActionRecord.getRequestTimes)
-    persistenceResourceActionRecord.setRequestResourceAll(ResourceUtils.serializeResource(resourceActionRecord.getRequestResourceAll))
-    persistenceResourceActionRecord.setUsedTimes(resourceActionRecord.getUsedTimes)
-    persistenceResourceActionRecord.setUsedResourceAll(ResourceUtils.serializeResource(resourceActionRecord.getUsedResourceAll))
-    persistenceResourceActionRecord.setReleaseTimes(resourceActionRecord.getReleaseTimes)
-    persistenceResourceActionRecord.setReleaseResourceAll(ResourceUtils.serializeResource(resourceActionRecord.getReleaseResourceAll))
-    persistenceResourceActionRecord.setCreateTime(resourceActionRecord.getCreateTime)
-    persistenceResourceActionRecord.setUpdateTime(resourceActionRecord.getUpdateTime)
-    persistenceResourceActionRecord
-  }
-
-  def recordUserResourceAction(userCreatorEngineType: CombinedLabel, ticketId: String, changeType: String, resource: Resource): Unit = {
-    if (RMUtils.RM_RESOURCE_ACTION_RECORD.getValue) {
-      var persistenceResourceActionRecord = resourceManagerPersistence.getResourceActionRecord(ticketId)
-      if(persistenceResourceActionRecord == null) {
-        persistenceResourceActionRecord = new PersistenceResourceActionRecord(userCreatorEngineType.getStringValue, ticketId, resource)
-        Utils.tryQuietly(resourceManagerPersistence.insertResourceActionRecord(persistenceResourceActionRecord))
-      }
-      val resourceActionRecord = deserialize(persistenceResourceActionRecord)
-      changeType match {
-        case ChangeType.ENGINE_REQUEST => {
-          resourceActionRecord.setRequestTimes(resourceActionRecord.getRequestTimes + 1)
-          resourceActionRecord.setRequestResourceAll(resourceActionRecord.getRequestResourceAll + resource)
-        }
-        case ChangeType.ENGINE_INIT => {
-          resourceActionRecord.setUsedTimes(resourceActionRecord.getUsedTimes + 1)
-          resourceActionRecord.setUsedResourceAll(resourceActionRecord.getUsedResourceAll + resource)
-        }
-        case ChangeType.ENGINE_CLEAR => {
-          resourceActionRecord.setReleaseTimes(resourceActionRecord.getReleaseTimes + 1)
-          resourceActionRecord.setReleaseResourceAll(resourceActionRecord.getReleaseResourceAll + resource)
-        }
-      }
-      Utils.tryCatch(resourceManagerPersistence.updateResourceActionRecord(serialize(resourceActionRecord))) {
-        case exception: Exception => {
-          warn(s"ResourceActionRecord failed, ${userCreatorEngineType.getStringValue} with ticketId ${ticketId} after ${changeType}, ${resource}", exception)
-        }
-      }
+  def recordUserResourceAction(labelContainer: RMLabelContainer, ticketId: String, changeType: String, resource: Resource): Unit = if (RMUtils.RM_RESOURCE_ACTION_RECORD.getValue) Utils.tryAndWarn {
+    val userCreatorEngineType: CombinedLabel = labelContainer.getCombinedUserCreatorEngineTypeLabel
+    val engineInstanceLabel: EngineInstanceLabel = labelContainer.getEngineInstanceLabel
+    val eMInstanceLabel = labelContainer.getEMInstanceLabel
+    if (null == userCreatorEngineType) return
+    var ecResourceInfoRecord = ecResourceRecordMapper.getECResourceInfoRecord(ticketId)
+    if (ecResourceInfoRecord == null) {
+      val logDirSuffix = getECLogDirSuffix(labelContainer, ticketId)
+      val user = if (null != labelContainer.getUserCreatorLabel) labelContainer.getUserCreatorLabel.getUser else ""
+      ecResourceInfoRecord = new ECResourceInfoRecord(userCreatorEngineType.getStringValue, user, ticketId, resource, logDirSuffix)
+      ecResourceRecordMapper.insertECResourceInfoRecord(ecResourceInfoRecord)
     }
+    if (null != engineInstanceLabel) {
+      ecResourceInfoRecord.setServiceInstance(engineInstanceLabel.getInstance)
+    }
+    if (null != eMInstanceLabel) {
+      ecResourceInfoRecord.setEcmInstance(eMInstanceLabel.getInstance())
+    }
+    changeType match {
+      case ChangeType.ENGINE_REQUEST =>
+        ecResourceInfoRecord.setRequestTimes(ecResourceInfoRecord.getRequestTimes + 1)
+        if (null != resource) {
+          ecResourceInfoRecord.setRequestResource(resource.toJson)
+        }
+      case ChangeType.ENGINE_INIT =>
+        ecResourceInfoRecord.setUsedTimes(ecResourceInfoRecord.getUsedTimes + 1)
+        if (null != resource) {
+          ecResourceInfoRecord.setUsedResource(resource.toJson)
+        }
+        ecResourceInfoRecord.setUsedTime(new Date(System.currentTimeMillis));
+      case ChangeType.ENGINE_CLEAR =>
+        ecResourceInfoRecord.setReleaseTimes(ecResourceInfoRecord.getReleaseTimes + 1)
+        if (null != resource) {
+          ecResourceInfoRecord.setReleasedResource(resource.toJson)
+        }
+        ecResourceInfoRecord.setReleaseTime(new Date(System.currentTimeMillis))
+    }
+    ecResourceRecordMapper.updateECResourceInfoRecord(ecResourceInfoRecord)
   }
 
-  def recordUserResourceAction(userCreatorEngineType: CombinedLabel, engineInstanceLabel: EngineInstanceLabel, changeType: String, resource: Resource): Unit = {
-    if (RMUtils.RM_RESOURCE_ACTION_RECORD.getValue) {
-      val instanceResource = labelResourceService.getPersistenceResource(engineInstanceLabel)
-      if(instanceResource == null) {
-        throw new ResourceWarnException(11005, s"${engineInstanceLabel} resource is null, resource action will not be record")
-      }
-      recordUserResourceAction(userCreatorEngineType, instanceResource.getTicketId, changeType, resource)
+  def getECLogDirSuffix(labelContainer: RMLabelContainer, ticketId: String): String = {
+    val engineTypeLabel = labelContainer.getEngineTypeLabel
+    val userCreatorLabel = labelContainer.getUserCreatorLabel
+    if (null == engineTypeLabel || null == userCreatorLabel) {
+      return ""
     }
+    val suffix = ECPathUtils.getECWOrkDirPathSuffix(userCreatorLabel.getUser, ticketId, engineTypeLabel.getEngineType)
+    suffix + File.separator + "logs"
   }
 
 }

d08b38d0b9dc15fc09a2d7ccc6072db8bba3d370
peacewong <wpeace1212@gmail.com>
2022-06-08 15:14:27 +0800

    update 1.1.3 upgrade sql

--- db/upgrade/1.1.3_schema/mysql/linkis_ddl.sql
+++ db/upgrade/1.1.3_schema/mysql/linkis_ddl.sql
@@ -37,27 +37,33 @@ ALTER TABLE `linkis_ps_cs_context_listener` ADD COLUMN `create_time` datetime NO
 ALTER TABLE `linkis_ps_cs_context_listener` ADD COLUMN `access_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'last access time' AFTER `create_time`;
 
 
--- manager ec record
+alter table linkis_cg_manager_linkis_resources modify column max_resource varchar(1020) COLLATE utf8_bin DEFAULT NULL;
+alter table linkis_cg_manager_linkis_resources modify column min_resource varchar(1020) COLLATE utf8_bin DEFAULT NULL;
+alter table linkis_cg_manager_linkis_resources modify column used_resource varchar(1020) COLLATE utf8_bin DEFAULT NULL;
+alter table linkis_cg_manager_linkis_resources modify column left_resource varchar(1020) COLLATE utf8_bin DEFAULT NULL;
+alter table linkis_cg_manager_linkis_resources modify column expected_resource varchar(1020) COLLATE utf8_bin DEFAULT NULL;
+alter table linkis_cg_manager_linkis_resources modify column locked_resource varchar(1020) COLLATE utf8_bin DEFAULT NULL;
+
 DROP TABLE IF EXISTS `linkis_cg_ec_resource_info_record`;
 CREATE TABLE `linkis_cg_ec_resource_info_record` (
-	`id` INT(20) NOT NULL AUTO_INCREMENT,
-	`label_value` VARCHAR(255) NOT NULL,
-	`create_user` VARCHAR(128) NOT NULL,
-	`service_instance` varchar(128) COLLATE utf8_bin DEFAULT NULL,
-	`ecm_instance` varchar(128) COLLATE utf8_bin DEFAULT NULL,
-	`ticket_id` VARCHAR(100) NOT NULL,
-	`log_dir_suffix` varchar(128) COLLATE utf8_bin DEFAULT NULL,
-	`request_times` INT(8),
-	`request_resource` VARCHAR(255),
-	`used_times` INT(8),
-	`used_resource` VARCHAR(255),
-	`release_times` INT(8),
-	`released_resource` VARCHAR(255),
-	`release_time` datetime DEFAULT NULL,
-	`used_time` datetime DEFAULT NULL,
-	`create_time` datetime DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`),
-	KEY (`ticket_id`),
-	UNIQUE KEY `label_value_ticket_id` (`ticket_id`,`label_value`)
+    `id` INT(20) NOT NULL AUTO_INCREMENT,
+    `label_value` VARCHAR(255) NOT NULL COMMENT 'ec labels stringValue',
+    `create_user` VARCHAR(128) NOT NULL COMMENT 'ec create user',
+    `service_instance` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'ec instance info',
+    `ecm_instance` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'ecm instance info ',
+    `ticket_id` VARCHAR(100) NOT NULL COMMENT 'ec ticket id',
+    `log_dir_suffix` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'log path',
+    `request_times` INT(8) COMMENT 'resource request times',
+    `request_resource` VARCHAR(1020) COMMENT 'request resource',
+    `used_times` INT(8) COMMENT 'resource used times',
+    `used_resource` VARCHAR(1020) COMMENT 'used resource',
+    `release_times` INT(8) COMMENT 'resource released times',
+    `released_resource` VARCHAR(1020)  COMMENT 'released resource',
+    `release_time` datetime DEFAULT NULL COMMENT 'released time',
+    `used_time` datetime DEFAULT NULL COMMENT 'used time',
+    `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
+    PRIMARY KEY (`id`),
+    KEY (`ticket_id`),
+    UNIQUE KEY `label_value_ticket_id` (`ticket_id`,`label_value`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 

2e9813e0d7d9ad036d962ea707b88a624a67ba77
Leomax_Sun <282130830@qq.com>
2022-06-08 11:47:00 +0800

    Enable the prometheus for monitoring (#2069)
    * Add codecov git action to track test coverage metrics (#1945)
    
    * enable prometheus monitoring
    * Enable to specify the port range of engine conn(EC)
    * Enable prometheus in install script
    
    Co-authored-by: Casion <casionone@gmail.com>
    Co-authored-by: husofskyzy <15997168992@163.com>
    Co-authored-by: peacewong <wpeace1212@gmail.com>

--- assembly-combined-package/bin/install.sh
+++ assembly-combined-package/bin/install.sh
@@ -400,6 +400,18 @@ then
   sed -i ${txt}  "s#spring.server.port.*#spring.server.port=$CS_PORT#g" $cs_conf
 fi
 
+##Eanble prometheus for monitoring
+if [ "true" == "$PROMETHEUS_ENABLE" ]
+then
+  echo "prometheus is enabled"
+  sed -i ${txt} '$a \wds.linkis.prometheus.enable={{ PROMETHEUS_ENABLE }}' $LINKIS_HOME/conf/linkis.properties
+  sed -i ${txt} '$a \wds.linkis.server.user.restful.uri.pass.auth=/actuator/prometheus,' $LINKIS_HOME/conf/linkis.properties
+  sed -i ${txt}  '/eureka:/a \\  instance:\n    metadata-map:\n      prometheus.path: ${prometheus.path:${prometheus.endpoint}}' $LINKIS_HOME/conf/application-linkis.yml
+  sed -i ${txt}  's#include: refresh,info#include: refresh,info,health,metrics,prometheus#g' $LINKIS_HOME/conf/application-linkis.yml
+  sed -i ${txt} '/instance:/a \    metadata-map:\n      prometheus.path: ${prometheus.path:/actuator/prometheus}' $LINKIS_HOME/conf/application-eureka.yml
+  sed -i ${txt} '$a \\nmanagement:\n  endpoints:\n    web:\n      exposure:\n        include: refresh,info,health,metrics,prometheus' $LINKIS_HOME/conf/application-eureka.yml
+fi
+
 echo "preveliges linkis command shells"
 sudo chmod -R 777 $LINKIS_HOME/bin/*
 sudo chmod -R 777 $LINKIS_HOME/sbin/*

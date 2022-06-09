acb45f697ba134d056b7b6fdc98cff1f8757f7fe
peacewong <wpeace1212@gmail.com>
2022-06-09 20:16:16 +0800

    module optimize that remove bml-engine-hook module

--- linkis-computation-governance/linkis-engineconn/linkis-computation-engineconn/src/main/scala/org/apache/linkis/engineconn/computation/executor/bml/BmlHookDownloadException.scala
+++ linkis-computation-governance/linkis-engineconn/linkis-computation-engineconn/src/main/scala/org/apache/linkis/engineconn/computation/executor/bml/BmlHookDownloadException.scala
@@ -5,37 +5,18 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-package org.apache.linkis.bml.hook
 
-case class ResourceVersion(resourceId:String, version:String)
+package org.apache.linkis.engineconn.computation.executor.bml
 
+import org.apache.linkis.common.exception.ErrorException
 
-trait BmlResourceParser {
-  /**
-    * 通过传入的code
-    * @param code
-    * @return
-    */
-  def getResource(code:String):Array[ResourceVersion]
-}
-
-
-object DefaultBmlResourceParser extends BmlResourceParser{
-  /**
-    * 通过传入的code
-    *
-    * @param code
-    * @return
-    */
-  override def getResource(code: String): Array[ResourceVersion] = Array.empty
-}
+case class BmlHookDownloadException(errMsg: String) extends ErrorException(50046, errMsg)

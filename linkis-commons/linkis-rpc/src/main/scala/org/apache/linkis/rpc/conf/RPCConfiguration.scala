be897b56dad3fb207b519ec35a171d86c0099c8a
casionone <casionone@gmail.com>
2022-06-09 15:01:54 +0800

    for compatible with metadata module name refactoring

--- linkis-commons/linkis-rpc/src/main/scala/org/apache/linkis/rpc/conf/RPCConfiguration.scala
+++ linkis-commons/linkis-rpc/src/main/scala/org/apache/linkis/rpc/conf/RPCConfiguration.scala
@@ -40,6 +40,10 @@ object RPCConfiguration {
   val ENABLE_PUBLIC_SERVICE = CommonVars("wds.linkis.gateway.conf.enable.publicservice", true)
   val PUBLIC_SERVICE_APPLICATION_NAME = CommonVars("wds.linkis.gateway.conf.publicservice.name", "linkis-ps-publicservice")
   val PUBLIC_SERVICE_LIST = CommonVars("wds.linkis.gateway.conf.publicservice.list", "query,jobhistory,application,configuration,filesystem,udf,variable,microservice,errorcode,bml,datasource").getValue.split(",")
+
+  val METEDATAQUERY_SERVICE_APPLICATION_NAME = CommonVars("wds.linkis.gateway.conf.publicservice.name", "linkis-ps-metedataquery")
+  val METEDATAQUERY_SERVICE_LIST = CommonVars("wds.linkis.gateway.conf.metadataquery.list", "metadatamanager,metadataquery").getValue.split(",")
+
   val PUBLIC_SERVICE_APP_PREFIX = CommonVars("wds.linkis.gateway.conf.publicservice.name", "linkis-ps-").getValue
   val BDP_RPC_INSTANCE_ALIAS_SERVICE_REFRESH_INTERVAL = CommonVars("wds.linkis.rpc.instancealias.refresh.interval", new TimeType("3s"))
 

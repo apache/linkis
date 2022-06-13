89553f14add3b68ac9ff846c18adb1131ea85190
Jack Xu <xulingcom@126.com>
2022-06-13 17:47:52 +0800

    refactor: format some code and add test for LoopArray

--- linkis-commons/linkis-common/src/main/java/org/apache/linkis/common/utils/ByteTimeUtils.java
+++ linkis-commons/linkis-common/src/main/java/org/apache/linkis/common/utils/ByteTimeUtils.java
@@ -17,8 +17,6 @@
 
 package org.apache.linkis.common.utils;
 
-import com.google.common.collect.ImmutableMap;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -26,6 +24,8 @@ import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import com.google.common.collect.ImmutableMap;
 
 public class ByteTimeUtils {
 
@@ -38,7 +38,7 @@ public class ByteTimeUtils {
         return hash != Integer.MIN_VALUE ? Math.abs(hash) : 0;
     }
 
-    /*
+    /**
      * Delete a file or directory and its contents recursively.
      * Don't follow directories if they are symlinks.
      * Throws an exception if deletion is unsuccessful.

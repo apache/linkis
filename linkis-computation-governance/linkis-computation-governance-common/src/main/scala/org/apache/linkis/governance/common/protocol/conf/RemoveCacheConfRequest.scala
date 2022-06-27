9e2f989a97790030de0f10aa2c48f90299a8bc2f
peacewong <wpeace1212@gmail.com>
2022-06-27 18:00:16 +0800

    Optimized to automatically refresh all LinkisManager caches and add trino ec type (#2323)
    * Added trino engine type close #2217
    
    * Modify the configuration to clean up the broadcast message close #2077
    
    * fix build error

--- linkis-computation-governance/linkis-computation-governance-common/src/main/scala/org/apache/linkis/governance/common/protocol/conf/RemoveCacheConfRequest.scala
+++ linkis-computation-governance/linkis-computation-governance-common/src/main/scala/org/apache/linkis/governance/common/protocol/conf/RemoveCacheConfRequest.scala
@@ -5,42 +5,19 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
-package org.apache.linkis.manager.label.entity.engine
 
-object RunType extends Enumeration {
+package org.apache.linkis.governance.common.protocol.conf
 
-  type RunType = Value
-  val SQL = Value("sql")
-  val HIVE = Value("hql")
-  val SCALA = Value("scala")
-  val PYTHON = Value("python")
-  val JAVA = Value("java")
-  val PYSPARK = Value("py")
-  val R = Value("r")
-  val STORAGE = Value("out")
-  val SHELL = Value("shell")
-  val IO_FILE = Value("io_file")
-  val IO_HDFS = Value("io_hdfs")
-  val FPS = Value("fps")
-  val PIPELINE = Value("pipeline")
-  val JDBC = Value("jdbc")
-  val PRESTO_SQL = Value("psql")
-  val JAR = Value("jar")
-  val APPCONN = Value("appconn")
-  val FUNCTION_MDQ_TYPE = Value("function.mdq")
-  val ES_SQL = Value("essql")
-  val ES_JSON = Value("esjson")
+import org.apache.linkis.manager.label.entity.engine.{EngineTypeLabel, UserCreatorLabel}
+import org.apache.linkis.protocol.BroadcastProtocol
 
-  val TRINO_SQL = Value("tsql")
-
-}
+case class RemoveCacheConfRequest(userCreatorLabel: UserCreatorLabel, engineTypeLabel: EngineTypeLabel) extends BroadcastProtocol

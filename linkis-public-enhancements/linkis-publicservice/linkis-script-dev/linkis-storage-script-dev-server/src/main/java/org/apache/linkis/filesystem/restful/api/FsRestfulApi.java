1880569b7f7968dea7bc5f507134cd6a27ba6d19
hunter-cloud09 <393009865@qq.com>
2022-06-29 17:50:54 +0800

    [Feature] Automatically create workspace for newly added users

--- linkis-public-enhancements/linkis-publicservice/linkis-script-dev/linkis-storage-script-dev-server/src/main/java/org/apache/linkis/filesystem/restful/api/FsRestfulApi.java
+++ linkis-public-enhancements/linkis-publicservice/linkis-script-dev/linkis-storage-script-dev-server/src/main/java/org/apache/linkis/filesystem/restful/api/FsRestfulApi.java
@@ -104,8 +104,7 @@ public class FsRestfulApi {
         LOGGER.debug("workspacePath:" + workspacePath);
         LOGGER.debug("enginconnPath:" + enginconnPath);
         LOGGER.debug("adminUser:" + WorkSpaceConfiguration.FILESYSTEM_LOG_ADMIN.getValue());
-        return (requestPath.contains(workspacePath))
-                || (requestPath.contains(enginconnPath));
+        return (requestPath.contains(workspacePath)) || (requestPath.contains(enginconnPath));
     }
 
     @RequestMapping(path = "/getUserRootPath", method = RequestMethod.GET)
@@ -130,6 +129,7 @@ public class FsRestfulApi {
         FsPath fsPath = new FsPath(path);
         FileSystem fileSystem = fsService.getFileSystem(userName, fsPath);
         if (!fileSystem.exists(fsPath)) {
+
             if (FILESYSTEM_PATH_AUTO_CREATE.getValue()) {
                 try {
                     fileSystem.mkdirs(fsPath);

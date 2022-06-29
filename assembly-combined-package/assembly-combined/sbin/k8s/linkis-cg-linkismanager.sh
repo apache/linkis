403bcc1038a906a5cc5f376642f26eebbef32714
jack tao <7956214@qq.com>
2022-06-29 20:57:17 +0800

    Added k8S deployment file and Jenkins pipeline profile (#2342)
    * Added k8S deployment file and Jenkins pipeline profile
    
    * fix license
    
    * Create publish-docker.yaml
    
    * Update build.yml
    
    * Update build.yml
    
    * Update publish-docker.yaml
    
    * Update publish-docker.yaml
    
    * add metadata service and data source manager service
    remove node port
    add ingress
    add log volume
    change the imagePullPolicy to IfNotPresent
    add headless service
    
    * fix jenkinsfile
    
    * fix metadatamanager
    
    * Update Jenkinsfile
    
    * Update linkis.yaml
    
    * Update build.yml

---
+++ assembly-combined-package/assembly-combined/sbin/k8s/linkis-cg-linkismanager.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# description:  manager start cmd
+#
+# Modified for Linkis 1.3.0
+
+export SERVER_NAME=linkis-cg-linkismanager
+
+export LINKIS_COMMONS_LIB=/opt/linkis/public-module
+export SERVER_CONF_PATH=/opt/linkis/linkis-cg-linkismanager/conf
+export SERVER_LIB=/opt/linkis/linkis-cg-linkismanager/lib
+export LINKIS_LOG_DIR=/opt/linkis/linkis-cg-linkismanager/logs
+
+if [ ! -w "$LINKIS_LOG_DIR" ] ; then
+  mkdir -p "$LINKIS_LOG_DIR"
+fi
+
+export SERVER_JAVA_OPTS=" -DserviceName=$SERVER_NAME -Xmx1G -XX:+UseG1GC -Xloggc:$LINKIS_LOG_DIR/${SERVER_NAME}-gc.log $DEBUG_CMD"
+
+export SERVER_CLASS=org.apache.linkis.manager.am.LinkisManagerApplication
+
+export SERVER_CLASS_PATH=$SERVER_CONF_PATH:$LINKIS_COMMONS_LIB/*:$SERVER_LIB/*
+
+exec java $SERVER_JAVA_OPTS -cp $SERVER_CLASS_PATH $SERVER_CLASS

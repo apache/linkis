73bf56ee393d5b772ed2e5f3c48e761a9ac4fc0e
AaronLinOops <vboylin1987@gmail.com>
2022-07-07 10:14:34 +0800

    Evaluate project version automatic and load linkis image of this version

--- linkis-dist/helm/scripts/create-test-kind.sh
+++ linkis-dist/helm/scripts/create-test-kind.sh
@@ -16,21 +16,33 @@
 #
 
 WORK_DIR=`cd $(dirname $0); pwd -P`
+PROJECT_ROOT=${WORK_DIR}/../..
 RESOURCE_DIR=${WORK_DIR}/resources
 
+set -e
+
 KIND_CLUSTER_NAME=test-helm
+MYSQL_VERSION=5.7
+
+# evaluate project version
+PROJECT_VERSION=`cd ${PROJECT_ROOT} \
+   && MAVEN_OPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=WARN -Dorg.slf4j.simpleLogger.log.org.apache.maven.plugins.help=INFO" \
+   mvn help:evaluate -o -Dexpression=project.version | tail -1`
+echo "# Project version: ${PROJECT_VERSION}"
 
 # create kind cluster
+echo "# Creating KinD cluster ..."
 kind create cluster --name ${KIND_CLUSTER_NAME}
 
 # load images
-kind load docker-image linkis:1.3.0 --name ${KIND_CLUSTER_NAME}
-kind load docker-image mysql:5.7 --name ${KIND_CLUSTER_NAME}
+echo "# Loading images into KinD cluster ..."
+kind load docker-image linkis:${PROJECT_VERSION} --name ${KIND_CLUSTER_NAME}
+kind load docker-image mysql:${MYSQL_VERSION} --name ${KIND_CLUSTER_NAME}
 
 # deploy mysql
+echo "# Deploying MySQL ..."
 kubectl create ns mysql
 kubectl apply -n mysql -f ${RESOURCE_DIR}/mysql.yaml
 
-# install helm charts
 
 

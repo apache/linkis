beceab398fc5b2eebc833bd530c36b4f2040cb7c
Jack Xu <xulingcom@126.com>
2022-07-09 18:09:05 +0800

    refactor(linkis-scheduler): refactor some code (#2436)

--- linkis-commons/linkis-module/src/main/scala/org/apache/linkis/server/Knife4jConfig.scala
+++ linkis-commons/linkis-module/src/main/scala/org/apache/linkis/server/Knife4jConfig.scala
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.linkis.server
 
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j

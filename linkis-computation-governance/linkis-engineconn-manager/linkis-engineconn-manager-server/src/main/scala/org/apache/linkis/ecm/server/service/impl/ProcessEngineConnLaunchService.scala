4b1cb6eb11e0aa1bc311d7931c1d32595752201c
peacewong <wpeace1212@gmail.com>
2022-07-22 19:36:31 +0800

    Optimized engine startup to support more Retry scenarios close #2505

--- linkis-computation-governance/linkis-engineconn-manager/linkis-engineconn-manager-server/src/main/scala/org/apache/linkis/ecm/server/service/impl/ProcessEngineConnLaunchService.scala
+++ linkis-computation-governance/linkis-engineconn-manager/linkis-engineconn-manager-server/src/main/scala/org/apache/linkis/ecm/server/service/impl/ProcessEngineConnLaunchService.scala
@@ -36,6 +36,7 @@ import org.apache.linkis.manager.engineplugin.common.launch.entity.EngineConnLau
 import org.apache.linkis.rpc.Sender
 import org.apache.commons.io.IOUtils
 import org.apache.commons.lang3.exception.ExceptionUtils
+import org.apache.commons.lang3.StringUtils
 import org.apache.linkis.ecm.server.service.LocalDirsHandleService
 import org.apache.linkis.manager.label.utils.LabelUtil
 
@@ -64,6 +65,9 @@ abstract class ProcessEngineConnLaunchService extends AbstractEngineConnLaunchSe
           val canRetry = e match {
             case ecmError: ECMErrorException =>
               if (ECMErrorCode.EC_START_TIME_OUT == ecmError.getErrCode) {
+                true
+              } else if (StringUtils.isBlank(ecmError.getDesc)) {
+                logger.info("exception desc is null, can be retry")
                 true
               } else {
                 false

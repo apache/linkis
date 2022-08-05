de58e7fe1c130e4aaf63882761586a17a5e6e703
Knypys <sinkuo@qq.com>
2022-08-05 15:27:34 +0800

    remove unnecessary dependencies

--- linkis-computation-governance/linkis-client/linkis-computation-client/src/main/scala/org/apache/linkis/ujes/client/utils/UJESClientUtils.scala
+++ linkis-computation-governance/linkis-client/linkis-computation-client/src/main/scala/org/apache/linkis/ujes/client/utils/UJESClientUtils.scala
@@ -43,7 +43,7 @@ object UJESClientUtils {
     case "hql" => EngineType.HIVE.HQL
     case "shell" => EngineType.SHELL.SH
     case "python" => EngineType.PYTHON.PY
-    case "tql" => EngineType.TRINO.TSQL
+    case "tsql" => EngineType.TRINO.TSQL
     case _ => EngineType.SPARK.SQL
   }
 

c80f3d0911b8308d691560674fe7b9caee5aea5f
Zhen Wang <wangzhen@apache.org>
2022-08-19 20:38:32 +0800

    [LINKIS-2902] Add license header (#2903)

--- linkis-dist/package/db/upgrade/1.2.0_schema/mysql/linkis_dml.sql
+++ linkis-dist/package/db/upgrade/1.2.0_schema/mysql/linkis_dml.sql
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 UPDATE `linkis_ps_configuration_config_key` SET `default_value` = 'default' WHERE `key` = 'wds.linkis.rm.yarnqueue';
 UPDATE `linkis_ps_configuration_config_key` SET `description` = '范围：-1-10000，单位：个', `default_value` = '-1', `validate_range` = '[-1,10000]' WHERE `key` = 'mapred.reduce.tasks';
 DELETE FROM `linkis_ps_configuration_config_key` WHERE `key` IN ('dfs.block.size','hive.exec.reduce.bytes.per.reducer','wds.linkis.engineconn.max.free.time');

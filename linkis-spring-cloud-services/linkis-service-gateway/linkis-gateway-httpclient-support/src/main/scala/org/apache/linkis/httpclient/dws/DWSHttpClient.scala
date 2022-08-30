b93beeb1cbb70999810e20a155a4adc0fd917edd
binbinCheng <cheng741201916@163.com>
2022-08-30 16:25:18 +0800

    [linkis-gateway-httpclient-support] Modification of scala file floating red

--- linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-httpclient-support/src/main/scala/org/apache/linkis/httpclient/dws/DWSHttpClient.scala
+++ linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-httpclient-support/src/main/scala/org/apache/linkis/httpclient/dws/DWSHttpClient.scala
@@ -39,7 +39,7 @@ import org.apache.http.{HttpException, HttpResponse}
 
 import java.util
 
-import scala.collection.JavaConversions
+import scala.collection.{JavaConversions, JavaConverters}
 
 class DWSHttpClient(clientConfig: DWSClientConfig, clientName: String)
     extends AbstractHttpClient(clientConfig, clientName)
@@ -101,8 +101,9 @@ class DWSHttpClient(clientConfig: DWSClientConfig, clientName: String)
             transfer(value, map)
             value
           case list: util.List[util.Map[String, Object]] =>
-            val results = JavaConversions
-              .asScalaBuffer(list)
+            val results = JavaConverters
+              .asScalaBufferConverter(list)
+              .asScala
               .map { map =>
                 val value = clazz.getConstructor().newInstance().asInstanceOf[Result]
                 transfer(value, map)

e803bd69dd67e079e8e170bb2b41063b20806aab
binbinCheng <cheng741201916@163.com>
2022-08-30 16:06:57 +0800

    [linkis-gateway-httpclient-support] Modification of scala file floating red

--- linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-httpclient-support/src/main/scala/org/apache/linkis/httpclient/dws/DWSHttpClient.scala
+++ linkis-spring-cloud-services/linkis-service-gateway/linkis-gateway-httpclient-support/src/main/scala/org/apache/linkis/httpclient/dws/DWSHttpClient.scala
@@ -116,14 +116,14 @@ class DWSHttpClient(clientConfig: DWSClientConfig, clientName: String)
   }
 
   protected def deserializeResponseBody(responseBody: String): Any = {
-    if (responseBody.startsWith("{") && responseBody.endsWith("}"))
+    if (responseBody.startsWith("{") && responseBody.endsWith("}")) {
       DWSHttpClient.jacksonJson.readValue(responseBody, classOf[util.Map[String, Object]])
-    else if (responseBody.startsWith("[") && responseBody.endsWith("]"))
+    } else if (responseBody.startsWith("[") && responseBody.endsWith("]")) {
       DWSHttpClient.jacksonJson.readValue(
         responseBody,
         classOf[util.List[util.Map[String, Object]]]
       )
-    else if (StringUtils.isEmpty(responseBody)) new util.HashMap[String, Object]
+    } else if (StringUtils.isEmpty(responseBody)) new util.HashMap[String, Object]
     else if (responseBody.length > 200) throw new HttpException(responseBody.substring(0, 200))
     else throw new HttpException(responseBody)
   }

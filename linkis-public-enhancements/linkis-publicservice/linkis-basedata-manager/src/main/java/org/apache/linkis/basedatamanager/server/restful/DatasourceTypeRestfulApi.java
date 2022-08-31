0616c01f7b2622d27755a61ec0f2e30d25d7f97c
jacktao007 <7956214@qq.com>
2022-08-31 22:14:46 +0800

    feat(basedata-manager):  add swagger annotation

--- linkis-public-enhancements/linkis-publicservice/linkis-basedata-manager/src/main/java/org/apache/linkis/basedatamanager/server/restful/DatasourceTypeRestfulApi.java
+++ linkis-public-enhancements/linkis-publicservice/linkis-basedata-manager/src/main/java/org/apache/linkis/basedatamanager/server/restful/DatasourceTypeRestfulApi.java
@@ -17,12 +17,17 @@
 package org.apache.linkis.basedatamanager.server.restful;
 
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.apache.linkis.basedatamanager.server.domain.DatasourceTypeEntity;
 import org.apache.linkis.basedatamanager.server.service.DatasourceTypeService;
 import org.apache.linkis.server.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags="DatasourceTypeRestfulApi")
 @RestController
 @RequestMapping(path = "/basedata_manager/datasource_type")
 public class DatasourceTypeRestfulApi {
@@ -30,30 +35,52 @@ public class DatasourceTypeRestfulApi {
     @Autowired
     DatasourceTypeService datasourceTypeService;
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", dataType = "string", name = "searchName", value = ""),
+            @ApiImplicitParam(paramType = "query", dataType = "int", name = "currentPage", value = ""),
+            @ApiImplicitParam(paramType = "query", dataType = "int", name = "pageSize", value = "")
+    })
+    @ApiOperation(value = "list", notes = "get list data", httpMethod = "GET")
     @RequestMapping(path = "", method = RequestMethod.GET)
     public Message list(String searchName,Integer currentPage,Integer pageSize) {
         PageInfo pageList = datasourceTypeService.getListByPage(searchName,currentPage,pageSize);
         return Message.ok("").data("list", pageList);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "path", dataType = "long", name = "id", value = "")
+    })
+    @ApiOperation(value = "get", notes = "get data by id", httpMethod = "GET")
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public Message get(@PathVariable("id") Long id) {
         DatasourceTypeEntity datasourceType = datasourceTypeService.getById(id);
         return Message.ok("").data("item", datasourceType);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "body", dataType = "DatasourceTypeEntity", name = "datasourceType", value = "")
+    })
+    @ApiOperation(value = "add", notes = "add data", httpMethod = "POST")
     @RequestMapping(path = "", method = RequestMethod.POST)
     public Message add(@RequestBody DatasourceTypeEntity datasourceType) {
         boolean result = datasourceTypeService.save(datasourceType);
         return Message.ok("").data("result", result);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "path", dataType = "long", name = "id", value = "")
+    })
+    @ApiOperation(value = "remove", notes = "remove data by id", httpMethod = "DELETE")
     @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
     public Message remove(@PathVariable("id") Long id) {
         boolean result = datasourceTypeService.removeById(id);
         return Message.ok("").data("result", result);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "body", dataType = "DatasourceTypeEntity", name = "errorCode", value = "")
+    })
+    @ApiOperation(value = "update", notes = "update data", httpMethod = "PUT")
     @RequestMapping(path = "", method = RequestMethod.PUT)
     public Message update(@RequestBody DatasourceTypeEntity errorCode) {
         boolean result = datasourceTypeService.updateById(errorCode);

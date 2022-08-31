b583b2bf1e25e340076103bf15570711b534fab1
jacktao007 <7956214@qq.com>
2022-08-31 22:12:39 +0800

    feat(basedata-manager):  add swagger annotation

--- linkis-public-enhancements/linkis-publicservice/linkis-basedata-manager/src/main/java/org/apache/linkis/basedatamanager/server/restful/GatewayAuthTokenRestfulApi.java
+++ linkis-public-enhancements/linkis-publicservice/linkis-basedata-manager/src/main/java/org/apache/linkis/basedatamanager/server/restful/GatewayAuthTokenRestfulApi.java
@@ -17,6 +17,10 @@
 package org.apache.linkis.basedatamanager.server.restful;
 
 import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.apache.linkis.basedatamanager.server.domain.GatewayAuthTokenEntity;
 import org.apache.linkis.basedatamanager.server.service.GatewayAuthTokenService;
 import org.apache.linkis.server.Message;
@@ -26,36 +30,59 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 
+@Api(tags = "GatewayAuthTokenRestfulApi")
 @RestController
 @RequestMapping(path = "/basedata_manager/gateway_auth_token")
 public class GatewayAuthTokenRestfulApi {
     @Autowired
     GatewayAuthTokenService gatewayAuthTokenService;
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", dataType = "string", name = "searchName", value = ""),
+            @ApiImplicitParam(paramType = "query", dataType = "int", name = "currentPage", value = ""),
+            @ApiImplicitParam(paramType = "query", dataType = "int", name = "pageSize", value = "")
+    })
+    @ApiOperation(value = "list", notes = "get list data", httpMethod = "GET")
     @RequestMapping(path = "", method = RequestMethod.GET)
     public Message list(String searchName,Integer currentPage,Integer pageSize) {
         PageInfo pageList = gatewayAuthTokenService.getListByPage(searchName,currentPage,pageSize);
         return Message.ok("").data("list", pageList);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "path", dataType = "long", name = "id", value = "")
+    })
+    @ApiOperation(value = "get", notes = "get data by id", httpMethod = "GET")
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public Message get(@PathVariable("id") Long id) {
         GatewayAuthTokenEntity gatewayAuthToken = gatewayAuthTokenService.getById(id);
         return Message.ok("").data("item", gatewayAuthToken);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "body", dataType = "GatewayAuthTokenEntity", name = "gatewayAuthToken", value = "")
+    })
+    @ApiOperation(value = "add", notes = "add data", httpMethod = "POST")
     @RequestMapping(path = "", method = RequestMethod.POST)
     public Message add(@RequestBody GatewayAuthTokenEntity gatewayAuthToken) {
         boolean result = gatewayAuthTokenService.save(gatewayAuthToken);
         return Message.ok("").data("result", result);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "body", dataType = "GatewayAuthTokenEntity", name = "token", value = "")
+    })
+    @ApiOperation(value = "update", notes = "remove data by id", httpMethod = "PUT")
     @RequestMapping(path = "", method = RequestMethod.PUT)
     public Message update(@RequestBody GatewayAuthTokenEntity token) {
         boolean result = gatewayAuthTokenService.updateById(token);
         return Message.ok("").data("result", result);
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", dataType = "string", name = "tokenName", value = "")
+    })
+    @ApiOperation(value = "remove", notes = "update data", httpMethod = "DELETE")
     @RequestMapping(path = "", method = RequestMethod.DELETE)
     public Message remove(String tokenName) {
         HashMap columnMap = new HashMap<String,Object>();

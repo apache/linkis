cd29778e8b8280b20c293414d2ffd4fb188cb6ad
binbinCheng <cheng741201916@163.com>
2022-09-01 15:22:21 +0800

    [linkis-code-orchestrator] Modification of scala file floating red

--- linkis-orchestrator/linkis-code-orchestrator/src/main/scala/org/apache/linkis/orchestrator/code/plans/ast/CodeJob.scala
+++ linkis-orchestrator/linkis-code-orchestrator/src/main/scala/org/apache/linkis/orchestrator/code/plans/ast/CodeJob.scala
@@ -112,5 +112,5 @@ class CodeJob(private var parents: Array[Job], private var children: Array[Job])
     job
   }
 
-  override def toString = s"CodeJob($name, $submitUser, $codeLogicalUnit)"
+  override def toString: Unit = s"CodeJob($name, $submitUser, $codeLogicalUnit)"
 }

28deab2e77cd80d3b8445c97c0c701e92ed547a1
binbinCheng <cheng741201916@163.com>
2022-09-07 17:25:41 +0800

    InsLabelRelationDaoTest unit test

--- linkis-public-enhancements/linkis-instance-label/linkis-instance-label-server/src/test/java/org/apache/linkis/instance/label/dao/InsLabelRelationDaoTest.java
+++ linkis-public-enhancements/linkis-instance-label/linkis-instance-label-server/src/test/java/org/apache/linkis/instance/label/dao/InsLabelRelationDaoTest.java
@@ -1,10 +1,9 @@
 package org.apache.linkis.instance.label.dao;
 
-
-import org.apache.ibatis.annotations.Param;
+import org.apache.linkis.common.ServiceInstance;
 import org.apache.linkis.instance.label.entity.InsPersistenceLabel;
 import org.apache.linkis.instance.label.entity.InstanceInfo;
-import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
@@ -12,91 +11,127 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class InsLabelRelationDaoTest extends  BaseDaoTest{
+public class InsLabelRelationDaoTest extends BaseDaoTest {
 
+  @Autowired InsLabelRelationDao insLabelRelationDao;
 
-    @Autowired
-    InsLabelRelationDao insLabelRelationDao;
+  @Test
+  public void testSearchInsDirectByValues() {
+    Map<String, String> map = new HashMap<>();
+    map.put("test", "test1");
+    List<Map<String, String>> list = new ArrayList<>();
+    list.add(map);
+    String relation = "testRelation";
+    List<InstanceInfo> instanceInfoList =
+        insLabelRelationDao.searchInsDirectByValues(list, relation);
+    assertTrue(instanceInfoList.size() <= 0);
+  }
 
-    @Test
-    public void testSearchInsDirectByValues() {
-        Map<String,String> map=new HashMap<>();
-        map.put("test","test1");
-        List<Map<String, String>> list=new ArrayList<>();
-        list.add(map);
-        String relation="testRelation";
-        List<InstanceInfo> instanceInfoList=insLabelRelationDao.searchInsDirectByValues(list,relation);
-        assertTrue(instanceInfoList.size() <= 0);
-    }
+  @Test
+  public void testSearchInsDirectByLabels() {
+    List<InsPersistenceLabel> labels = new ArrayList<>();
+    InsPersistenceLabel insPersistenceLabel = new InsPersistenceLabel();
+    insPersistenceLabel.setLabelKey("testKey");
+    insPersistenceLabel.setStringValue("testVa");
+    labels.add(insPersistenceLabel);
+    List<InstanceInfo> instanceInfoList = insLabelRelationDao.searchInsDirectByLabels(labels);
+    assertTrue(instanceInfoList.size() <= 0);
+  }
 
-    @Test
-    public void testSearchInsDirectByLabels() {
-        List<InsPersistenceLabel> labels=new ArrayList<>();
-        InsPersistenceLabel insPersistenceLabel=new InsPersistenceLabel();
-        insPersistenceLabel.setLabelKey("testKey");
-        insPersistenceLabel.setStringValue("testVa");
-        labels.add(insPersistenceLabel);
-        List<InstanceInfo> instanceInfoList=insLabelRelationDao.searchInsDirectByLabels(labels);
-        assertTrue(instanceInfoList.size() <= 0);
-    }
+  @Test
+  public void testSearchInsCascadeByValues() {
+    Map<String, String> map = new HashMap<>();
+    map.put("test", "test1");
+    List<Map<String, String>> valueContent = new ArrayList<>();
+    valueContent.add(map);
+    String relation = "testRelation";
+    List<InstanceInfo> instanceInfoList =
+        insLabelRelationDao.searchInsCascadeByValues(valueContent, relation);
+    assertTrue(instanceInfoList.size() <= 0);
+  }
 
-    @Test
-    public void testSearchInsCascadeByValues() {
-        Map<String,String> map=new HashMap<>();
-        map.put("test","test1");
-        List<Map<String, String>> valueContent=new ArrayList<>();
-        valueContent.add(map);
-        String relation="testRelation";
-        List<InstanceInfo> instanceInfoList=insLabelRelationDao.searchInsCascadeByValues(valueContent,relation);
-        assertTrue(instanceInfoList.size() <= 0);
-    }
+  @Test
+  public void testSearchInsCascadeByLabels() {
+    List<InsPersistenceLabel> labels = new ArrayList<>();
+    InsPersistenceLabel insPersistenceLabel = new InsPersistenceLabel();
+    insPersistenceLabel.setLabelKey("testKey");
+    insPersistenceLabel.setStringValue("testVa");
+    labels.add(insPersistenceLabel);
+    List<InstanceInfo> instanceInfoList = insLabelRelationDao.searchInsCascadeByLabels(labels);
+    assertTrue(instanceInfoList.size() <= 0);
+  }
 
-    @Test
-    public void testSearchInsCascadeByLabels() {
-        List<InsPersistenceLabel> labels=new ArrayList<>();
-        InsPersistenceLabel insPersistenceLabel=new InsPersistenceLabel();
-        insPersistenceLabel.setLabelKey("testKey");
-        insPersistenceLabel.setStringValue("testVa");
-        labels.add(insPersistenceLabel);
-        List<InstanceInfo> instanceInfoList=insLabelRelationDao.searchInsCascadeByLabels(labels);
-        assertTrue(instanceInfoList.size() <= 0);
-    }
+  @Test
+  public void testSearchUnRelateInstances() {
+    InstanceInfo instanceInfo = new InstanceInfo();
+    instanceInfo.setApplicationName("testApplicationName");
+    List<InstanceInfo> instanceInfoList = insLabelRelationDao.searchUnRelateInstances(instanceInfo);
+    assertTrue(instanceInfoList.size() <= 0);
+  }
 
-    @Test
-    public void testSearchUnRelateInstances() {
-    }
+  @Test
+  public void testSearchLabelRelatedInstances() {
+    InstanceInfo instanceInfo = new InstanceInfo();
+    instanceInfo.setApplicationName("testApplicationName");
+    List<InstanceInfo> instanceInfoList =
+        insLabelRelationDao.searchLabelRelatedInstances(instanceInfo);
+    assertTrue(instanceInfoList.size() <= 0);
+  }
 
-    @Test
-    public void testSearchLabelRelatedInstances() {
-    }
+  @Test
+  public void testSearchLabelsByInstance() {
+    String instance = "testInstance";
+    List<InsPersistenceLabel> insPersistenceLabelList =
+        insLabelRelationDao.searchLabelsByInstance(instance);
+    assertTrue(insPersistenceLabelList.size() <= 0);
+  }
 
-    @Test
-    public void testSearchLabelsByInstance() {
-    }
+  @Test
+  public void testListAllInstanceWithLabel() {
+    List<InstanceInfo> instanceInfoList = insLabelRelationDao.listAllInstanceWithLabel();
+    assertTrue(instanceInfoList.size() <= 0);
+  }
 
-    @Test
-    public void testListAllInstanceWithLabel() {
-    }
+  @Test
+  public void testGetInstancesByNames() {
+    String appName = "testAppName";
+    List<ServiceInstance> serviceInstanceList = insLabelRelationDao.getInstancesByNames(appName);
+    assertTrue(serviceInstanceList.size() <= 0);
+  }
 
-    @Test
-    public void testGetInstancesByNames() {
-    }
+  @Test
+  public void testDropRelationsByInstanceAndLabelIds() {
+    String testInstance = "testInstance";
+    List<Integer> labelIds = new ArrayList<>();
+    labelIds.add(1);
+    labelIds.add(2);
+    insLabelRelationDao.dropRelationsByInstanceAndLabelIds(testInstance, labelIds);
+  }
 
-    @Test
-    public void testDropRelationsByInstanceAndLabelIds() {
-    }
+  @Test
+  public void testDropRelationsByInstance() {
+    String testInstance = "testInstance";
+    insLabelRelationDao.dropRelationsByInstance(testInstance);
+  }
 
-    @Test
-    public void testDropRelationsByInstance() {
-    }
+  @Test
+  public void testInsertRelations() {
+    String testInstance = "testInstance";
+    List<Integer> labelIds = new ArrayList<>();
+    labelIds.add(1);
+    labelIds.add(2);
+    insLabelRelationDao.insertRelations(testInstance, labelIds);
+  }
 
-    @Test
-    public void testInsertRelations() {
-    }
-
-    @Test
-    public void testExistRelations() {
-    }
+  @Test
+  public void testExistRelations() {
+    testInsertRelations();
+    Integer labelId = 1;
+    Integer integer = insLabelRelationDao.existRelations(1);
+    assertTrue(integer == 1);
+  }
 }

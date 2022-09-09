d28409372eef3f1e61109ac514d0ad6d1d8b0366
binbinCheng <cheng741201916@163.com>
2022-09-09 15:16:19 +0800

    DefaultInsLabelServiceTest  unit test

--- linkis-public-enhancements/linkis-instance-label/linkis-instance-label-server/src/test/java/org/apache/linkis/instance/label/service/impl/DefaultInsLabelServiceTest.java
+++ linkis-public-enhancements/linkis-instance-label/linkis-instance-label-server/src/test/java/org/apache/linkis/instance/label/service/impl/DefaultInsLabelServiceTest.java
@@ -1,4 +1,3 @@
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,13 +15,17 @@
  * limitations under the License.
  */
 
-package org.apache.linkis.instance.label.service.impl; 
- 
+package org.apache.linkis.instance.label.service.impl;
+
 import org.apache.linkis.common.ServiceInstance;
 import org.apache.linkis.instance.label.dao.InsLabelRelationDao;
+import org.apache.linkis.instance.label.dao.InstanceInfoDao;
 import org.apache.linkis.instance.label.entity.InsPersistenceLabel;
-import org.apache.linkis.manager.label.entity.Label;
-import org.junit.jupiter.api.DisplayName;
+import org.apache.linkis.instance.label.entity.InstanceInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -30,288 +33,102 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/** 
- * DefaultInsLabelService Tester
-*/
+/** DefaultInsLabelService Tester */
 @ExtendWith(MockitoExtension.class)
 public class DefaultInsLabelServiceTest {
 
-    @InjectMocks
-    private DefaultInsLabelService defaultInsLabelService;
+  @InjectMocks private DefaultInsLabelService defaultInsLabelService;
 
-    @Mock
-    private InsLabelRelationDao insLabelRelationDao;
- 
+  @Mock private InsLabelRelationDao insLabelRelationDao;
 
- 
- 
-    @Test
-    public void testRemoveLabelsFromInstance() throws Exception {
-        //TODO: Test goes here...
-        List<InsPersistenceLabel> labelsCandidateRemoved =new ArrayList<>();
-        InsPersistenceLabel insPersistenceLabel=new InsPersistenceLabel();
-        insPersistenceLabel.setId(1);
-        insPersistenceLabel.setLabelKey("testLabelKey");
-        insPersistenceLabel.setStringValue("testStringValue");
-        labelsCandidateRemoved.add(insPersistenceLabel);
-        ServiceInstance serviceInstance=new ServiceInstance();
-        serviceInstance.setInstance("testInstance");
-        serviceInstance.setApplicationName("testApplicationName");
-        Mockito.when(insLabelRelationDao.searchLabelsByInstance(serviceInstance.getInstance())).thenReturn(labelsCandidateRemoved);
-        Mockito.doNothing().when(insLabelRelationDao).dropRelationsByInstance(serviceInstance.getInstance());
-        defaultInsLabelService.removeLabelsFromInstance(serviceInstance);
+  @Mock private InstanceInfoDao instanceInfoDao;
 
-    }
- 
- 
-    @Test
-    public void testSearchInstancesByLabelsLabels() throws Exception { 
-        //TODO: Test goes here...
-        /*List<? extends Label<?>> labels =new ArrayList<>();
-        List<ServiceInstance> list=new ArrayList<>();
-        ServiceInstance serviceInstance=new ServiceInstance();
-        serviceInstance.setInstance("testInstance");
-        serviceInstance.setApplicationName("testApplicationName");
-        list.add(serviceInstance);
-        Mockito.when(defaultInsLabelService.searchInstancesByLabels(labels, Label.ValueRelation.ALL)).thenReturn(list);
-        List<ServiceInstance> lists=defaultInsLabelService.searchInstancesByLabels(labels);
-        assertTrue( list.equals(lists));*/
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testSearchInstancesByLabelsForLabelsRelation() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testSearchUnRelateInstances() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testSearchLabelRelatedInstances() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testRemoveLabelsIfNotRelation() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testListAllInstanceWithLabel() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testGetInstancesByNames() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testRemoveInstance() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testGetInstanceInfoByServiceInstance() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testUpdateInstance() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testGetEurekaURL() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testMarkInstanceLabel() throws Exception { 
-        //TODO: Test goes here... 
-    } 
- 
- 
+  @Mock private InstanceInfoDao instanceDao;
 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testInitQueue() throws Exception { 
-        //TODO: Test goes here... 
-        /* 
-        try { 
-           Method method = DefaultInsLabelService.getClass().getMethod("initQueue"); 
-           method.setAccessible(true); 
-           method.invoke(<Object>, <Parameters>); 
-        } catch(NoSuchMethodException e) { 
-        } catch(IllegalAccessException e) { 
-        } catch(InvocationTargetException e) { 
-        } 
-        */ 
-        } 
- 
+  @Test
+  public void testRemoveLabelsFromInstance() throws Exception {
+    // TODO: Test goes here...
+    List<InsPersistenceLabel> labelsCandidateRemoved = new ArrayList<>();
+    InsPersistenceLabel insPersistenceLabel = new InsPersistenceLabel();
+    insPersistenceLabel.setId(1);
+    insPersistenceLabel.setLabelKey("testLabelKey");
+    insPersistenceLabel.setStringValue("testStringValue");
+    labelsCandidateRemoved.add(insPersistenceLabel);
+    ServiceInstance serviceInstance = new ServiceInstance();
+    serviceInstance.setInstance("testInstance");
+    serviceInstance.setApplicationName("testApplicationName");
+    Mockito.when(insLabelRelationDao.searchLabelsByInstance(serviceInstance.getInstance()))
+        .thenReturn(labelsCandidateRemoved);
+    Mockito.doNothing()
+        .when(insLabelRelationDao)
+        .dropRelationsByInstance(serviceInstance.getInstance());
+    defaultInsLabelService.removeLabelsFromInstance(serviceInstance);
+  }
 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testUrlPreDeal() throws Exception { 
-        //TODO: Test goes here... 
-        /* 
-        try { 
-           Method method = DefaultInsLabelService.getClass().getMethod("urlPreDeal", String.class); 
-           method.setAccessible(true); 
-           method.invoke(<Object>, <Parameters>); 
-        } catch(NoSuchMethodException e) { 
-        } catch(IllegalAccessException e) { 
-        } catch(InvocationTargetException e) { 
-        } 
-        */ 
-        } 
- 
+  @Test
+  public void testListAllInstanceWithLabel() throws Exception {
+    // TODO: Test goes here...
+    List<InstanceInfo> list = new ArrayList<>();
+    InstanceInfo instanceInfo = new InstanceInfo();
+    instanceInfo.setInstance("testInstance");
+    instanceInfo.setApplicationName("testApplicationName");
+    instanceInfo.setId(1);
+    list.add(instanceInfo);
+    Mockito.when(defaultInsLabelService.listAllInstanceWithLabel()).thenReturn(list);
+    List<InstanceInfo> list1 = defaultInsLabelService.listAllInstanceWithLabel();
+    assertTrue(list.equals(list1));
+  }
 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testReplaceEurekaURL() throws Exception { 
-        //TODO: Test goes here... 
-        /* 
-        try { 
-           Method method = DefaultInsLabelService.getClass().getMethod("replaceEurekaURL", String.class); 
-           method.setAccessible(true); 
-           method.invoke(<Object>, <Parameters>); 
-        } catch(NoSuchMethodException e) { 
-        } catch(IllegalAccessException e) { 
-        } catch(InvocationTargetException e) { 
-        } 
-        */ 
-        } 
- 
+  @Test
+  public void testGetInstancesByNames() throws Exception {
+    // TODO: Test goes here...
+    String appName = "testApplicationName";
+    List<ServiceInstance> list = new ArrayList<>();
+    ServiceInstance serviceInstance = new ServiceInstance();
+    serviceInstance.setInstance("testInstance");
+    serviceInstance.setApplicationName("testApplicationName");
+    list.add(serviceInstance);
+    Mockito.when(insLabelRelationDao.getInstancesByNames(appName)).thenReturn(list);
+    List<ServiceInstance> list1 = defaultInsLabelService.getInstancesByNames(appName);
+    assertTrue(list.equals(list1));
+  }
 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testIsIPAddress() throws Exception { 
-        //TODO: Test goes here... 
-        /* 
-        try { 
-           Method method = DefaultInsLabelService.getClass().getMethod("isIPAddress", String.class); 
-           method.setAccessible(true); 
-           method.invoke(<Object>, <Parameters>); 
-        } catch(NoSuchMethodException e) { 
-        } catch(IllegalAccessException e) { 
-        } catch(InvocationTargetException e) { 
-        } 
-        */ 
-        } 
- 
+  @Test
+  public void testRemoveInstance() throws Exception {
+    // TODO: Test goes here...
+    ServiceInstance serviceInstance = new ServiceInstance();
+    serviceInstance.setInstance("testInstance");
+    Mockito.doNothing().when(instanceInfoDao).removeInstance(serviceInstance);
+    defaultInsLabelService.removeInstance(serviceInstance);
+  }
 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testFilterLabelNeededInsert() throws Exception { 
-        //TODO: Test goes here... 
-        /* 
-        try { 
-           Method method = DefaultInsLabelService.getClass().getMethod("filterLabelNeededInsert", List<InsPersistenceLabel>.class, boolean.class); 
-           method.setAccessible(true); 
-           method.invoke(<Object>, <Parameters>); 
-        } catch(NoSuchMethodException e) { 
-        } catch(IllegalAccessException e) { 
-        } catch(InvocationTargetException e) { 
-        } 
-        */ 
-        } 
- 
+  @Test
+  public void testGetInstanceInfoByServiceInstance() throws Exception {
+    // TODO: Test goes here...
+    InstanceInfo instanceInfo = new InstanceInfo();
+    instanceInfo.setInstance("testInstance");
+    instanceInfo.setApplicationName("testApplicationName");
+    instanceInfo.setId(1);
+    ServiceInstance serviceInstance = new ServiceInstance();
+    serviceInstance.setInstance("testInstance");
+    serviceInstance.setApplicationName("testApplicationName");
+    Mockito.when(instanceInfoDao.getInstanceInfoByServiceInstance(serviceInstance))
+        .thenReturn(instanceInfo);
+    InstanceInfo instanceInfo1 =
+        defaultInsLabelService.getInstanceInfoByServiceInstance(serviceInstance);
+    assertTrue(instanceInfo1.equals(instanceInfo));
+  }
 
-    @Test
-    @DisplayName("Method description: ...")
-    public void testDoInsertInsLabels() throws Exception { 
-        //TODO: Test goes here... 
-        /* 
-        try { 
-           Method method = DefaultInsLabelService.getClass().getMethod("doInsertInsLabels", List<InsPersistenceLabel>.class); 
-           method.setAccessible(true); 
-           method.invoke(<Object>, <Parameters>); 
-        } catch(NoSuchMethodException e) { 
-        } catch(IllegalAccessException e) { 
-        } catch(InvocationTargetException e) { 
-        } 
-        */ 
-        } 
- 
-
-    @Test
-    @DisplayName("Method description: ...")
-    public void testDoInsertInstance() throws Exception { 
-        //TODO: Test goes here... 
-        /* 
-        try { 
-           Method method = DefaultInsLabelService.getClass().getMethod("doInsertInstance", ServiceInstance.class); 
-           method.setAccessible(true); 
-           method.invoke(<Object>, <Parameters>); 
-        } catch(NoSuchMethodException e) { 
-        } catch(IllegalAccessException e) { 
-        } catch(InvocationTargetException e) { 
-        } 
-        */ 
-        } 
- 
-
-    @Test
-    @DisplayName("Method description: ...")
-    public void testToInsPersistenceLabels() throws Exception { 
-        //TODO: Test goes here... 
-        /* 
-        try { 
-           Method method = DefaultInsLabelService.getClass().getMethod("toInsPersistenceLabels", List<?.class); 
-           method.setAccessible(true); 
-           method.invoke(<Object>, <Parameters>); 
-        } catch(NoSuchMethodException e) { 
-        } catch(IllegalAccessException e) { 
-        } catch(InvocationTargetException e) { 
-        } 
-        */ 
-        } 
- 
-
-    @Test
-    @DisplayName("Method description: ...")
-    public void testBatchOperation() throws Exception { 
-        //TODO: Test goes here... 
-        /* 
-        try { 
-           Method method = DefaultInsLabelService.getClass().getMethod("batchOperation", T.class, Consumer<T>.class, int.class); 
-           method.setAccessible(true); 
-           method.invoke(<Object>, <Parameters>); 
-        } catch(NoSuchMethodException e) { 
-        } catch(IllegalAccessException e) { 
-        } catch(InvocationTargetException e) { 
-        } 
-        */ 
-        } 
- 
-} 
+  @Test
+  public void testUpdateInstance() throws Exception {
+    // TODO: Test goes here...
+    InstanceInfo instanceInfo = new InstanceInfo();
+    instanceInfo.setInstance("testInstance1");
+    instanceInfo.setApplicationName("testApplicationName1");
+    instanceInfo.setId(1);
+    Mockito.doNothing().when(instanceInfoDao).updateInstance(instanceInfo);
+    defaultInsLabelService.updateInstance(instanceInfo);
+  }
+}

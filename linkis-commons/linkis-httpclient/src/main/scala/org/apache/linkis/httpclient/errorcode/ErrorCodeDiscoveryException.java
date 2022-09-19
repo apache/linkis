8eea67aa4ec80f97c30de1e6f9eebb5f76948a72
binbinCheng <cheng741201916@163.com>
2022-09-19 11:36:09 +0800

    [linkis-httpclient] errorcode optimization

---
+++ linkis-commons/linkis-httpclient/src/main/scala/org/apache/linkis/httpclient/errorcode/ErrorCodeDiscoveryException.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.linkis.httpclient.errorcode;
+
+public enum ErrorCodeDiscoveryException {
+  CONNECT_TO_SERVERURL(
+          10901,
+          "connect to serverUrl failed! Reason: gateway server is unhealthy!(连接到 serverUrl 失败！ 原因：网关服务器请求失败！)",
+          "connect to serverUrl failed! Reason: gateway server is unhealthy!(连接到 serverUrl 失败！ 原因：网关服务器请求失败！)",
+          "");
+
+  /** 错误码 */
+  private int errorCode;
+  /** 错误描述 */
+  private String errorDesc;
+  /** 错误可能出现的原因 */
+  private String comment;
+  /** 所属的linkis的模块 */
+  private String module;
+
+  ErrorCodeDiscoveryException(int errorCode, String errorDesc, String comment, String module) {
+    this.errorCode = errorCode;
+    this.errorDesc = errorDesc;
+    this.comment = comment;
+    this.module = module;
+  }
+
+  public int getErrorCode() {
+    return errorCode;
+  }
+
+  public void setErrorCode(int errorCode) {
+    this.errorCode = errorCode;
+  }
+
+  public String getErrorDesc() {
+    return errorDesc;
+  }
+
+  public void setErrorDesc(String errorDesc) {
+    this.errorDesc = errorDesc;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
+  public String getModule() {
+    return module;
+  }
+
+  public void setModule(String module) {
+    this.module = module;
+  }
+
+  @Override
+  public String toString() {
+    return "errorCode: " + this.errorCode + ", errorDesc:" + this.errorDesc;
+  }
+}

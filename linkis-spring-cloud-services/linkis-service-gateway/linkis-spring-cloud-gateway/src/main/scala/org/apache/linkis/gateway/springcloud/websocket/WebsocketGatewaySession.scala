03f6c4f38e6fb7d647a5a9df31d4d2176c333e77
huangxiaoping <35296098+huangxiaopingRD@users.noreply.github.com>
2022-09-20 14:34:40 +0800

    linkis-spring-cloud-gateway module errorcode optimization and documentation (#3470)

--- linkis-spring-cloud-services/linkis-service-gateway/linkis-spring-cloud-gateway/src/main/scala/org/apache/linkis/gateway/springcloud/websocket/WebsocketGatewaySession.scala
+++ linkis-spring-cloud-services/linkis-service-gateway/linkis-spring-cloud-gateway/src/main/scala/org/apache/linkis/gateway/springcloud/websocket/WebsocketGatewaySession.scala
@@ -20,7 +20,7 @@ package org.apache.linkis.gateway.springcloud.websocket
 import org.apache.linkis.common.ServiceInstance
 import org.apache.linkis.common.utils.Utils
 import org.apache.linkis.gateway.exception.GatewayErrorException
-import org.apache.linkis.gateway.springcloud.errorcode.GatewayErrorConstants
+import org.apache.linkis.gateway.springcloud.errorcode.GatewayErrorCodeSummary
 import org.apache.linkis.gateway.springcloud.websocket.SpringCloudGatewayWebsocketUtils._
 import org.apache.linkis.server.conf.ServerConfiguration
 
@@ -65,7 +65,7 @@ class GatewayWebSocketSessionConnection(
     ) {
       proxySession.close(CloseStatus.SERVER_ERROR)
       throw new GatewayErrorException(
-        GatewayErrorConstants.WEBSOCKET_CONNECT_ERROR,
+        GatewayErrorCodeSummary.WEBSOCKET_CONNECT_ERROR.getErrorCode,
         s"Create a " +
           s"WebSocket connection for" +
           s" ${serviceInstance.getApplicationName} repeatedly!(重复地为${serviceInstance.getApplicationName}创建WebSocket连接！)"

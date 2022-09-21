fa414bd7d6eef85fef3991b1119552a1d6ce2c70
aiceflower <kinsanities@sina.com>
2022-09-21 15:32:24 +0800

    add linkis-manager-persistence test cases

--- linkis-computation-governance/linkis-manager/linkis-manager-persistence/src/test/java/org/apache/linkis/manager/dao/NodeManagerMapperTest.java
+++ linkis-computation-governance/linkis-manager/linkis-manager-persistence/src/test/java/org/apache/linkis/manager/dao/NodeManagerMapperTest.java
@@ -19,10 +19,9 @@ package org.apache.linkis.manager.dao;
 
 import org.apache.linkis.manager.common.entity.persistence.PersistenceNode;
 
-import org.apache.ibatis.annotations.*;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -37,6 +36,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class NodeManagerMapperTest extends BaseDaoTest {
 
   @Autowired NodeManagerMapper nodeManagerMapper;
+  @Autowired LabelManagerMapper labelManagerMapper;
 
   @BeforeAll
   @DisplayName("Each unit test method is executed once before execution")
@@ -55,14 +55,6 @@ public class NodeManagerMapperTest extends BaseDaoTest {
   private String creator = "testCreator";
   private String updator = "testUpdator";
 
-  // @Insert({
-  //        "insert into
-  // linkis_cg_manager_service_instance(instance,name,owner,mark,update_time,create_time,updator,creator)"
-  //                +
-  // "values(#{instance},#{name},#{owner},#{mark},#{updateTime},#{createTime},#{updator},#{creator})"
-  // @Options(useGeneratedKeys = true, keyProperty = "id")
-  // void addNodeInstance(PersistenceNode node) throws DuplicateKeyException;
-
   public PersistenceNode insertOne() {
     PersistenceNode persistenceNode = new PersistenceNode();
     persistenceNode.setInstance(instance);
@@ -77,16 +69,6 @@ public class NodeManagerMapperTest extends BaseDaoTest {
     return persistenceNode;
   }
 
-  //  @Update({
-  //          "update linkis_cg_manager_service_instance set instance = #{persistenceNode.instance},
-  // owner = #{persistenceNode.owner},mark = #{persistenceNode.mark},name =
-  // #{persistenceNode.name},"
-  //                  + "update_time = #{persistenceNode.updateTime},updator =
-  // #{persistenceNode.updator},creator = #{persistenceNode.creator} where instance = #{instance}"
-  //  })
-  //  void updateNodeInstance(
-  //          @Param("instance") String instance,
-  //          @Param("persistenceNode") PersistenceNode persistenceNode);
   @Test
   public void testUpdateNodeInstance() {
     PersistenceNode pn = insertOne();
@@ -98,8 +80,6 @@ public class NodeManagerMapperTest extends BaseDaoTest {
     assertEquals("ttt", persistenceNode.getInstance());
   }
 
-  //  @Delete("delete from  linkis_cg_manager_service_instance where instance = #{instance}")
-  //  void removeNodeInstance(@Param("instance") String instance);
   @Test
   public void testRemoveNodeInstance() {
     PersistenceNode pn = insertOne();
@@ -110,13 +90,6 @@ public class NodeManagerMapperTest extends BaseDaoTest {
     assertNull(persistenceNode);
   }
 
-  //
-  //  @Select("select * from  linkis_cg_manager_service_instance where owner = #{owner}")
-  //  @Results({
-  //          @Result(property = "updateTime", column = "update_time"),
-  //          @Result(property = "createTime", column = "create_time")
-  //  })
-  //  List<PersistenceNode> getNodeInstancesByOwner(@Param("owner") String owner);
   @Test
   public void testGetNodeInstancesByOwner() {
     PersistenceNode pn = insertOne();
@@ -124,13 +97,6 @@ public class NodeManagerMapperTest extends BaseDaoTest {
     assertEquals(1, instances.size());
   }
 
-  //
-  //  @Select("select * from  linkis_cg_manager_service_instance")
-  //  @Results({
-  //          @Result(property = "updateTime", column = "update_time"),
-  //          @Result(property = "createTime", column = "create_time")
-  //  })
-  //  List<PersistenceNode> getAllNodes();
   @Test
   public void testGetAllNodes() {
     PersistenceNode pn = insertOne();
@@ -139,150 +105,90 @@ public class NodeManagerMapperTest extends BaseDaoTest {
     assertEquals(2, instances.size());
   }
 
-  //
-  //  @Update({
-  //          "update linkis_cg_manager_service_instance set owner = #{persistenceNode.owner},mark =
-  // #{persistenceNode.mark},name = #{persistenceNode.name},"
-  //                  + "update_time = #{persistenceNode.updateTime},create_time =
-  // #{persistenceNode.createTime},updator = #{persistenceNode.updator},creator =
-  // #{persistenceNode.creator} where instance = #{instance}"
-  //  })
-  //  void updateNodeInstanceOverload(PersistenceNode persistenceNode);
   @Test
-  public void testUpdateNodeInstanceOverload() {}
+  public void testUpdateNodeInstanceOverload() {
+    PersistenceNode pn = insertOne();
+    pn.setUpdator("testUp");
+    nodeManagerMapper.updateNodeInstanceOverload(pn);
+    PersistenceNode persistenceNode = nodeManagerMapper.getNodeInstanceById(pn.getId());
+    assertEquals("testUp", persistenceNode.getUpdator());
+  }
 
-  //
-  //  @Select("select id from  linkis_cg_manager_service_instance where instance = #{instance}")
-  //  int getNodeInstanceId(@Param("instance") String instance);
   @Test
-  public void testGetNodeInstanceId() {}
+  public void testGetNodeInstanceId() {
+    PersistenceNode pn = insertOne();
+    int id = nodeManagerMapper.getNodeInstanceId(instance);
+    assertEquals(pn.getId(), id);
+  }
 
-  //
-  //  @Select("select id from  linkis_cg_manager_service_instance where instance = #{instance}")
-  //  int getIdByInstance(@Param("instance") String instance);
   @Test
-  public void testGetIdByInstance() {}
+  public void testGetIdByInstance() {
+    PersistenceNode pn = insertOne();
+    int id = nodeManagerMapper.getIdByInstance(instance);
+    assertEquals(pn.getId(), id);
+  }
 
-  //
-  //  @Select(
-  //          "<script>"
-  //                  + "select id from linkis_cg_manager_service_instance where instance in("
-  //                  + "<foreach collection='instances' separator=',' item='instance'>"
-  //                  + "#{instance} "
-  //                  + "</foreach> "
-  //                  + ")</script>")
-  //  List<Integer> getNodeInstanceIds(@Param("serviceInstances") List<String> instances);
   @Test
-  public void testGetNodeInstanceIds() {}
+  public void testGetNodeInstanceIds() {
+    PersistenceNode pn = insertOne();
+    List<Integer> list = nodeManagerMapper.getNodeInstanceIds(Arrays.asList(instance, "test"));
+    assertEquals(pn.getId(), list.get(0));
+  }
 
-  //
-  //  @Select("select * from linkis_cg_manager_service_instance where instance = #{instance}")
-  //  @Results({
-  //          @Result(property = "updateTime", column = "update_time"),
-  //          @Result(property = "createTime", column = "create_time")
-  //  })
-  //  PersistenceNode getNodeInstance(@Param("instance") String instance);
   @Test
-  public void testGetNodeInstance() {}
+  public void testGetNodeInstance() {
+    insertOne();
+    PersistenceNode node = nodeManagerMapper.getNodeInstance(instance);
+    assertEquals(mark, node.getMark());
+  }
 
-  //
-  //  @Select("select * from  linkis_cg_manager_service_instance where id = #{id}")
-  //  @Results({
-  //          @Result(property = "updateTime", column = "update_time"),
-  //          @Result(property = "createTime", column = "create_time")
-  //  })
-  //  PersistenceNode getNodeInstanceById(@Param("id") int id);
   @Test
-  public void testGetNodeInstanceById() {}
+  public void testGetEMNodeInstanceByEngineNode() {
+    insertOne();
+    nodeManagerMapper.addEngineNode(instance, instance);
+    PersistenceNode node = nodeManagerMapper.getEMNodeInstanceByEngineNode(instance);
+    assertEquals(mark, node.getMark());
+  }
 
-  //
-  //  @Select(
-  //          "select * from linkis_cg_manager_service_instance where instance in (select
-  // em_instance from linkis_cg_manager_engine_em where engine_instance=#{instance})")
-  //  @Results({
-  //          @Result(property = "updateTime", column = "update_time"),
-  //          @Result(property = "createTime", column = "create_time")
-  //  })
-  //  PersistenceNode getEMNodeInstanceByEngineNode(@Param("instance") String instance);
   @Test
-  public void testGetEMNodeInstanceByEngineNode() {}
+  public void testGetNodeInstances() {
+    insertOne();
+    nodeManagerMapper.addEngineNode(instance, instance);
+    List<PersistenceNode> instances = nodeManagerMapper.getNodeInstances(instance);
+    assertEquals(1, instances.size());
+  }
 
-  //
-  //  @Select(
-  //          "select * from linkis_cg_manager_service_instance where instance in ( select
-  // engine_instance from linkis_cg_manager_engine_em where em_instance=#{instance})")
-  //  @Results({
-  //          @Result(property = "updateTime", column = "update_time"),
-  //          @Result(property = "createTime", column = "create_time")
-  //  })
-  //  List<PersistenceNode> getNodeInstances(@Param("instance") String instance);
   @Test
-  public void testGetNodeInstances() {}
+  public void testGetNodesByInstances() {
+    insertOne();
+    List<PersistenceNode> nodes = nodeManagerMapper.getNodesByInstances(Arrays.asList(instance));
+    assertEquals(1, nodes.size());
+  }
 
-  //
-  //  @Select(
-  //          "<script>"
-  //                  + "select * from linkis_cg_manager_service_instance where instance in("
-  //                  + "<foreach collection='instances' separator=',' item='instance'>"
-  //                  + "#{instance} "
-  //                  + "</foreach> "
-  //                  + ")</script>")
-  //  @Results({
-  //          @Result(property = "updateTime", column = "update_time"),
-  //          @Result(property = "createTime", column = "create_time")
-  //  })
-  //  List<PersistenceNode> getNodesByInstances(@Param("engineNodeIds") List<String> instances);
   @Test
-  public void testGetNodesByInstances() {}
+  public void testDeleteEngineNode() {
+    insertOne();
+    nodeManagerMapper.addEngineNode(instance, instance);
+    List<PersistenceNode> instances = nodeManagerMapper.getNodeInstances(instance);
+    assertEquals(1, instances.size());
+    nodeManagerMapper.deleteEngineNode(instance, instance);
+    instances = nodeManagerMapper.getNodeInstances(instance);
+    assertEquals(0, instances.size());
+  }
 
-  //
-  //  @Insert(
-  //          "insert into  linkis_cg_manager_engine_em (engine_instance, em_instance, update_time,
-  // create_time)"
-  //                  + "values(#{engineNodeInstance}, #{emNodeInstance}, now(), now())")
-  //  void addEngineNode(
-  //          @Param("engineNodeInstance") String engineNodeInstance,
-  //          @Param("emNodeInstance") String emNodeInstance);
   @Test
-  public void testAddEngineNode() {}
+  public void testUpdateNodeRelation() {
+    insertOne();
+    nodeManagerMapper.addEngineNode(instance, instance);
+    List<PersistenceNode> instances = nodeManagerMapper.getNodeInstances(instance);
+    assertEquals(1, instances.size());
 
-  //
-  //  @Delete(
-  //          "delete from  linkis_cg_manager_engine_em where engine_instance =
-  // #{engineNodeInstance} and em_instance = #{emNodeInstance}")
-  //  void deleteEngineNode(
-  //          @Param("engineNodeInstance") String engineNodeInstance,
-  //          @Param("emNodeInstance") String emNodeInstance);
-  @Test
-  public void testDeleteEngineNode() {}
+    nodeManagerMapper.updateNodeRelation(instance, "test");
+  }
 
-  //
-  //  @Select("select engine_id from  linkis_cg_manager_engine_em where em_id = #{emId}")
-  //  List<Integer> getEngineNodeIDsByEMId(@Param("emId") int emId);
   @Test
-  public void testGetEngineNodeIDsByEMId() {}
-
-  //
-  //  @Select("select em_id from  linkis_cg_manager_engine_em where engine_id = #{engineNodeId}")
-  //  int getEMIdByEngineId(@Param("engineNodeId") int engineNodeId);
-  @Test
-  public void testGetEMIdByEngineId() {}
-
-  //
-  //  @Select("select id from linkis_cg_manager_service_instance where owner = #{owner}")
-  //  List<Integer> getNodeInstanceIdsByOwner(@Param("owner") String owner);
-  @Test
-  public void testGetNodeInstanceIdsByOwner() {}
-
-  //
-  //  void updateNodeRelation(@Param("tickedId") String tickedId, @Param("instance") String
-  // instance);
-  @Test
-  public void testUpdateNodeRelation() {}
-
-  //
-  //  void updateNodeLabelRelation(
-  //          @Param("tickedId") String tickedId, @Param("instance") String instance);
-  @Test
-  public void testUpdateNodeLabelRelation() {}
+  public void testUpdateNodeLabelRelation() {
+    labelManagerMapper.addLabelServiceInstance(instance, Arrays.asList(8888));
+    nodeManagerMapper.updateNodeLabelRelation(instance, instance);
+  }
 }

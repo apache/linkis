6be667d8df588c0ffcd32bd13ee1de5ce59d1f10
binbinCheng <cheng741201916@163.com>
2022-09-26 17:06:21 +0800

    update LinkisConfigurationErrorCodeSummary.java and ConfigurationException.java

--- linkis-public-enhancements/linkis-datasource/linkis-datasource-manager/common/src/main/java/org/apache/linkis/datasourcemanager/common/errorcode/LinkisDatasourceManagerErrorCodeSummary.java
+++ linkis-public-enhancements/linkis-datasource/linkis-datasource-manager/common/src/main/java/org/apache/linkis/datasourcemanager/common/errorcode/LinkisDatasourceManagerErrorCodeSummary.java
@@ -17,26 +17,24 @@
 
 package org.apache.linkis.datasourcemanager.common.errorcode;
 
-import org.apache.linkis.common.errorcode.ErrorCodeUtils;
-
 public enum LinkisDatasourceManagerErrorCodeSummary {
   SERIALIZATION_FAILED(
       16897,
       "Unable to deserialize to object from string(json) in type: (序列化失败)",
       "Unable to deserialize to object from string(json) in type: (序列化失败)"),
   CANNOT_BE_SERIALIZATION(16898, "cannot be serialized  (无法序列化)", "cannot be serialized (无法序列化)"),
-  EXPRESS_IS_NULL(99987, "", ""),
-  OPERATE_FILE_IN_REQUEST(
-      99987,
-      "Fail to operate file in request(上传文件处理失败)",
-      "Fail to operate file in request(上传文件处理失败)"),
   CONNECTION_FAILED(99983, "Connection Failed(连接失败)", "Connection Failed(连接失败)"),
   REMOTE_SERVICE_ERROR(
       99983, "Remote Service Error(远端服务出错, 联系运维处理)", "Remote Service Error(远端服务出错, 联系运维处理)"),
   DATASOURCE_NOT_FOUND(99988, "datasource not found(未找到数据源)", "datasource not found(未找到数据源)"),
   PARAM_VALIDATE_FAILED(99986, "Param Validate Failed(参数校验出错)", "Param Validate Failed(参数校验出错)"),
   ENVID_ATYPICAL(99986, "envId atypical(环境非典型)", "envId atypical(环境非典型)"),
-  IS_NULL_MS(99986, "", "");
+  IS_NULL_MS(99986, "", ""),
+  EXPRESS_IS_NULL(99987, "", ""),
+  OPERATE_FILE_IN_REQUEST(
+      99987,
+      "Fail to operate file in request(上传文件处理失败)",
+      "Fail to operate file in request(上传文件处理失败)");
 
   /** 错误码 */
   private int errorCode;
@@ -46,7 +44,6 @@ public enum LinkisDatasourceManagerErrorCodeSummary {
   private String comment;
 
   LinkisDatasourceManagerErrorCodeSummary(int errorCode, String errorDesc, String comment) {
-    ErrorCodeUtils.validateErrorCode(errorCode, 20000, 24999);
     this.errorCode = errorCode;
     this.errorDesc = errorDesc;
     this.comment = comment;

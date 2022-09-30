dd4a73f9400c6317cf656af9bb05835607856ce1
binbinCheng <cheng741201916@163.com>
2022-09-30 17:56:21 +0800

    DataSourceVersionDaoTest.java

--- linkis-public-enhancements/linkis-datasource/linkis-datasource-manager/server/src/test/java/org/apache/linkis/datasourcemanager/core/dao/DataSourceVersionDaoTest.java
+++ linkis-public-enhancements/linkis-datasource/linkis-datasource-manager/server/src/test/java/org/apache/linkis/datasourcemanager/core/dao/DataSourceVersionDaoTest.java
@@ -78,9 +78,9 @@ class DataSourceVersionDaoTest extends BaseDaoTest {
   @Disabled
   @Test
   void testSelectOneVersion() {
-    insertOne();
+    /*insertOne();
     String param = dataSourceVersionDao.selectOneVersion(1l, 2l);
-    assertTrue("a:b".equals(param));
+    assertTrue("a:b".equals(param));*/
   }
 
   @Test

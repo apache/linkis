938c7dd42649ca82847d19b13fee744bf5e3d731
成彬彬 <106590848+binbinCheng@users.noreply.github.com>
2022-10-14 16:06:44 +0800

    [linkis-entrance]module errorcode optimization and documentation (#3600)

--- linkis-computation-governance/linkis-entrance/src/main/scala/org/apache/linkis/entrance/exception/SensitiveTablesCheckException.scala
+++ linkis-computation-governance/linkis-entrance/src/main/scala/org/apache/linkis/entrance/exception/SensitiveTablesCheckException.scala
@@ -18,7 +18,9 @@
 package org.apache.linkis.entrance.exception
 
 import org.apache.linkis.common.exception.ErrorException
+import org.apache.linkis.entrance.errorcode.EntranceErrorCodeSummary._
 
 case class SensitiveTablesCheckException(errorMsg: String) extends ErrorException(50079, errorMsg)
 
-case class DangerousGramsCheckException(errorMsg: String) extends ErrorException(50081, errorMsg)
+case class DangerousGramsCheckException(errorMsg: String)
+    extends ErrorException(JOB_HISTORY_FAILED_ID.getErrorCode, errorMsg)

849cd8b3ed5c6cc96d775f1cc8648f3ceeca91f9
Dlimeng <77954309@qq.com>
2022-10-15 13:58:48 +0800

    seatunnel

--- linkis-engineconn-plugins/seatunnel/src/main/java/org/apache/linkis/engineconnplugin/seatunnel/client/LinkisSeatunnelFlinkClient.java
+++ linkis-engineconn-plugins/seatunnel/src/main/java/org/apache/linkis/engineconnplugin/seatunnel/client/LinkisSeatunnelFlinkClient.java
@@ -1,11 +1,12 @@
 /*
- * Copyright 2019 WeBank
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,8 +17,9 @@
 
 package org.apache.linkis.engineconnplugin.seatunnel.client;
 
-import org.apache.linkis.engineconnplugin.seatunnel.client.utils.JarLoader;
 
+
+import org.apache.linkis.engineconn.computation.executor.utlis.JarLoader;
 import org.apache.seatunnel.core.flink.SeatunnelFlink;
 
 import java.io.PrintWriter;

272b933800c868f2df85cae2989c8d87994d5461
alexkun <xuanyu710@qq.com>
2022-10-27 21:55:01 +0800

    linkis-entrance - fix npe bug linkis-instance-label-cleint - fix npe

--- linkis-computation-governance/linkis-entrance/src/main/scala/org/apache/linkis/entrance/scheduler/EntranceGroupFactory.scala
+++ linkis-computation-governance/linkis-entrance/src/main/scala/org/apache/linkis/entrance/scheduler/EntranceGroupFactory.scala
@@ -53,6 +53,11 @@ import java.util.regex.Pattern
 import scala.collection.JavaConverters._
 
 import com.google.common.cache.{Cache, CacheBuilder}
+import org.apache.linkis.common.ServiceInstance
+import org.apache.linkis.instance.label.client.InstanceLabelClient
+import org.apache.linkis.manager.label.builder.factory.LabelBuilderFactoryContext
+import org.apache.linkis.manager.label.constant.{LabelKeyConstant, LabelValueConstant}
+import org.apache.linkis.manager.label.entity.route.RouteLabel
 
 class EntranceGroupFactory extends GroupFactory with Logging {
 
@@ -165,14 +170,11 @@ class EntranceGroupFactory extends GroupFactory with Logging {
     val offlineRouteLabel = LabelBuilderFactoryContext.getLabelBuilderFactory
       .createLabel[RouteLabel](LabelKeyConstant.ROUTE_KEY, LabelValueConstant.OFFLINE_VALUE)
     labelList.add(offlineRouteLabel)
-    val offlineIns = InstanceLabelClient.getInstance
-      .getInstanceFromLabel(labelList)
-      .asScala
-      .filter(l =>
-        null != l && l.getApplicationName
-          .equalsIgnoreCase(Sender.getThisServiceInstance.getApplicationName)
-      )
-      .toArray
+    var offlineIns: Array[ServiceInstance] = null
+    Utils.tryAndWarn {
+      offlineIns = InstanceLabelClient.getInstance.getInstanceFromLabel(labelList)
+        .asScala.filter(l => null != l && l.getApplicationName.equalsIgnoreCase(Sender.getThisServiceInstance.getApplicationName)).toArray
+    }
     if (null != offlineIns) {
       logger.info(s"There are ${offlineIns.length} offlining instance.")
       entranceNum = entranceNum - offlineIns.length

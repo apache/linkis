15a4b398f8a6eb083900954d4d9ea8a822fbe354
Knypys <sinkuo@qq.com>
2022-10-28 14:07:36 +0800

    Change linkis version from 1.3.0 to 1.3.1 (#3737)

--- linkis-dist/deploy-config/linkis-env.sh
+++ linkis-dist/deploy-config/linkis-env.sh
@@ -148,7 +148,7 @@ export SERVER_HEAP_SIZE="512M"
 ##The extended lib such mysql-connector-java-*.jar
 #LINKIS_EXTENDED_LIB=/appcom/common/linkisExtendedLib
 
-LINKIS_VERSION=1.3.0
+LINKIS_VERSION=1.3.1
 
 # for install
 LINKIS_PUBLIC_MODULE=lib/linkis-commons/public-module

4ef7b24d42a143408014a47efe8c0acbbd626f0b
dlimeng <77954309@qq.com>
2022-11-01 22:32:00 +0800

    [Feature] 1.3.1 linkis metadata query add doris/clickhouse (#3457)

--- linkis-public-enhancements/linkis-datasource/linkis-metadata-query/service/jdbc/src/main/java/org/apache/linkis/metadata/query/service/conf/SqlParamsMapper.java
+++ linkis-public-enhancements/linkis-datasource/linkis-metadata-query/service/jdbc/src/main/java/org/apache/linkis/metadata/query/service/conf/SqlParamsMapper.java
@@ -37,4 +37,7 @@ public class SqlParamsMapper {
 
   public static final CommonVars<String> PARAM_SQL_EXTRA_PARAMS =
       CommonVars.apply("wds.linkis.server.mdm.service.sql.params", "params");
+
+  public static final CommonVars<String> PARAM_SQL_SERVICE_NAME =
+      CommonVars.apply("wds.linkis.server.mdm.service.sql.service.name", "servicename");
 }

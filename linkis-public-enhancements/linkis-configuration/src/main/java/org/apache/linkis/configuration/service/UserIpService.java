26e258e4ca1effef8048659602814c07cfd20a48
v_kkhuang <420895376@qq.com>
2022-11-11 09:32:44 +0800

    Code optimization

--- linkis-public-enhancements/linkis-configuration/src/main/java/org/apache/linkis/configuration/service/UserIpService.java
+++ linkis-public-enhancements/linkis-configuration/src/main/java/org/apache/linkis/configuration/service/UserIpService.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.linkis.configuration.service;
 
 import org.apache.linkis.governance.common.protocol.conf.UserIpRequest;

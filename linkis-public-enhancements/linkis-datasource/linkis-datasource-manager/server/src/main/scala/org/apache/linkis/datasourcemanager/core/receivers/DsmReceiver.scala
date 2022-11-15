66a89da8213b8590eaef978fb1dd742216eccee1
成彬彬 <106590848+binbinCheng@users.noreply.github.com>
2022-11-15 14:07:50 +0800

    [ISSUE-3798]Optimize DsmQueryProtocol (#3803)

--- linkis-public-enhancements/linkis-datasource/linkis-datasource-manager/server/src/main/scala/org/apache/linkis/datasourcemanager/core/receivers/DsmReceiver.scala
+++ linkis-public-enhancements/linkis-datasource/linkis-datasource-manager/server/src/main/scala/org/apache/linkis/datasourcemanager/core/receivers/DsmReceiver.scala
@@ -30,6 +30,7 @@ import org.apache.linkis.rpc.message.annotation.Receiver
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
+import java.text.MessageFormat
 import java.util
 
 import org.slf4j.LoggerFactory
@@ -62,7 +63,13 @@ class DsmReceiver {
           var publishedVersionId = dataSource.getPublishedVersionId
           if (publishedVersionId == null) {
             logger.warn("Datasource name:{} is not published.", dataSource.getDataSourceName)
-            return DsInfoResponse(status = false, "", new util.HashMap[String, Object](), "")
+            return DsInfoResponse(
+              status = false,
+              errorMsg = MessageFormat.format(
+                "Datasource name:{0} is not published.",
+                dataSource.getDataSourceName
+              )
+            )
           }
 
           RestfulApiHelper.decryptPasswordKey(
@@ -71,13 +78,13 @@ class DsmReceiver {
           )
           DsInfoResponse(
             status = true,
-            dataSource.getDataSourceType.getName,
-            dataSource.getConnectParams,
-            dataSource.getCreateUser
+            dsType = dataSource.getDataSourceType.getName,
+            params = dataSource.getConnectParams,
+            creator = dataSource.getCreateUser
           )
         } else {
           logger.warn("Can not get any dataSource")
-          DsInfoResponse(status = true, "", new util.HashMap[String, Object](), "")
+          DsInfoResponse(status = true, errorMsg = "Can not get any dataSource")
         }
       } {
         case e: Exception =>
@@ -85,16 +92,24 @@ class DsmReceiver {
             s"Fail to get data source information, id:${dsInfoQueryRequest.id} system:${dsInfoQueryRequest.system}",
             e
           )
-          DsInfoResponse(status = false, "", new util.HashMap[String, Object](), "")
+          DsInfoResponse(
+            status = false,
+            errorMsg =
+              s"Fail to get data source information, id:${dsInfoQueryRequest.id} system:${dsInfoQueryRequest.system}"
+          )
         case t: Throwable =>
           logger.error(
             s"Fail to get data source information, id:{dsInfoQueryRequest.id} system:${dsInfoQueryRequest.system}",
             t
           )
-          DsInfoResponse(status = false, "", new util.HashMap[String, Object](), "")
+          DsInfoResponse(
+            status = false,
+            errorMsg =
+              s"Fail to get data source information, id:{dsInfoQueryRequest.id} system:${dsInfoQueryRequest.system}"
+          )
       }
     } else {
-      DsInfoResponse(status = true, "", new util.HashMap[String, Object](), "")
+      DsInfoResponse(status = true)
     }
   }
 

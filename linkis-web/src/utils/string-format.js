cb43c6ebba4c2bf5c589a591f6875b9f07e0cda2
jacktao007 <jacktao006@gmail.com>
2022-11-19 19:42:53 +0800

    fix:add apache license

--- linkis-web/src/utils/string-format.js
+++ linkis-web/src/utils/string-format.js
@@ -1,11 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 String.prototype.format = function(args) {
   let result = this;
   if (arguments.length < 1) {
     return result;
   }
-  let data = arguments;		//如果模板参数是数组
+  let data = arguments;
   if (arguments.length == 1 && typeof (args) == "object") {
-    //如果模板参数是对象
     data = args;
   }
   for (let key in data) {

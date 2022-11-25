24b4fbd68338ca838348f9c62264a35fc86ff82f
casionone <casionone@gmail.com>
2022-11-25 11:51:42 +0800

    support wds.linkis.jobhistory.admin

--- linkis-public-enhancements/linkis-jobhistory/src/main/scala/org/apache/linkis/jobhistory/conf/JobhistoryConfiguration.scala
+++ linkis-public-enhancements/linkis-jobhistory/src/main/scala/org/apache/linkis/jobhistory/conf/JobhistoryConfiguration.scala
@@ -22,6 +22,9 @@ import org.apache.linkis.common.conf.{CommonVars, Configuration}
 object JobhistoryConfiguration {
   // modify this param in linkis.properties
   val GOVERNANCE_STATION_ADMIN = Configuration.GOVERNANCE_STATION_ADMIN
+
+  val JOB_HISTORY_ADMIN = CommonVars("wds.linkis.jobhistory.admin", "hadoop")
+
   val JOB_HISTORY_SAFE_TRIGGER = CommonVars("wds.linkis.jobhistory.safe.trigger", true).getValue
 
   val ENTRANCE_SPRING_NAME = CommonVars("wds.linkis.entrance.spring.name", "linkis-cg-entrance")

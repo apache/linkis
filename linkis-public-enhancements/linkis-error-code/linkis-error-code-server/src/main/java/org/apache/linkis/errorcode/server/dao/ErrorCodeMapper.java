4623644660ec609dedc5886c5ec734b08bd421a7
成彬彬 <106590848+binbinCheng@users.noreply.github.com>
2022-12-03 23:13:39 +0800

    Convert annotations to mappers (#3941)

--- linkis-public-enhancements/linkis-error-code/linkis-error-code-server/src/main/java/org/apache/linkis/errorcode/server/dao/ErrorCodeMapper.java
+++ linkis-public-enhancements/linkis-error-code/linkis-error-code-server/src/main/java/org/apache/linkis/errorcode/server/dao/ErrorCodeMapper.java
@@ -20,21 +20,10 @@ package org.apache.linkis.errorcode.server.dao;
 import org.apache.linkis.errorcode.common.LinkisErrorCode;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface ErrorCodeMapper {
-
-  @Select("SELECT * FROM linkis_ps_error_code")
-  @Results({
-    @Result(property = "errorCode", column = "error_code"),
-    @Result(property = "errorDesc", column = "error_desc"),
-    @Result(property = "errorRegexStr", column = "error_regex"),
-    @Result(property = "errorType", column = "error_type"),
-  })
   List<LinkisErrorCode> getAllErrorCodes();
 }

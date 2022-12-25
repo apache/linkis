bc30984b032c0eb5775d458615d330791b1e08d5
guophilipse <guofei_ok@126.com>
2022-12-25 15:26:22 +0800

    fix NPE

--- linkis-computation-governance/linkis-entrance/src/main/scala/org/apache/linkis/entrance/interceptor/impl/SQLCodeCheckInterceptor.scala
+++ linkis-computation-governance/linkis-entrance/src/main/scala/org/apache/linkis/entrance/interceptor/impl/SQLCodeCheckInterceptor.scala
@@ -26,8 +26,15 @@ import org.apache.linkis.manager.label.utils.LabelUtil
 class SQLCodeCheckInterceptor extends EntranceInterceptor {
 
   override def apply(jobRequest: JobRequest, logAppender: java.lang.StringBuilder): JobRequest = {
-    val engineType = LabelUtil.getCodeType(jobRequest.getLabels)
-    val languageType = CodeAndRunTypeUtils.getLanguageTypeByCodeType(engineType.toLowerCase())
+    val engineType = {
+      val engineType = LabelUtil.getCodeType(jobRequest.getLabels)
+      if (null != engineType) {
+        engineType.toLowerCase()
+      } else {
+        ""
+      }
+    }
+    val languageType = CodeAndRunTypeUtils.getLanguageTypeByCodeType(engineType)
     languageType match {
       case CodeAndRunTypeUtils.LANGUAGE_TYPE_SQL =>
         val sb: StringBuilder = new StringBuilder

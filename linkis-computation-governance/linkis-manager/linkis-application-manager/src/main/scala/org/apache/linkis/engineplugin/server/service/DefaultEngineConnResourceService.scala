515e66308d482e4d67418bb25a7ce479a3b22be3
binbinCheng <cheng741201916@163.com>
2023-01-03 17:05:59 +0800

    remove version leading edge

--- linkis-computation-governance/linkis-manager/linkis-application-manager/src/main/scala/org/apache/linkis/engineplugin/server/service/DefaultEngineConnResourceService.scala
+++ linkis-computation-governance/linkis-manager/linkis-application-manager/src/main/scala/org/apache/linkis/engineplugin/server/service/DefaultEngineConnResourceService.scala
@@ -200,6 +200,8 @@ class DefaultEngineConnResourceService extends EngineConnResourceService with Lo
         engineConnBmlResource.setFileName(localizeResource.fileName)
         engineConnBmlResource.setFileSize(localizeResource.fileSize)
         engineConnBmlResource.setLastModified(localizeResource.lastModified)
+        if (version.startsWith("v")) engineConnBmlResource.setVersion(version.substring(1))
+        else engineConnBmlResource.setVersion(version)
         engineConnBmlResource.setVersion(version)
         engineConnBmlResourceDao.save(engineConnBmlResource)
       } else {

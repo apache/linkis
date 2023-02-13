a90242862297e1e6c9eded641729655ffa5cf440
LuckilyTimer <36446084+liaotian1005@users.noreply.github.com>
2023-02-13 08:48:56 +0800

    Update clear-server.sh

--- linkis-dist/package/sbin/clear-server.sh
+++ linkis-dist/package/sbin/clear-server.sh
@@ -1,8 +1,25 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
 source ${LINKIS_HOME}/conf/db.sh
-echo 'delete from  `linkis_cg_manager_label_value_relation` where label_value_key="alias";
-delete from  `linkis_cg_manager_label_value_relation` where label_value_key="serviceName";
-delete from  `linkis_cg_manager_label_value_relation` where label_value_key="instance";
-delete from  `linkis_cg_manager_label` where label_key="engineInstance" or label_key="emInstance";
+echo 'DELETE FROM  `linkis_cg_manager_label_value_relation` where label_value_key="alias";
+DELETE FROM  `linkis_cg_manager_label_value_relation` where label_value_key="serviceName";
+DELETE FROM  `linkis_cg_manager_label_value_relation` where label_value_key="instance";
+DELETE FROM  `linkis_cg_manager_label` where label_key="engineInstance" or label_key="emInstance";
 
 DELETE FROM  `linkis_cg_manager_label_service_instance`;
 DELETE FROM  `linkis_cg_manager_linkis_resources`;

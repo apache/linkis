33abe993be50f223cdd1b8dc8d5b04cb741f18f9
huangKai-2323 <420895376@qq.com>
2023-02-16 10:05:52 +0800

    fix stddev_samp  bug

--- linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/executor/SQLSession.scala
+++ linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/executor/SQLSession.scala
@@ -214,7 +214,15 @@ object SQLSession extends Logging {
           .mkString("{", ",", "}")
 
       case (str: String, StringType) => str.replaceAll("\n|\t", " ")
-      case (double: Double, DoubleType) => nf.format(double)
+      case (double: Double, DoubleType) =>
+        logger.info("double :" + double)
+        if (double.isNaN) {
+          logger.info("double y:" + double)
+          "NaN"
+        } else {
+          logger.info("double n:" + double)
+          nf.format(double)
+        }
       case (decimal: java.math.BigDecimal, DecimalType()) => formatDecimal(decimal)
       case (other: Any, tpe) => other.toString
       case _ => null

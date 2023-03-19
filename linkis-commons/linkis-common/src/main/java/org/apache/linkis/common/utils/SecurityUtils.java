ed1f2e55ef6cab5dce2e7d367e63cbdbd2189c73
aiceflower <kinsanities@sina.com>
2023-03-19 18:59:57 +0800

    mysql connect error

--- linkis-commons/linkis-common/src/main/java/org/apache/linkis/common/utils/SecurityUtils.java
+++ linkis-commons/linkis-common/src/main/java/org/apache/linkis/common/utils/SecurityUtils.java
@@ -178,6 +178,9 @@ public abstract class SecurityUtils {
   private static Map<String, Object> parseMysqlUrlParamsToMap(String paramsUrl) {
     String[] params = paramsUrl.split(AND_SYMBOL);
     Map<String, Object> map = new LinkedHashMap<>(params.length);
+    if (StringUtils.isBlank(paramsUrl)) {
+      return map;
+    }
     for (String param : params) {
       String[] item = param.split(EQUAL_SIGN);
       if (item.length != 2) {

a18fccedaf913d54b45fa5e14c3a396ea9c874c9
huangKai-2323 <62878639+huangKai-2323@users.noreply.github.com>
2023-03-20 21:38:50 +0800

    【1.1.10】Fix the garbled code problem of spark function (#93)
    * fix stddev_samp  bug
    
    * Code optimization

--- linkis-commons/linkis-storage/src/main/scala/org/apache/linkis/storage/domain/DataType.scala
+++ linkis-commons/linkis-storage/src/main/scala/org/apache/linkis/storage/domain/DataType.scala
@@ -79,7 +79,14 @@ object DataType extends Logging {
     case IntType => if (isNumberNull(value)) null else value.toInt
     case LongType | BigIntType => if (isNumberNull(value)) null else value.toLong
     case FloatType => if (isNumberNull(value)) null else value.toFloat
-    case DoubleType => if (isNumberNull(value)) null else value.toDouble
+    case DoubleType =>
+      if (isNumberNull(value)) {
+        null
+      } else if (value.asInstanceOf[Double].isNaN) {
+        "NaN"
+      } else {
+        value.toDouble
+      }
     case DecimalType => if (isNumberNull(value)) null else new JavaBigDecimal(value)
     case DateType => if (isNumberNull(value)) null else Date.valueOf(value)
     case TimestampType =>

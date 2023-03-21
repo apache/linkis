301a7e69f2a04988b014f332b13a07019199bc14
GuoPhilipse <46367746+GuoPhilipse@users.noreply.github.com>
2023-03-21 20:15:18 +0800

    backport spark compatibility #4301 to 1.3.2 (#4395)

--- linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/datacalc/sink/JdbcSink.scala
+++ linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/datacalc/sink/JdbcSink.scala
@@ -17,14 +17,16 @@
 
 package org.apache.linkis.engineplugin.spark.datacalc.sink
 
+import org.apache.linkis.common.utils.ClassUtils.getFieldVal
 import org.apache.linkis.common.utils.Logging
 import org.apache.linkis.engineplugin.spark.datacalc.api.DataCalcSink
 
 import org.apache.commons.lang3.StringUtils
+import org.apache.spark.SPARK_VERSION
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
-import org.apache.spark.sql.execution.datasources.jdbc.{JDBCOptions, JdbcUtils}
+import org.apache.spark.sql.execution.datasources.jdbc.JDBCOptions
 
-import java.sql.Connection
+import java.sql.{Connection, DriverManager}
 
 import scala.collection.JavaConverters._
 
@@ -58,7 +60,8 @@ class JdbcSink extends DataCalcSink[JdbcSinkConfig] with Logging {
         .repartition(1)
         .foreachPartition((_: Iterator[Row]) => {
           val jdbcOptions = new JDBCOptions(options)
-          val conn: Connection = JdbcUtils.createConnectionFactory(jdbcOptions)()
+          val conn: Connection =
+            DriverManager.getConnection(config.getUrl, config.getUser, config.getPassword)
           try {
             config.getPreQueries.asScala.foreach(query => {
               logger.info(s"Execute pre query: $query")
@@ -86,7 +89,12 @@ class JdbcSink extends DataCalcSink[JdbcSinkConfig] with Logging {
     logger.info("Execute query: {}", query)
     val statement = conn.prepareStatement(query)
     try {
-      statement.setQueryTimeout(jdbcOptions.queryTimeout)
+      // `queryTimeout` was added after spark2.4.0, more details please check SPARK-23856
+      if (SPARK_VERSION >= "2.4") {
+        val queryTimeout = getFieldVal(jdbcOptions, "queryTimeout").asInstanceOf[Int]
+        statement.setQueryTimeout(queryTimeout)
+      }
+
       val rows = statement.executeUpdate()
       logger.info("{} rows affected", rows)
     } catch {

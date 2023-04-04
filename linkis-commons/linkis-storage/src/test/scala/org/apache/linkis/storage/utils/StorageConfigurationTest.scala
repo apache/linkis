f545197a4d6716f2d25e4c3e53f6a07b24304cd4
CharlieYan <16526279+CharlieYan24@users.noreply.github.com>
2023-04-04 17:01:59 +0800

    feat : linkis-storage support OSS filesystem support write/read launch log and resultSet in OSS(#4185) (#4424)
    * linkis-storge support oss filesystem(#4185)

--- linkis-commons/linkis-storage/src/test/scala/org/apache/linkis/storage/utils/StorageConfigurationTest.scala
+++ linkis-commons/linkis-storage/src/test/scala/org/apache/linkis/storage/utils/StorageConfigurationTest.scala
@@ -62,7 +62,8 @@ class StorageConfigurationTest {
       storageresultsetclasses
     )
     Assertions.assertEquals(
-      "org.apache.linkis.storage.factory.impl.BuildHDFSFileSystem,org.apache.linkis.storage.factory.impl.BuildLocalFileSystem",
+      "org.apache.linkis.storage.factory.impl.BuildHDFSFileSystem,org.apache.linkis.storage.factory.impl.BuildLocalFileSystem," +
+        "org.apache.linkis.storage.factory.impl.BuildOSSSystem",
       storagebuildfsclasses
     )
     Assertions.assertTrue(issharenode)

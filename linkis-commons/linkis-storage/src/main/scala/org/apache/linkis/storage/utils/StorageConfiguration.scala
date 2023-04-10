f11db0b0234881a24899b59f4ced7721bfedb304
sjgllgh <129264181+sjgllgh@users.noreply.github.com>
2023-04-10 21:08:30 +0800

    feat : linkis-storage support s3 filesystem support write/read launch log and resultSet in s3(#4185) (#4435)
    * linkis-storage support S3 filesystem
    
    * format code
    
    * add comment
    
    * remove wds.
    
    * 1. format code 2. add dependant license
    
    * format linkis-storage.pom
    
    * format linkis-storage.pom
    
    * format code
    
    ---------
    
    Co-authored-by: zyyBjtu <20431508@qq.com>

--- linkis-commons/linkis-storage/src/main/scala/org/apache/linkis/storage/utils/StorageConfiguration.scala
+++ linkis-commons/linkis-storage/src/main/scala/org/apache/linkis/storage/utils/StorageConfiguration.scala
@@ -48,7 +48,7 @@ object StorageConfiguration {
   val STORAGE_BUILD_FS_CLASSES = CommonVars(
     "wds.linkis.storage.build.fs.classes",
     "org.apache.linkis.storage.factory.impl.BuildHDFSFileSystem,org.apache.linkis.storage.factory.impl.BuildLocalFileSystem," +
-      "org.apache.linkis.storage.factory.impl.BuildOSSSystem"
+      "org.apache.linkis.storage.factory.impl.BuildOSSSystem,org.apache.linkis.storage.factory.impl.BuildS3FileSystem"
   )
 
   val IS_SHARE_NODE = CommonVars("wds.linkis.storage.is.share.node", true)
@@ -112,4 +112,15 @@ object StorageConfiguration {
     CommonVars[Boolean]("wds.linkis.storage.oss.prefix_check.enable", false)
 
   val OSS_PATH_PREFIX_REMOVE = CommonVars[Boolean]("wds.linkis.storage.oss.prefix.remove", true)
+
+  val S3_ACCESS_KEY = CommonVars("linkis.storage.s3.access.key", "")
+
+  val S3_SECRET_KEY =
+    CommonVars("linkis.storage.s3.secret.key", "")
+
+  val S3_ENDPOINT = CommonVars("linkis.storage.s3.endpoint", "")
+
+  val S3_REGION = CommonVars("linkis.storage.s3.region", "")
+
+  val S3_BUCKET = CommonVars("linkis.storage.s3.bucket", "")
 }

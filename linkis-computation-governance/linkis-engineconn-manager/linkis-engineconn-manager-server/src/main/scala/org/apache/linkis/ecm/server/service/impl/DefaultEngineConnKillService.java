0f1005c4310a527f73ff3e060e11ab49d4e60650
huangKai-2323 <62878639+huangKai-2323@users.noreply.github.com>
2023-04-14 23:04:58 +0800

    optimize engine log (#4455)

--- linkis-computation-governance/linkis-engineconn-manager/linkis-engineconn-manager-server/src/main/scala/org/apache/linkis/ecm/server/service/impl/DefaultEngineConnKillService.java
+++ linkis-computation-governance/linkis-engineconn-manager/linkis-engineconn-manager-server/src/main/scala/org/apache/linkis/ecm/server/service/impl/DefaultEngineConnKillService.java
@@ -5,16 +5,16 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package org.apache.linkis.ecm.server.service.impl;
 
 import org.apache.commons.io.IOUtils;
@@ -98,7 +98,7 @@ public class DefaultEngineConnKillService implements EngineConnKillService {
         String engineConnInstance = engineConn.getServiceInstance().toString();
         logger.info("try to kill yarn app ids in the engine of ({}).", engineConnInstance);
         String engineLogDir = engineConn.getEngineConnManagerEnv().engineConnLogDirs();
-        final String errEngineLogPath = engineLogDir.concat(File.separator).concat("yarnApp.log");
+        final String errEngineLogPath = engineLogDir.concat(File.separator).concat("yarnApp");
         logger.info("try to parse the yarn app id from the engine err log file path: {}", errEngineLogPath);
         File file = new File(errEngineLogPath);
         if (file.exists())

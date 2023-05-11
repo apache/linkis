025fe3e8ddc2286ee7ffcbe0e3a94068e7f9cdbc
ChengJie1053 <18033291053@163.com>
2023-05-11 18:33:37 +0800

    Remove useless code

--- linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/SparkEngineConnPlugin.scala
+++ linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/SparkEngineConnPlugin.scala
@@ -38,13 +38,9 @@ class SparkEngineConnPlugin extends EngineConnPlugin {
 
   private val resourceLocker = new Object()
 
-  private val engineLaunchBuilderLocker = new Object()
-
   private val engineFactoryLocker = new Object()
 
   private var engineResourceFactory: EngineResourceFactory = _
-
-  private var engineLaunchBuilder: EngineConnLaunchBuilder = _
 
   private var engineFactory: EngineConnFactory = _
 

63c66b1a96f7a2f9f31e9650c73d9a046cb4eb43
peacewong <wpeace1212@gmail.com>
2023-05-19 12:10:27 +0800

    [Gateway]All backend caches should be cleared when the user logs out (#4548)
    * Fix log out cookie not to clear
    
    * Task progress exceeding 1 will give fake progress

--- linkis-computation-governance/linkis-computation-governance-common/src/main/scala/org/apache/linkis/governance/common/conf/GovernanceCommonConf.scala
+++ linkis-computation-governance/linkis-computation-governance-common/src/main/scala/org/apache/linkis/governance/common/conf/GovernanceCommonConf.scala
@@ -75,6 +75,8 @@ object GovernanceCommonConf {
   val ERROR_CODE_DESC_LEN =
     CommonVars("linkis.error.code.desc.len", 512, "Error code description maximum length").getValue
 
+  val FAKE_PROGRESS: Float = CommonVars[Float]("linkis.job.fake.progress", 0.99f).getValue
+
   def getEngineEnvValue(envKey: String): String = {
     CommonVars(envKey, "").getValue
   }

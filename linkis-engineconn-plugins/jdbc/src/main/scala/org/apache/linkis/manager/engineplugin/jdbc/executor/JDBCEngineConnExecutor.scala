46c2a2b54218d9283c4009d0806d11de8fee6435
aiceflower <kinghao007@gmail.com>
2023-06-19 01:28:11 +0800

    Update JDBCEngineConnExecutor.scala
    add annotation

--- linkis-engineconn-plugins/jdbc/src/main/scala/org/apache/linkis/manager/engineplugin/jdbc/executor/JDBCEngineConnExecutor.scala
+++ linkis-engineconn-plugins/jdbc/src/main/scala/org/apache/linkis/manager/engineplugin/jdbc/executor/JDBCEngineConnExecutor.scala
@@ -115,10 +115,12 @@ class JDBCEngineConnExecutor(override val outputPrintLimit: Int, val id: Int)
     var resultSet: ResultSet = null
     logger.info(s"The data source properties is $properties")
     Utils.tryCatch({
+      /* url + user as the cache key */
       val jdbcUrl: String = properties.get(JDBCEngineConnConstant.JDBC_URL)
       val execUser: String = properties.get(JDBCEngineConnConstant.JDBC_SCRIPTS_EXEC_USER)
       val proxyUser: String = properties.get(JDBCEngineConnConstant.JDBC_PROXY_USER_PROPERTY)
       var dataSourceIdentifier = s"$jdbcUrl-$execUser-$proxyUser"
+      /* If datasource is used, use datasource name as the cache key */
       if (StringUtils.isNotBlank(dataSourceName)) {
         dataSourceIdentifier = s"$dataSourceName-$dataSourceMaxVersionId"
       }

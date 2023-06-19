af30f6c98646d150e410fcd39f094502504fb089
peacewong <wpeace1212@gmail.com>
2023-06-19 16:13:16 +0800

    Fix build issue

--- linkis-commons/linkis-storage/src/main/scala/org/apache/linkis/storage/utils/FileSystemUtils.scala
+++ linkis-commons/linkis-storage/src/main/scala/org/apache/linkis/storage/utils/FileSystemUtils.scala
@@ -22,10 +22,9 @@ import org.apache.linkis.common.utils.{Logging, Utils}
 import org.apache.linkis.storage.FSFactory
 import org.apache.linkis.storage.fs.FileSystem
 import org.apache.linkis.storage.fs.impl.LocalFileSystem
-import org.apache.linkis.storage.source.{FileSource, FileSource$}
+import org.apache.linkis.storage.source.FileSource
 
 import org.apache.commons.io.IOUtils
-import org.apache.commons.math3.util.Pair
 
 import java.io.IOException
 import java.util

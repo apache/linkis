57ba18539fa19e5a32c5a14a98f5a7daa6c23cf1
peacewong <wpeace1212@gmail.com>
2023-06-29 11:18:29 +0800

    update default value to ""

--- linkis-engineconn-plugins/impala/src/main/scala/org/apache/linkis/engineplugin/impala/conf/ImpalaConfiguration.scala
+++ linkis-engineconn-plugins/impala/src/main/scala/org/apache/linkis/engineplugin/impala/conf/ImpalaConfiguration.scala
@@ -38,31 +38,31 @@ object ImpalaConfiguration {
   val IMPALA_MAX_CONNECTIONS = CommonVars[Int]("linkis.impala.maxConnections", 10)
 
   val IMPALA_SSL_ENABLE = CommonVars[Boolean]("linkis.impala.ssl.enable", false)
-  val IMPALA_SSL_KEYSTORE = CommonVars[String]("linkis.impala.ssl.keystore", null)
+  val IMPALA_SSL_KEYSTORE = CommonVars[String]("linkis.impala.ssl.keystore", "")
 
   val IMPALA_SSL_KEYSTORE_TYPE =
     CommonVars[String]("linkis.impala.ssl.keystore.type", KeyStore.getDefaultType)
 
-  val IMPALA_SSL_KEYSTORE_PASSWORD = CommonVars[String]("linkis.impala.ssl.keystore.password", null)
-  val IMPALA_SSL_TRUSTSTORE = CommonVars[String]("linkis.impala.ssl.truststore", null)
+  val IMPALA_SSL_KEYSTORE_PASSWORD = CommonVars[String]("linkis.impala.ssl.keystore.password", "")
+  val IMPALA_SSL_TRUSTSTORE = CommonVars[String]("linkis.impala.ssl.truststore", "")
 
   val IMPALA_SSL_TRUSTSTORE_TYPE =
     CommonVars[String]("linkis.impala.ssl.truststore.type", KeyStore.getDefaultType)
 
   val IMPALA_SSL_TRUSTSTORE_PASSWORD =
-    CommonVars[String]("linkis.impala.ssl.truststore.password", null)
+    CommonVars[String]("linkis.impala.ssl.truststore.password", "")
 
   val IMPALA_SASL_ENABLE = CommonVars[Boolean]("linkis.impala.sasl.enable", false)
   val IMPALA_SASL_MECHANISM = CommonVars[String]("linkis.impala.sasl.mechanism", "PLAIN")
-  val IMPALA_SASL_AUTHORIZATION_ID = CommonVars[String]("linkis.impala.sasl.authorizationId", null)
+  val IMPALA_SASL_AUTHORIZATION_ID = CommonVars[String]("linkis.impala.sasl.authorizationId", "")
   val IMPALA_SASL_PROTOCOL = CommonVars[String]("linkis.impala.sasl.protocol", "LDAP")
-  val IMPALA_SASL_PROPERTIES = CommonVars[String]("linkis.impala.sasl.properties", null)
+  val IMPALA_SASL_PROPERTIES = CommonVars[String]("linkis.impala.sasl.properties", "")
   val IMPALA_SASL_USERNAME = CommonVars("linkis.impala.sasl.username", IMPALA_ENGINE_USER.getValue)
-  val IMPALA_SASL_PASSWORD = CommonVars[String]("linkis.impala.sasl.password", null)
-  val IMPALA_SASL_PASSWORD_CMD = CommonVars[String]("linkis.impala.sasl.password.cmd", null)
+  val IMPALA_SASL_PASSWORD = CommonVars[String]("linkis.impala.sasl.password", "")
+  val IMPALA_SASL_PASSWORD_CMD = CommonVars[String]("linkis.impala.sasl.password.cmd", "")
 
   val IMPALA_HEARTBEAT_SECONDS = CommonVars[Int]("linkis.impala.heartbeat.seconds", 1)
   val IMPALA_QUERY_TIMEOUT_SECONDS = CommonVars[Int]("linkis.impala.query.timeout.seconds", 0)
   val IMPALA_QUERY_BATCH_SIZE = CommonVars[Int]("linkis.impala.query.batchSize", 1000)
-  val IMPALA_QUERY_OPTIONS = CommonVars[String]("linkis.impala.query.options", null)
+  val IMPALA_QUERY_OPTIONS = CommonVars[String]("linkis.impala.query.options", "")
 }

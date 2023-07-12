c61612d2e8ffa0e44042955201bab1e3c9032d24
人生有如两个橘子 <15764973965@163.com>
2023-07-12 21:44:36 +0800

    [bugfix] Append code `val linkisVar=1` to prevent bugs that do not exit tasks for a long time (#4760)

--- linkis-computation-governance/linkis-computation-governance-common/src/test/scala/org/apache/linkis/governance/common/paser/ScalaCodeParserTest.scala
+++ linkis-computation-governance/linkis-computation-governance-common/src/test/scala/org/apache/linkis/governance/common/paser/ScalaCodeParserTest.scala
@@ -29,8 +29,7 @@ class ScalaCodeParserTest {
       "val codeBuffer = new ArrayBuffer[String]()\n    val statementBuffer = new ArrayBuffer[String]()"
     val scalaCodeParser = new ScalaCodeParser
     val array = scalaCodeParser.parse(scalaCode)
-    Assertions.assertTrue(array.size == 1)
-
+    Assertions.assertTrue(array.length == 2)
   }
 
   @Test
@@ -41,7 +40,7 @@ class ScalaCodeParserTest {
       "   def addInt( a:Int, b:Int )\n      var sum:Int = 0\n      sum = a + b\n      return sum\n   }"
     val scalaCodeParser = new ScalaCodeParser
     val array = scalaCodeParser.parse(abnormalCode)
-    Assertions.assertTrue(array.length == 1)
+    Assertions.assertTrue(array.length == 2)
 
   }
 
@@ -54,7 +53,7 @@ class ScalaCodeParserTest {
 
     val scalaCodeParser = new ScalaCodeParser
     val array = scalaCodeParser.parse(importCode)
-    Assertions.assertTrue(array.length == 2)
+    Assertions.assertTrue(array.length == 3)
 
   }
 
@@ -68,7 +67,7 @@ class ScalaCodeParserTest {
     val scalaCodeParser = new ScalaCodeParser
     val arrayResult1 = scalaCodeParser.parse(specialCodeExp1)
 
-    Assertions.assertTrue(arrayResult1.length == 2)
+    Assertions.assertTrue(arrayResult1.length == 3)
 
     val specialCodeExp2 =
       "  @BeanProperty\n  var id: Long = _\n  @BeanProperty\n  var status: Int = 0\n  " +
@@ -79,7 +78,7 @@ class ScalaCodeParserTest {
         ".append(data, that.data)\n      .isEquals\n  }"
 
     val arrayResult2 = scalaCodeParser.parse(specialCodeExp2)
-    Assertions.assertTrue(arrayResult2.length == 1)
+    Assertions.assertTrue(arrayResult2.length == 2)
 
   }
 

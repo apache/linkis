de7ad07ddfe15bca10712abbd81e37d29f5f30dd
人生有如两个橘子 <15764973965@163.com>
2023-07-15 23:52:03 +0800

    [Bugfix] Task state changes should before engine state changes (#4775)
    this close #4774

--- linkis-computation-governance/linkis-engineconn/linkis-computation-engineconn/src/main/scala/org/apache/linkis/engineconn/computation/executor/execute/ComputationExecutor.scala
+++ linkis-computation-governance/linkis-engineconn/linkis-computation-engineconn/src/main/scala/org/apache/linkis/engineconn/computation/executor/execute/ComputationExecutor.scala
@@ -21,7 +21,10 @@ import org.apache.linkis.DataWorkCloudApplication
 import org.apache.linkis.common.log.LogUtils
 import org.apache.linkis.common.utils.{Logging, Utils}
 import org.apache.linkis.engineconn.acessible.executor.entity.AccessibleExecutor
-import org.apache.linkis.engineconn.acessible.executor.listener.event.TaskStatusChangedEvent
+import org.apache.linkis.engineconn.acessible.executor.listener.event.{
+  TaskResponseErrorEvent,
+  TaskStatusChangedEvent
+}
 import org.apache.linkis.engineconn.common.conf.{EngineConnConf, EngineConnConstant}
 import org.apache.linkis.engineconn.computation.executor.conf.ComputationExecutorConf
 import org.apache.linkis.engineconn.computation.executor.entity.EngineConnTask
@@ -237,11 +240,9 @@ abstract class ComputationExecutor(val outputPrintLimit: Int = 1000)
       response = response match {
         case _: OutputExecuteResponse =>
           succeedTasks.increase()
-          transformTaskStatus(engineConnTask, ExecutionNodeStatus.Succeed)
           SuccessExecuteResponse()
         case s: SuccessExecuteResponse =>
           succeedTasks.increase()
-          transformTaskStatus(engineConnTask, ExecutionNodeStatus.Succeed)
           s
         case _ => response
       }
@@ -261,7 +262,17 @@ abstract class ComputationExecutor(val outputPrintLimit: Int = 1000)
     taskCache.put(engineConnTask.getTaskId, engineConnTask)
     lastTask = engineConnTask
     val response = ensureOp {
-      toExecuteTask(engineConnTask)
+      val executeResponse = toExecuteTask(engineConnTask)
+      executeResponse match {
+        case successExecuteResponse: SuccessExecuteResponse =>
+          transformTaskStatus(engineConnTask, ExecutionNodeStatus.Succeed)
+        case errorExecuteResponse: ErrorExecuteResponse =>
+          listenerBusContext.getEngineConnSyncListenerBus.postToAll(
+            TaskResponseErrorEvent(engineConnTask.getTaskId, errorExecuteResponse.message)
+          )
+          transformTaskStatus(engineConnTask, ExecutionNodeStatus.Failed)
+      }
+      executeResponse
     }
 
     Utils.tryAndWarn(afterExecute(engineConnTask, response))

5e584ab30192bbe146c4096f44d1fcf446f0636c
aiceflower <kinsanities@sina.com>
2023-07-25 20:54:18 +0800

    update vertion to 1.4.0

--- linkis-dist/deploy-config/linkis-env.sh
+++ linkis-dist/deploy-config/linkis-env.sh
@@ -160,7 +160,7 @@ export SERVER_HEAP_SIZE="512M"
 ##The extended lib such mysql-connector-java-*.jar
 #LINKIS_EXTENDED_LIB=/appcom/common/linkisExtendedLib
 
-LINKIS_VERSION=1.3.2
+LINKIS_VERSION=1.4.0
 
 # for install
 LINKIS_PUBLIC_MODULE=lib/linkis-commons/public-module

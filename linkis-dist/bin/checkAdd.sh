bb26f6955c9d95e42d016e2e64a2ad2730374ac3
peter.peng <153836542@qq.com>
2023-09-28 04:30:01 +0800

    update checkAdd.sh : 1, format comments for ---1,---2,---3 2, Indicate parameter define files

--- linkis-dist/bin/checkAdd.sh
+++ linkis-dist/bin/checkAdd.sh
@@ -16,6 +16,9 @@
 
 shellDir=`dirname $0`
 workDir=`cd ${shellDir}/..;pwd`
+DBParDir=${workDir}/deploy-config/db.sh
+LinkisParDir=${workDir}/deploy-config/linkis-env.sh
+
 source ${workDir}/bin/common.sh
 source ${workDir}/deploy-config/linkis-env.sh
 source ${workDir}/deploy-config/db.sh
@@ -23,7 +26,7 @@ source ${workDir}/deploy-config/db.sh
 function print_usage(){
   echo "Usage: checkAdd [EngineName]"
   echo " EngineName : The Engine name that you want to check"
-  echo " Engine list as bellow: JDBC Flink openLooKeng Pipeline Presto Sqoop Elasticsearch Impala Trino Seatunnel"
+  echo " Engine list as bellow: JDBC Flink openLooKeng  Presto Sqoop Elasticsearch Impala Trino Seatunnel"
 }
 
 if [ $# -gt 1 ]; then
@@ -31,26 +34,26 @@ if [ $# -gt 1 ]; then
   exit 2
 fi
 
-# --- Define verification functions for addtional engines: 1,check command;2,check parameters;3,check server status.
+# Define verification functions for addtional engines: 1,check command;2,check parameters;3,check server status.
 
 function checkJDBC(){
 
-# --- 1. check command
+# 1. check command
     java -version > /dev/null 2>&1
     isSuccess "execute cmd: java -version"
 
-# --- 2. check parameters
+# 2. check parameters
      if [ -z "${MYSQL_HOST}" ] || [ -z "${MYSQL_PORT}" ] || [ -z "${MYSQL_DB}" ] || [ -z "${MYSQL_USER}" ] || [ -z "${MYSQL_PASSWORD}" ];then
-       echo "MYSQL_HOST/MYSQL_PORT/MYSQL_USER/MYSQL_PASSWORD] are  Invalid,Pls check parameter define"
+       echo "[MYSQL_HOST/MYSQL_PORT/MYSQL_USER/MYSQL_PASSWORD] are Invalid,Pls check parameters  in ${DBParDir} "
        exit 2
      fi
 
      if [ ! -f ${MYSQL_CONNECT_JAVA_PATH} ];then
-       echo "MysQl connector_java_path is not exist" ${MYSQL_CONNECT_JAVA_PATH}
+       echo "Mysql connector ${MYSQL_CONNECT_JAVA_PATH} is not exist"
        exit 2
      fi
 
-# --- 3. check server status
+# 3. check server status
 # 设置Java类路径，指向你所下载的MySQL JDBC驱动程序的JAR文件和其他依赖项
 CLASSPATH=$CLASSPATH:${MYSQL_CONNECT_JAVA_PATH}
 
@@ -88,17 +91,17 @@ public class JdbcTest {
 
 function checkFlink(){
 
-# --- 1. check command
+# 1. check command
     flink --version > /dev/null 2>&1
     isSuccess "execute cmd: flink--version"
 
-# --- 2. check parameters
+# 2. check parameters
       if [ -z ${FLINK_HOME} ];then
-         echo "Parameter FLINK_HOME is Invalid,Pls check"
+         echo "Parameter [FLINK_HOME] is Invalid,Pls check parameters  in ${LinkisParDir} "
          exit 2
       fi
 
-# --- 3. check server status
+# 3. check server status
     cd ${FLINK_HOME}
     ./bin/flink run -m yarn-cluster ./examples/batch/WordCount.jar > /dev/null 2>&1
     isSuccess "execute cmd: Flink run -m yarn-cluster "
@@ -107,15 +110,19 @@ function checkFlink(){
 
 function checkopenLooKeng(){
 
-# --- 2. check parameters
+# 2. check parameters
      if [ -z "${OLK_HOST}" ] || [ -z "${OLK_PORT}" ] || [ -z "${OLK_CATALOG}" ]|| [ -z "${OLK_SCHEMA}" ] || [ -z "${OLK_USER}" ] || [ -z "${OLK_PASSWORD}" ];then
-       echo "OLK_HOST/OLK_PORT/OLK_USER/OLK_PASSWORD] are  Invalid,Pls check parameter define"
+       echo "[OLK_HOST/OLK_PORT/OLK_USER/OLK_PASSWORD] are  Invalid,Pls check parameters in ${DBParDir} "
        exit 2
      fi
 
-# --- 3. check server status
+     if [ ! -f ${OLK_JDBC_PATH} ];then
+       echo "openLooKeng connector ${OLK_JDBC_PATH} is not exist"
+       exit 2
+     fi
+# 3. check server status
 # 设置Java类路径，指向你所下载的MySQL JDBC驱动程序的JAR文件和其他依赖项
-CLASSPATH=$CLASSPATH:/data/test/bin/hetu-jdbc-1.10.0.jar
+CLASSPATH=$CLASSPATH:${OLK_JDBC_PATH}
 
 # 编写Java代码，尝试建立JDBC连接并验证
 echo "import java.sql.*;
@@ -152,100 +159,99 @@ rm openLooKengTest.*
 
 function checkPresto(){
 
-# --- 1. check command
+# 1. check command
     presto --version > /dev/null 2>&1
     isSuccess "execute cmd: presto --version"
 
-# --- 2. check parameters
+# 2. check parameters
       if [ -z "${PRESTO_HOST}" ] || [ -z "${PRESTO_PORT}" ] || [ -z "${PRESTO_CATALOG}" ]|| [ -z "${PRESTO_SCHEMA}"  ];then
-       echo "PRESTO_HOST/PRESTO_PORT/PRESTO_CATALOG/PRESTO_SCHEMA] are  Invalid,Pls check parameters definition"
+       echo "[PRESTO_HOST/PRESTO_PORT/PRESTO_CATALOG/PRESTO_SCHEMA] are  Invalid,Pls check parameters in ${DBParDir}"
          exit 2
       fi
 
-# --- 3. check server status
+# 3. check server status
     presto --server ${PRESTO_HOST}:${PRESTO_PORT} --catalog ${PRESTO_CATALOG} --schema ${PRESTO_SCHEMA} --execute "show catalogs" >/dev/null 2>&1
     isSuccess "execute cmd: presto --server ${PRESTO_HOST}:${PRESTO_PORT}"
 }
 
 function checkSqoop(){
 
-# --- 1. check command
+# 1. check command
     sqoop version > /dev/null 2>&1
     isSuccess "execute cmd: sqoop version"
 
-# --- 2. check parameters
+# 2. check parameters
       if [ -z "${HIVE_META_URL}" ] || [ -z "${HIVE_META_USER}" ] || [ -z "${HIVE_META_PASSWORD}" ];then
-       echo "[HIVE_META_URL/HIVE_META_USER/HIVE_META_PASSWORD] are  Invalid,Pls check parameters definition"
+       echo "[HIVE_META_URL/HIVE_META_USER/HIVE_META_PASSWORD] are  Invalid,Pls check parameters in ${DBParDir}"
          exit 2
       fi
 
-# --- 3. check server status
+# 3. check server status
     sqoop list-databases --connect ${HIVE_META_URL} --username ${HIVE_META_USER} --password ${HIVE_META_PASSWORD} >/dev/null 2>&1
     isSuccess "execute cmd: sqoop list-databases --connect ${HIVE_META_URL}"
     }
 
 function checkElasticsearch(){
 
-# --- 2. check parameters
+# 2. check parameters
       if [ -z "${ES_RESTFUL_URL}" ]; then
-         echo "Parameter ES_RESTFUL_URL is Invalid:" ${ES_RESTFUL_URL}
-         exit 2
+         echo "Parameter [ES_RESTFUL_URL] is Invalid,Pls check parameters in ${LinkisParDir}"
       fi
 
-# --- 3. check server status
+# 3. check server status
     curl ${ES_RESTFUL_URL} > /dev/null 2>&1
     isSuccess "execute cmd: curl ElasticSearch address ${ES_RESTFUL_URL}"
 }
 
 function checkImpala(){
 
-# --- 1. check command
+# 1. check command
     impala-shell --version > /dev/null 2>&1
     isSuccess "execute cmd: impala-shell --version"
 
-# --- 2. check parameters
+# 2. check parameters
       if [ -z "${IMPALA_HOST}" ] || [ -z "${IMPALA_PORT}" ];then
-         echo "Parameter [IMPALA_HOST/IMPALA_PORT] are Invalid ,Pls check parameters definition"
+         echo "Parameter [IMPALA_HOST/IMPALA_PORT] are Invalid ,Pls check parameters in ${DBParDir}"
          exit 2
       fi
 
-# --- 3. check server status
+# 3. check server status
     impala-shell -i ${IMPALA_HOST}:${IMPALA_PORT} > /dev/null 2>&1
     isSuccess "execute cmd: impala-shell -i ${IMPALA_HOST}:${IMPALA_PORT}"
 
 }
 
 function checkTrino(){
-# --- 1. check command
+# 1. check command
     trino-cli --version > /dev/null 2>&1
     isSuccess "execute cmd: trino-shell --version"
 
-# --- 2. check parameters
+# 2. check parameters
       if [ -z "${TRINO_COORDINATOR_HOST}" ] || [ -z "${TRINO_COORDINATOR_PORT}" ];then
-         echo "Parameter [TRINO_COORDINATOR_HOST/TRINO_COORDINATOR_PORT] are Invalid ,Pls check parameters definition"
+         echo "Parameter [TRINO_COORDINATOR_HOST/TRINO_COORDINATOR_PORT] are Invalid ,Pls check parameters in ${DBParDir}"
          exit 2
       fi
 
-# -- 3. check server status
+# 3. check server status
     trino-cli --server ${TRINO_COORDINATOR_HOST}:${TRINO_COORDINATOR_PORT} --catalog ${TRINO_COORDINATOR_CATALOG} --schema ${TRINO_COORDINATOR_SCHEMA} --execute "show catalogs" > /dev/null 2>&1
     isSuccess "trino-cli --server ${TRINO_COORDINATOR_HOST}:${TRINO_COORDINATOR_PORT}"
 }
 
 function checkSeatunnel(){
 
-# --- 2. check parameters
+# 2. check parameters
       if [ -z "${SEATUNNEL_HOST}" ] || [ -z "${SEATUNNEL_PORT}" ];then
-         echo "Parameter [SEATUNNEL_HOST/SEATUNNEL_PORT] are Invalid ,Pls check parameters definition"
+         echo "Parameter [SEATUNNEL_HOST/SEATUNNEL_PORT] are Invalid ,Pls check parameters in ${DBParDir}"
          exit 2
       fi
 
-# --- 3. check server status
+# 3. check server status
     curl http://${SEATUNNEL_HOST}:${SEATUNNEL_PORT} > /dev/null 2>&1
     isSuccess "execute cmd: curl http://${SEATUNNEL_HOST}:${SEATUNNEL_PORT}"
 }
 
 
-# --- Begin check addtional engine parameters
+# Begin check addtional engine parameters
 echo "======== Begin to check Engine: ${1} ======== "
 
 EngineName=$1

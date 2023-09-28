5b9a6886bb9278b223ae2b2a15dbde5dda19c3da
peter.peng <153836542@qq.com>
2023-09-28 18:57:23 +0800

    1, update db.sh: add db information for addtional engines; 2,update linkis-env.sh, add MySQL and Openlookeng JDBC PATH defination.

--- linkis-dist/deploy-config/linkis-env.sh
+++ linkis-dist/deploy-config/linkis-env.sh
@@ -178,6 +178,6 @@ export ENABLE_HDFS=true
 export ENABLE_HIVE=true
 export ENABLE_SPARK=true
 
-## if MYSQL_CONNECT_JAVA_PATH is set, the linkis can check JDBC driver
+## define  MYSQL_CONNECT_JAVA_PATH&OLK_JDBC_PATH, the linkis can check JDBC driver
 MYSQL_CONNECT_JAVA_PATH=
 OLK_JDBC_PATH=

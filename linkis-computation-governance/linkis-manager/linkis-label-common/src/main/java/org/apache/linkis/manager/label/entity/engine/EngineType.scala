524117b5caab2c27a27b53b1ec2da67a27383611
ChengJie1053 <18033291053@163.com>
2023-10-11 19:49:43 +0800

    Add repl ec to execute Java and scala code

--- linkis-computation-governance/linkis-manager/linkis-label-common/src/main/java/org/apache/linkis/manager/label/entity/engine/EngineType.scala
+++ linkis-computation-governance/linkis-manager/linkis-label-common/src/main/java/org/apache/linkis/manager/label/entity/engine/EngineType.scala
@@ -5,16 +5,16 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package org.apache.linkis.manager.label.entity.engine
 
 import org.apache.linkis.common.utils.Logging
@@ -32,6 +32,8 @@ object EngineType extends Enumeration with Logging {
   val HBASE = Value("hbase")
 
   val PYTHON = Value("python")
+
+  val REPL = Value("repl")
 
   val SHELL = Value("shell")
 
@@ -95,6 +97,7 @@ object EngineType extends Enumeration with Logging {
     case _ if PIPELINE.toString.equalsIgnoreCase(str) => PIPELINE
     case _ if PRESTO.toString.equalsIgnoreCase(str) => PRESTO
     case _ if NEBULA.toString.equalsIgnoreCase(str) => NEBULA
+    case _ if REPL.toString.equalsIgnoreCase(str) => REPL
     case _ if FLINK.toString.equalsIgnoreCase(str) => FLINK
     case _ if APPCONN.toString.equals(str) => APPCONN
     case _ if SQOOP.toString.equalsIgnoreCase(str) => SQOOP

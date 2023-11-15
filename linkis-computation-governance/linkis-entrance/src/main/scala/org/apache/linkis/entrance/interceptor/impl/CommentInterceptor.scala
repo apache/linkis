6c2f29c847e2b3b528374500cc39c814f0e300f8
sjgllgh <sjgl_lgh@163.com>
2023-11-15 15:13:10 +0800

    sql field comment semicolon with escape

--- linkis-computation-governance/linkis-entrance/src/main/scala/org/apache/linkis/entrance/interceptor/impl/CommentInterceptor.scala
+++ linkis-computation-governance/linkis-entrance/src/main/scala/org/apache/linkis/entrance/interceptor/impl/CommentInterceptor.scala
@@ -70,7 +70,31 @@ trait CommentHelper {
 object SQLCommentHelper extends CommentHelper {
   override val commentPattern: Regex = """\s*--.+\s*""".r.unanchored
   private val comment = "(?ms)('(?:''|[^'])*')|--.*?$|/\\*.*?\\*/|#.*?$|"
+  private val comment_sem = "(?i)(comment)\\s+'([^']*)'"
   private val logger: Logger = LoggerFactory.getLogger(getClass)
+
+  def replaceComment(code: String): String = {
+    try {
+      val pattern = Pattern.compile(comment_sem)
+      val matcher = pattern.matcher(code)
+      val sb = new StringBuffer
+      while (matcher.find()) {
+        val commentKeyword = matcher.group(1)
+        val comment = matcher.group(2)
+        val escapedComment = comment.replaceAll(";", "\\\\\\\\;")
+        matcher.appendReplacement(sb, commentKeyword + " '" + escapedComment + "'")
+      }
+      matcher.appendTail(sb)
+      sb.toString
+    } catch {
+      case e: Exception =>
+        logger.warn("sql comment semicolon replace failed")
+        code
+      case t: Throwable =>
+        logger.warn("sql comment semicolon replace failed")
+        code
+    }
+  }
 
   override def dealComment(code: String): String = {
     try {

5789c3b2259f7eca52c8c3adc7fedebd52c4166d
ChengJie1053 <18033291053@163.com>
2023-11-28 22:42:05 +0800

    Modify flink engine version (#4993)

--- linkis-computation-governance/linkis-manager/linkis-label-common/src/main/java/org/apache/linkis/manager/label/conf/LabelCommonConfig.java
+++ linkis-computation-governance/linkis-manager/linkis-label-common/src/main/java/org/apache/linkis/manager/label/conf/LabelCommonConfig.java
@@ -64,7 +64,7 @@ public class LabelCommonConfig {
       CommonVars.apply("wds.linkis.appconn.engine.version", "1");
 
   public static final CommonVars<String> FLINK_ENGINE_VERSION =
-      CommonVars.apply("wds.linkis.flink.engine.version", "1.12.2");
+      CommonVars.apply("wds.linkis.flink.engine.version", "1.16.2");
 
   public static final CommonVars<String> SQOOP_ENGINE_VERSION =
       CommonVars.apply("wds.linkis.sqoop.engine.version", "1.4.6");

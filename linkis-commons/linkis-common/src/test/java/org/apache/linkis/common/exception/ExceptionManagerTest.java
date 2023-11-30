07f2e4e6071182fda5a11aadd52984cf6d208dbd
yijut2 <52221089+yijut2@users.noreply.github.com>
2023-11-30 04:07:50 -0600

    Update ExceptionManagerTest.java (#5005)
    fix flaky test

--- linkis-commons/linkis-common/src/test/java/org/apache/linkis/common/exception/ExceptionManagerTest.java
+++ linkis-commons/linkis-common/src/test/java/org/apache/linkis/common/exception/ExceptionManagerTest.java
@@ -21,6 +21,7 @@ import org.apache.linkis.common.errorcode.CommonErrorConstants;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.junit.jupiter.api.Test;
 
@@ -40,7 +41,7 @@ class ExceptionManagerTest {
                 + "null");
     assertEquals(errorException.getClass(), ExceptionManager.generateException(null).getClass());
     assertEquals(errorException.toString(), ExceptionManager.generateException(null).toString());
-    Map<String, Object> map = new HashMap<>();
+    Map<String, Object> map = new TreeMap<>();
     map.put("level", null);
     map.put("errCode", 1);
     map.put("desc", "test");

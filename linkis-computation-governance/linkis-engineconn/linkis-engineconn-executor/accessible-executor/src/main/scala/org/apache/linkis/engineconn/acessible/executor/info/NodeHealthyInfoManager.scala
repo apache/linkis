434e14b75de08939e61fed2eb06352e27277172b
peacewong <peacewong@apache.org>
2024-06-07 18:45:20 +0800

    Fix engine exit hang bug

--- linkis-computation-governance/linkis-engineconn/linkis-engineconn-executor/accessible-executor/src/main/scala/org/apache/linkis/engineconn/acessible/executor/info/NodeHealthyInfoManager.scala
+++ linkis-computation-governance/linkis-engineconn/linkis-engineconn-executor/accessible-executor/src/main/scala/org/apache/linkis/engineconn/acessible/executor/info/NodeHealthyInfoManager.scala
@@ -20,7 +20,7 @@ package org.apache.linkis.engineconn.acessible.executor.info
 import org.apache.linkis.common.utils.Logging
 import org.apache.linkis.engineconn.acessible.executor.entity.AccessibleExecutor
 import org.apache.linkis.engineconn.core.executor.ExecutorManager
-import org.apache.linkis.manager.common.entity.enumeration.NodeStatus
+import org.apache.linkis.manager.common.entity.enumeration.{NodeHealthy, NodeStatus}
 import org.apache.linkis.manager.common.entity.metrics.NodeHealthyInfo
 
 import org.springframework.stereotype.Component
@@ -29,20 +29,45 @@ trait NodeHealthyInfoManager {
 
   def getNodeHealthyInfo(): NodeHealthyInfo
 
+  def setNodeHealthy(healthy: NodeHealthy): Unit
+
+  def getNodeHealthy(): NodeHealthy
+
+  def setByManager(setByManager: Boolean): Unit
+
 }
 
 @Component
 class DefaultNodeHealthyInfoManager extends NodeHealthyInfoManager with Logging {
 
+  private var healthy: NodeHealthy = NodeHealthy.Healthy
+
+  private var setByManager: Boolean = false
+
   override def getNodeHealthyInfo(): NodeHealthyInfo = {
     val nodeHealthyInfo = new NodeHealthyInfo
     nodeHealthyInfo.setMsg("")
-    nodeHealthyInfo.setNodeHealthy(
+
+    /** If it is actively set by the manager, then the manager setting shall prevail */
+    val newHealthy: NodeHealthy = if (this.setByManager) {
+      this.healthy
+    } else {
       NodeStatus.isEngineNodeHealthy(
         ExecutorManager.getInstance.getReportExecutor.asInstanceOf[AccessibleExecutor].getStatus
       )
-    )
+    }
+    logger.info("current node healthy status is {}", newHealthy)
+    nodeHealthyInfo.setNodeHealthy(newHealthy)
     nodeHealthyInfo
   }
 
+  override def setNodeHealthy(healthy: NodeHealthy): Unit = {
+    this.healthy = healthy
+  }
+
+  override def setByManager(setByManager: Boolean): Unit = {
+    this.setByManager = setByManager
+  }
+
+  override def getNodeHealthy(): NodeHealthy = this.healthy
 }

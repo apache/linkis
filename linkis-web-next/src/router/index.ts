942de4751e9f070d2f04944f4b7a86f379613333
Linhao-Qian <17768709606@163.com>
2024-10-16 02:22:56 -0700

    [linkis-web-next]: Modules refactoring & Bug fixing (#5169)
    * GlobalHistoryManagement and SideBar modules refactoring & previous bug fixing
    
    * [Linkis-web-next]: parameterConfig Module Refactor
    
    * [Linkis-web-next]: Add code comments to ParameterConfig Module
    
    * [linkis-web-next]: Refactored Navbar & Login Page
    
    * [linkis-web-next]: 1. TaskResults & TaskDetails Refactoring; 2. Code Improvement & Bug Fixing;
    
    * [linkis-web-next]: Refactored GlobalVariables Module
    
    * [linkis-web-next]: Add license to newly created file
    
    * [linkis-web-next]: Microservice Management Module Refactoring
    
    * [linkis-web-next]: Resource Management Module Refactoring
    
    * [linkis-web-next]: ECM Management Module Refactoring
    
    * [linkis-web-next]: Base Data Management & Data Source Management & UDF Function Management Modules Refactoring
    
    * [linkis-web-next]: remove duplicate codes
    
    * [linkis-web-next]: Modify README files

--- linkis-web-next/src/router/index.ts
+++ linkis-web-next/src/router/index.ts
@@ -130,6 +130,199 @@ const routes: Array<RouteRecordRaw> = [
                         },
                         children: [],
                     },
+                    {
+                        name: 'dataSourceManage',
+                        path: '/console/dataSourceManage',
+                        redirect: '/console/dataSourceManage/dataSourceManagement',
+                        meta: {
+                            title: 'Data Source Manage',
+                            publicPage: true,
+                        },
+                        children: [
+                            {
+                                name: 'dataSourceManagement',
+                                path: '/console/dataSourceManage/dataSourceManagement',
+                                component: () =>
+                                    import('@/pages/dataSourceManage/dataSourceManagement/index.vue'),
+                                meta: {
+                                    title: 'Data Source Management',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'datasourceEnv',
+                                path: '/console/dataSourceManage/datasourceEnv',
+                                component: () =>
+                                    import('@/pages/dataSourceManage/datasourceEnv/index.vue'),
+                                meta: {
+                                    title: 'Data Source Env',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'datasourceType',
+                                path: '/console/dataSourceManage/datasourceType',
+                                component: () =>
+                                    import('@/pages/dataSourceManage/datasourceType/index.vue'),
+                                meta: {
+                                    title: 'Data Source Type',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'datasourceAccess',
+                                path: '/console/dataSourceManage/datasourceAccess',
+                                component: () =>
+                                    import('@/pages/dataSourceManage/datasourceAccess/index.vue'),
+                                meta: {
+                                    title: 'Data Source Access',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'datasourceTypeKey',
+                                path: '/console/dataSourceManage/datasourceTypeKey',
+                                component: () =>
+                                    import('@/pages/dataSourceManage/datasourceTypeKey/index.vue'),
+                                meta: {
+                                    title: 'Data Source Type Key',
+                                    publicPage: true,
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        name: 'udfFunctionTitle',
+                        path: '/console/udfFunctionTitle',
+                        redirect: '/console/udfFunctionTitle/udfFunctionManage',
+                        meta: {
+                            title: 'UDF Function',
+                            publicPage: true,
+                        },
+                        children: [
+                            {
+                                name: 'udfFunctionManage',
+                                path: '/console/udfFunctionTitle/udfFunctionManage',
+                                component: () =>
+                                    import('@/pages/udfFunctionTitle/udfFunctionManage/index.vue'),
+                                meta: {
+                                    title: 'UDF Management',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'functionManagement',
+                                path: '/console/udfFunctionTitle/functionManagement',
+                                component: () =>
+                                    import('@/pages/udfFunctionTitle/functionManagement/index.vue'),
+                                meta: {
+                                    title: 'Function Management',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'udfManager',
+                                path: '/console/udfFunctionTitle/udfManager',
+                                component: () =>
+                                    import('@/pages/udfFunctionTitle/udfManager/index.vue'),
+                                meta: {
+                                    title: 'UDF Manager',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'udfTree',
+                                path: '/console/udfFunctionTitle/udfTree',
+                                component: () =>
+                                    import('@/pages/udfFunctionTitle/udfTree/index.vue'),
+                                meta: {
+                                    title: 'UDF Tree',
+                                    publicPage: true,
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        name: 'basedataManagement',
+                        path: '/console/basedataManagement',
+                        redirect: '/console/basedataManagement/gatewayAuthToken',
+                        meta: {
+                            title: 'Base Data Management',
+                            publicPage: true,
+                        },
+                        children: [
+                            {
+                                name: 'gatewayAuthToken',
+                                path: '/console/basedataManagement/gatewayAuthToken',
+                                component: () =>
+                                    import('@/pages/basedataManagement/gatewayAuthToken/index.vue'),
+                                meta: {
+                                    title: 'Gateway Auth Token',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'errorCode',
+                                path: '/console/basedataManagement/errorCode',
+                                component: () =>
+                                    import('@/pages/basedataManagement/errorCode/index.vue'),
+                                meta: {
+                                    title: 'Error Code',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'rmExternalResourceProvider',
+                                path: '/console/basedataManagement/rmExternalResourceProvider',
+                                component: () =>
+                                    import('@/pages/basedataManagement/rmExternalResourceProvider/index.vue'),
+                                meta: {
+                                    title: 'Rm External Resource Provider',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'EnginePluginManagement',
+                                path: '/console/basedataManagement/EnginePluginManagement',
+                                component: () =>
+                                    import('@/pages/basedataManagement/EnginePluginManagement/index.vue'),
+                                meta: {
+                                    title: 'Engine Plugin Management',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'tenantTagManagement',
+                                path: '/console/basedataManagement/tenantTagManagement',
+                                component: () =>
+                                    import('@/pages/basedataManagement/tenantTagManagement/index.vue'),
+                                meta: {
+                                    title: 'Tenant Tag Management',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'ipListManagement',
+                                path: '/console/basedataManagement/ipListManagement',
+                                component: () =>
+                                    import('@/pages/basedataManagement/ipListManagement/index.vue'),
+                                meta: {
+                                    title: 'Ip List Management',
+                                    publicPage: true,
+                                },
+                            },
+                            {
+                                name: 'configManagement',
+                                path: '/console/basedataManagement/configManagement',
+                                component: () =>
+                                    import('@/pages/basedataManagement/configManagement/index.vue'),
+                                meta: {
+                                    title: 'Config Management',
+                                    publicPage: true,
+                                },
+                            },
+                        ],
+                    },
                 ]
             },
         ],

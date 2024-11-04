18613cefb3867cea8f05bb01e97603c577297eab
aiceflower <kinsanities@sina.com>
2024-11-04 19:08:56 +0800

    code optimization

--- linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/extension/SparkUDFCheckRule.scala
+++ linkis-engineconn-plugins/spark/src/main/scala/org/apache/linkis/engineplugin/spark/extension/SparkUDFCheckRule.scala
@@ -18,11 +18,11 @@
 package org.apache.linkis.engineplugin.spark.extension
 
 import org.apache.linkis.engineconn.computation.executor.conf.ComputationExecutorConf
-import org.apache.linkis.server.BDPJettyServerHelper
+import org.apache.linkis.engineplugin.spark.errorcode.SparkErrorCodeSummary
+import org.apache.linkis.engineplugin.spark.exception.RuleCheckFailedException
 
 import org.apache.commons.lang3.StringUtils
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.rules.Rule
 
@@ -50,7 +50,6 @@ case class SparkUDFCheckRule(sparkSession: SparkSession) extends Rule[LogicalPla
   }
 
   override def apply(plan: LogicalPlan): LogicalPlan = {
-    logger.info(plan.toString())
     // 从系统属性中获取代码类型
     val codeType: String = System.getProperty(ComputationExecutorConf.CODE_TYPE, "sql")
     logger.info("SparkUDFCheckRule codeType: {}", codeType)
@@ -69,9 +68,9 @@ case class SparkUDFCheckRule(sparkSession: SparkSession) extends Rule[LogicalPla
           node.collect {
             case e: LogicalPlan if containsSpecificFunction(e, udfName) =>
               logger.info("contains specific functionName: {}", e.toString())
-              // 如果找到包含特定udf函数的表达式，则抛出异常中断
-              throw new RuntimeException(
-                s"Found expression containing specific functionName in non-SQL code type, terminating optimization."
+              throw new RuleCheckFailedException(
+                SparkErrorCodeSummary.NOT_SUPPORT_FUNCTION.getErrorCode,
+                SparkErrorCodeSummary.NOT_SUPPORT_FUNCTION.getErrorDesc
               )
             case _ =>
               logger.info(
@@ -81,7 +80,9 @@ case class SparkUDFCheckRule(sparkSession: SparkSession) extends Rule[LogicalPla
           }
         }
       } catch {
-        case e: RuntimeException =>
+        case e: RuleCheckFailedException =>
+          throw e
+        case e: Exception =>
           logger.info("check udf function error: {}", e.getMessage)
       }
       plan

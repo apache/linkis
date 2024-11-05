0274e42e3a9693931c033e8af707d90e9520128f
aiceflower <kinsanities@sina.com>
2024-11-05 16:45:36 +0800

    optimization engine quitted unexpectedly error msg

--- linkis-orchestrator/linkis-computation-orchestrator/src/main/scala/org/apache/linkis/orchestrator/computation/monitor/EngineConnMonitor.scala
+++ linkis-orchestrator/linkis-computation-orchestrator/src/main/scala/org/apache/linkis/orchestrator/computation/monitor/EngineConnMonitor.scala
@@ -22,26 +22,20 @@ import org.apache.linkis.common.log.LogUtils
 import org.apache.linkis.common.utils.{Logging, Utils}
 import org.apache.linkis.governance.common.conf.GovernanceCommonConf
 import org.apache.linkis.governance.common.entity.{ExecutionNodeStatus, NodeExistStatus}
-import org.apache.linkis.governance.common.protocol.engineconn.{
-  RequestEngineStatusBatch,
-  ResponseEngineStatusBatch
-}
+import org.apache.linkis.governance.common.protocol.engineconn.{RequestEngineStatusBatch, ResponseEngineStatusBatch}
 import org.apache.linkis.governance.common.utils.GovernanceConstant
 import org.apache.linkis.manager.common.entity.enumeration.NodeStatus
 import org.apache.linkis.manager.common.protocol.node.{RequestNodeStatus, ResponseNodeStatus}
+import org.apache.linkis.manager.label.entity.Label
+import org.apache.linkis.manager.label.utils.LabelUtil
 import org.apache.linkis.orchestrator.computation.conf.ComputationOrchestratorConf
 import org.apache.linkis.orchestrator.computation.execute.{CodeExecTaskExecutor, EngineConnTaskInfo}
-import org.apache.linkis.orchestrator.listener.task.{
-  TaskErrorResponseEvent,
-  TaskLogEvent,
-  TaskStatusEvent
-}
+import org.apache.linkis.orchestrator.listener.task.{TaskErrorResponseEvent, TaskLogEvent, TaskStatusEvent}
 import org.apache.linkis.rpc.Sender
-import org.apache.linkis.server.{toJavaMap, BDPJettyServerHelper}
+import org.apache.linkis.server.{BDPJettyServerHelper, toJavaMap}
 
 import java.util
 import java.util.concurrent.TimeUnit
-
 import scala.collection.JavaConverters._
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
@@ -200,11 +194,13 @@ object EngineConnMonitor extends Logging {
     executors.foreach { executor =>
       val execTask = executor.getExecTask
       Utils.tryAndError {
+        val labels: Array[Label[_]] = executor.getEngineConnExecutor.getLabels()
+        val engineType: String = LabelUtil.getEngineTypeLabel(labels.toList.asJava).getEngineType
         logger.warn(
           s"Will kill task ${execTask.getIDInfo()} because the engine ${executor.getEngineConnExecutor.getServiceInstance.toString} quited unexpectedly."
         )
         val errLog = LogUtils.generateERROR(
-          s"Your job : ${execTask.getIDInfo()} was failed because the engine quitted unexpectedly(任务${execTask
+          s"Your job : ${execTask.getIDInfo()} was failed because the ${engineType} engine quitted unexpectedly(任务${execTask
             .getIDInfo()}失败，" +
             s"原因是引擎意外退出,可能是复杂任务导致引擎退出，如OOM)."
         )

55e87820527e1f3d5775b1a655ff36f8a8dd5184
aiceflower <kinsanities@sina.com>
2025-01-13 15:26:16 +0800

    fix queuename contains root.

--- linkis-computation-governance/linkis-manager/linkis-manager-common/src/main/java/org/apache/linkis/manager/common/entity/resource/LoadInstanceResource.java
+++ linkis-computation-governance/linkis-manager/linkis-manager-common/src/main/java/org/apache/linkis/manager/common/entity/resource/LoadInstanceResource.java
@@ -192,10 +192,10 @@ public class LoadInstanceResource extends Resource {
   }
 
   public int getInstances() {
-    return instances;
+    return this.instances;
   }
 
-  public int getInstance() {
-    return instances;
+  public int getInstance() {// NOSONAR
+    return this.instances; // NOSONAR
   }
 }

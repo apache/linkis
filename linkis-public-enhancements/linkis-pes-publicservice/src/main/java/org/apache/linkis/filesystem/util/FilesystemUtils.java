50860188e95882b7c1da90a29b3a8e45df45bab1
v-kkhuang <62878639+v-kkhuang@users.noreply.github.com>
2025-02-08 10:30:33 +0800

    Dev 1.10.0 check hdfs file (#721)
    * 添加HDFS文件检查接口并优化相关功能
    
    * 扩展文件路径正则表达式以支持更多字符
    
    * 1.优化线程池使用，提升资源利用和性能
    
    ---------
    
    Co-authored-by: “v_kkhuang” <“420895376@qq.com”>

--- linkis-public-enhancements/linkis-pes-publicservice/src/main/java/org/apache/linkis/filesystem/util/FilesystemUtils.java
+++ linkis-public-enhancements/linkis-pes-publicservice/src/main/java/org/apache/linkis/filesystem/util/FilesystemUtils.java
@@ -22,10 +22,20 @@ import org.apache.linkis.storage.fs.FileSystem;
 
 import org.apache.commons.lang3.StringUtils;
 
+import javax.annotation.PreDestroy;
+
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.linkis.filesystem.conf.WorkSpaceConfiguration.executorService;
 
 public class FilesystemUtils {
+
+  public static final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
   public static boolean checkFilePermissions(String filePermission) {
     boolean result = false;
     if (StringUtils.isNumeric(filePermission)) {
@@ -49,6 +59,20 @@ public class FilesystemUtils {
         traverseFolder(path, fileSystem, dirsToChmod);
       }
       dirsToChmod.push(path);
+    }
+  }
+
+  @PreDestroy
+  public void shutdown() {
+    // 在应用程序关闭时优雅地关闭线程池
+    executorService.shutdown();
+    try {
+      if (!executorService.awaitTermination(60L, TimeUnit.SECONDS)) {
+        executorService.shutdownNow();
+      }
+    } catch (InterruptedException e) {
+      executorService.shutdownNow();
+      Thread.currentThread().interrupt();
     }
   }
 }

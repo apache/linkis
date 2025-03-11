adeade1fa6011539b604910d9cd2156491535a72
mayinrain <624899618@qq.com>
2025-03-11 10:34:14 +0800

    fix: 调整执行阶段

--- linkis-web/src/apps/linkis/module/globalHistoryManagement/viewHistory.vue
+++ linkis-web/src/apps/linkis/module/globalHistoryManagement/viewHistory.vue
@@ -164,7 +164,7 @@ export default {
       this.tabs = this.tabs.filter(tab => tab.name !== 'udfLog')
     }
   },
-  unmouted() {
+  destroyed() {
     if(this.logTimer) {
       clearTimeout(this.logTimer);
     }

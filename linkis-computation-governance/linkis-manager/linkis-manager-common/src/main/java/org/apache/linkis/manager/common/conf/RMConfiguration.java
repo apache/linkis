11b451f4dd2d8a405bbedba804b6901c1dbc52d6
aiceflower <kinsanities@sina.com>
2025-08-05 10:47:37 +0800

    add log

--- linkis-computation-governance/linkis-manager/linkis-manager-common/src/main/java/org/apache/linkis/manager/common/conf/RMConfiguration.java
+++ linkis-computation-governance/linkis-manager/linkis-manager-common/src/main/java/org/apache/linkis/manager/common/conf/RMConfiguration.java
@@ -24,53 +24,68 @@ import org.apache.linkis.common.conf.TimeType;
 public class RMConfiguration {
 
   public static final CommonVars<Long> RM_WAIT_EVENT_TIME_OUT =
-      CommonVars.apply("wds.linkis.rm.wait.event.time.out", 1000 * 60 * 12L);
+          CommonVars.apply("wds.linkis.rm.wait.event.time.out", 1000 * 60 * 12L);
 
   public static final CommonVars<TimeType> LOCK_RELEASE_TIMEOUT =
-      CommonVars.apply("wds.linkis.manager.rm.lock.release.timeout", new TimeType("5m"));
+          CommonVars.apply("wds.linkis.manager.rm.lock.release.timeout", new TimeType("10m"));
 
   public static final CommonVars<TimeType> LOCK_RELEASE_CHECK_INTERVAL =
-      CommonVars.apply("wds.linkis.manager.rm.lock.release.check.interval", new TimeType("5m"));
+          CommonVars.apply("wds.linkis.manager.rm.lock.release.check.interval", new TimeType("5m"));
+
+  public static final CommonVars<TimeType> LOCK_FAILED_LABEL_RESOURCE_DEAL_INTERVAL =
+          CommonVars.apply("wds.linkis.manager.rm.lock.failed.deal.interval", new TimeType("10s"));
 
   // Resource parameter(资源参数)
   public static final CommonVars<Integer> USER_AVAILABLE_CPU =
-      CommonVars.apply("wds.linkis.rm.client.core.max", 10);
+          CommonVars.apply("wds.linkis.rm.client.core.max", 10);
   public static final CommonVars<ByteType> USER_AVAILABLE_MEMORY =
-      CommonVars.apply("wds.linkis.rm.client.memory.max", new ByteType("20g"));
+          CommonVars.apply("wds.linkis.rm.client.memory.max", new ByteType("20g"));
   public static final CommonVars<Integer> USER_AVAILABLE_INSTANCE =
-      CommonVars.apply("wds.linkis.rm.instance", 10);
+          CommonVars.apply("wds.linkis.rm.instance", 10);
 
   public static final CommonVars<Integer> USER_AVAILABLE_YARN_INSTANCE_CPU =
-      CommonVars.apply("wds.linkis.rm.yarnqueue.cores.max", 150);
+          CommonVars.apply("wds.linkis.rm.yarnqueue.cores.max", 150);
 
   public static final CommonVars<ByteType> USER_AVAILABLE_YARN_INSTANCE_MEMORY =
-      CommonVars.apply("wds.linkis.rm.yarnqueue.memory.max", new ByteType("450g"));
+          CommonVars.apply("wds.linkis.rm.yarnqueue.memory.max", new ByteType("450g"));
 
   public static final CommonVars<Integer> USER_AVAILABLE_YARN_INSTANCE =
-      CommonVars.apply("wds.linkis.rm.yarnqueue.instance.max", 30);
+          CommonVars.apply("wds.linkis.rm.yarnqueue.instance.max", 30);
   public static final CommonVars<String> USER_AVAILABLE_YARN_QUEUE_NAME =
-      CommonVars.apply("wds.linkis.rm.yarnqueue", "default");
+          CommonVars.apply("wds.linkis.rm.yarnqueue", "default");
   public static final CommonVars<String> USER_AVAILABLE_CLUSTER_NAME =
-      CommonVars.apply("wds.linkis.rm.cluster", "default");
+          CommonVars.apply("wds.linkis.rm.cluster", "default");
 
   public static final CommonVars<Integer> USER_AVAILABLE_KUBERNETES_INSTANCE_CPU =
-      CommonVars.apply("wds.linkis.rm.kubernetes.cores.max", 150000);
+          CommonVars.apply("wds.linkis.rm.kubernetes.cores.max", 150000);
   public static final CommonVars<ByteType> USER_AVAILABLE_KUBERNETES_INSTANCE_MEMORY =
-      CommonVars.apply("wds.linkis.rm.kubernetes.memory.max", new ByteType("450g"));
+          CommonVars.apply("wds.linkis.rm.kubernetes.memory.max", new ByteType("450g"));
   public static final CommonVars<String> USER_AVAILABLE_KUBERNETES_INSTANCE_NAMESPACE =
-      CommonVars.apply("wds.linkis.rm.kubernetes.namespace", "default");
+          CommonVars.apply("wds.linkis.rm.kubernetes.namespace", "default");
 
   public static final CommonVars<Long> RM_ENGINE_SCAN_INTERVAL =
-      CommonVars.apply("wds.linkis.rm.engine.scan.interval", 120000L);
+          CommonVars.apply("wds.linkis.rm.engine.scan.interval", 120000L);
 
   public static final CommonVars<String> DEFAULT_YARN_CLUSTER_NAME =
-      CommonVars.apply("wds.linkis.rm.default.yarn.cluster.name", "default");
+          CommonVars.apply("wds.linkis.rm.default.yarn.cluster.name", "default");
   public static final CommonVars<String> DEFAULT_YARN_TYPE =
-      CommonVars.apply("wds.linkis.rm.default.yarn.cluster.type", "Yarn");
+          CommonVars.apply("wds.linkis.rm.default.yarn.cluster.type", "Yarn");
   public static final CommonVars<String> DEFAULT_KUBERNETES_CLUSTER_NAME =
-      CommonVars.apply("wds.linkis.rm.default.kubernetes.cluster.name", "default");
+          CommonVars.apply("wds.linkis.rm.default.kubernetes.cluster.name", "default");
   public static final CommonVars<String> DEFAULT_KUBERNETES_TYPE =
-      CommonVars.apply("wds.linkis.rm.default.kubernetes.cluster.type", "K8S");
+          CommonVars.apply("wds.linkis.rm.default.kubernetes.cluster.type", "K8S");
   public static final CommonVars<Integer> EXTERNAL_RETRY_NUM =
-      CommonVars.apply("wds.linkis.rm.external.retry.num", 3);
+          CommonVars.apply("wds.linkis.rm.external.retry.num", 3);
+
+  public static final CommonVars<Integer> LABEL_SERVICE_PARTITION_NUM =
+          CommonVars.apply("wds.linkis.label.service.partition.num", 1000);
+
+  public static final CommonVars<Integer> LABEL_SERVICE_INSTANCE_SHUFF_NUM =
+          CommonVars.apply("wds.linkis.label.service.instance.shuff.num", 100);
+
+  public static final CommonVars<Boolean> LABEL_SERVICE_INSTANCE_SHUFF_SWITCH =
+          CommonVars.apply("wds.linkis.label.service.instance.shuff.switch", false);
+
+  public static final CommonVars<Boolean> GET_RESOURCE_BY_LABEL_VALUE_ENABLED =
+          CommonVars.apply("wds.linkis.get.resource.by.label.value.enable", false);
 }
--- linkis-computation-governance/linkis-manager/linkis-manager-persistence/src/main/java/org/apache/linkis/manager/persistence/impl/DefaultLabelManagerPersistence.java
+++ linkis-computation-governance/linkis-manager/linkis-manager-persistence/src/main/java/org/apache/linkis/manager/persistence/impl/DefaultLabelManagerPersistence.java
@@ -18,6 +18,7 @@
 package org.apache.linkis.manager.persistence.impl;
 
 import org.apache.linkis.common.ServiceInstance;
+import org.apache.linkis.manager.common.conf.RMConfiguration;
 import org.apache.linkis.manager.common.entity.persistence.PersistenceLabel;
 import org.apache.linkis.manager.common.entity.persistence.PersistenceLabelRel;
 import org.apache.linkis.manager.common.entity.persistence.PersistenceNode;
@@ -42,6 +43,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -73,7 +75,7 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
 
   @Override
   public List<PersistenceLabelRel> getLabelByPattern(
-      String labelValuePattern, String labelKey, Integer page, Integer size) {
+          String labelValuePattern, String labelKey, Integer page, Integer size) {
     return labelManagerMapper.listLabelBySQLPattern(labelValuePattern, labelKey);
   }
 
@@ -102,7 +104,7 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
     int labelId = persistenceLabel.getId();
     if (labelId <= 0) {
       PersistenceLabel labelByKeyValue =
-          labelManagerMapper.getLabelByKeyValue(labelKey, labelStringValue);
+              labelManagerMapper.getLabelByKeyValue(labelKey, labelStringValue);
       if (null == labelByKeyValue) {
         logger.warn("Can not find label labelKey {}, label value {}", labelKey, labelStringValue);
         return;
@@ -132,16 +134,16 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
         Map.Entry<String, String> labelKeyValue = iterator.next();
         if (StringUtils.isNotBlank(labelKeyValue.getKey())) {
           labelManagerMapper.replaceIntoLabelKeyValue(
-              labelKeyValue.getKey(), labelKeyValue.getValue(), labelId);
+                  labelKeyValue.getKey(), labelKeyValue.getValue(), labelId);
         }
       }
     }
   }
 
   @Retryable(
-      value = {CannotGetJdbcConnectionException.class},
-      maxAttempts = 5,
-      backoff = @Backoff(delay = 10000))
+          value = {CannotGetJdbcConnectionException.class},
+          maxAttempts = 5,
+          backoff = @Backoff(delay = 10000))
   @Override
   public PersistenceLabel getLabel(int id) {
     PersistenceLabel persistenceLabel = labelManagerMapper.getLabel(id);
@@ -154,7 +156,7 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
   @Override
   public List<PersistenceLabel> getLabelByServiceInstance(ServiceInstance serviceInstance) {
     List<PersistenceLabel> persistenceLabelList =
-        labelManagerMapper.getLabelByServiceInstance(serviceInstance.getInstance());
+            labelManagerMapper.getLabelByServiceInstance(serviceInstance.getInstance());
     persistenceLabelList.forEach(PersistenceUtils::setValue);
     return persistenceLabelList;
   }
@@ -162,7 +164,7 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
   @Override
   public List<PersistenceLabel> getLabelByResource(PersistenceResource persistenceResource) {
     List<PersistenceLabel> persistenceLabelList =
-        labelManagerMapper.getLabelByResource(persistenceResource);
+            labelManagerMapper.getLabelByResource(persistenceResource);
     return persistenceLabelList;
   }
 
@@ -175,14 +177,14 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
 
   @Override
   public List<PersistenceLabel> getLabelsByValue(
-      Map<String, String> value, Label.ValueRelation valueRelation) {
+          Map<String, String> value, Label.ValueRelation valueRelation) {
     return getLabelsByValueList(Collections.singletonList(value), valueRelation);
   }
 
   @Override
   @Deprecated
   public List<PersistenceLabel> getLabelsByValueList(
-      List<Map<String, String>> valueList, Label.ValueRelation valueRelation) {
+          List<Map<String, String>> valueList, Label.ValueRelation valueRelation) {
     if (PersistenceUtils.valueListIsEmpty(valueList)) return Collections.emptyList();
     if (valueRelation == null) valueRelation = Label.ValueRelation.ALL;
     return null;
@@ -190,23 +192,23 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
 
   @Override
   public PersistenceLabel getLabelsByKeyValue(
-      String labelKey, Map<String, String> value, Label.ValueRelation valueRelation) {
+          String labelKey, Map<String, String> value, Label.ValueRelation valueRelation) {
     List<PersistenceLabel> labelsByValueList =
-        getLabelsByKeyValueMap(Collections.singletonMap(labelKey, value), valueRelation);
+            getLabelsByKeyValueMap(Collections.singletonMap(labelKey, value), valueRelation);
     return labelsByValueList.isEmpty() ? null : labelsByValueList.get(0);
   }
 
   @Override
   public List<PersistenceLabel> getLabelsByKeyValueMap(
-      Map<String, Map<String, String>> keyValueMap, Label.ValueRelation valueRelation) {
+          Map<String, Map<String, String>> keyValueMap, Label.ValueRelation valueRelation) {
     if (PersistenceUtils.KeyValueMapIsEmpty(keyValueMap)) return Collections.emptyList();
     if (valueRelation == null) valueRelation = Label.ValueRelation.ALL;
     return labelManagerMapper
-        .dimListLabelByKeyValueMap(
-            PersistenceUtils.filterEmptyKeyValueMap(keyValueMap), valueRelation.name())
-        .stream()
-        .map(PersistenceUtils::setValue)
-        .collect(Collectors.toList());
+            .dimListLabelByKeyValueMap(
+                    PersistenceUtils.filterEmptyKeyValueMap(keyValueMap), valueRelation.name())
+            .stream()
+            .map(PersistenceUtils::setValue)
+            .collect(Collectors.toList());
   }
 
   @Override
@@ -291,7 +293,7 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
 
   @Override
   public Map<PersistenceLabel, List<ServiceInstance>> getNodeRelationsByLabels(
-      List<PersistenceLabel> persistenceLabels) {
+          List<PersistenceLabel> persistenceLabels) {
     // 空集合过滤& 转换
     if (PersistenceUtils.persistenceLabelListIsEmpty(persistenceLabels))
       return Collections.emptyMap();
@@ -300,9 +302,9 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
       String dimType = Label.ValueRelation.ALL.name();
       List<Map<String, Object>> nodeRelationsByLabels = labelManagerMapper.dimListNodeRelationsByKeyValueMap(keyValueMap, dimType);*/
       List<Map<String, Object>> nodeRelationsByLabels =
-          labelManagerMapper.getNodeRelationsByLabels(persistenceLabels);
+              labelManagerMapper.getNodeRelationsByLabels(persistenceLabels);
       List<Tunple<PersistenceLabel, ServiceInstance>> arrays =
-          new ArrayList<Tunple<PersistenceLabel, ServiceInstance>>();
+              new ArrayList<Tunple<PersistenceLabel, ServiceInstance>>();
       for (Map<String, Object> nodeRelationsByLabel : nodeRelationsByLabels) {
         ServiceInstance serviceInstance = new ServiceInstance();
         PersistenceLabel persistenceLabel = new PersistenceLabel();
@@ -312,55 +314,57 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
         arrays.add(new Tunple(persistenceLabel, serviceInstance));
       }
       return arrays.stream()
-          .collect(Collectors.groupingBy(Tunple::getKey))
-          .entrySet()
-          .stream()
-          .collect(
-              Collectors.toMap(
-                  Map.Entry::getKey,
-                  f -> f.getValue().stream().map(Tunple::getValue).collect(Collectors.toList())));
+              .collect(Collectors.groupingBy(Tunple::getKey))
+              .entrySet()
+              .stream()
+              .collect(
+                      Collectors.toMap(
+                              Map.Entry::getKey,
+                              f -> f.getValue().stream().map(Tunple::getValue).collect(Collectors.toList())));
     } catch (InvocationTargetException | IllegalAccessException e) {
       throw new PersistenceWarnException(
-          BEANUTILS_POPULATE_FAILED.getErrorCode(), BEANUTILS_POPULATE_FAILED.getErrorDesc(), e);
+              BEANUTILS_POPULATE_FAILED.getErrorCode(), BEANUTILS_POPULATE_FAILED.getErrorDesc(), e);
     }
   }
 
   @Override
   public Map<ServiceInstance, List<PersistenceLabel>> getLabelRelationsByServiceInstance(
-      List<ServiceInstance> serviceInstances) {
+          List<ServiceInstance> serviceInstances) {
     if (CollectionUtils.isEmpty(serviceInstances)) return Collections.emptyMap();
-    try {
-      List<Map<String, Object>> nodeRelationsByLabels =
-          labelManagerMapper.listLabelRelationByServiceInstance(serviceInstances);
-      List<Tunple<ServiceInstance, PersistenceLabel>> arrays =
-          new ArrayList<Tunple<ServiceInstance, PersistenceLabel>>();
-      for (Map<String, Object> nodeRelationsByLabel : nodeRelationsByLabels) {
-        Optional<ServiceInstance> instanceOption =
-            serviceInstances.stream()
-                .filter(
-                    serviceInstance1 ->
-                        serviceInstance1
-                            .getInstance()
-                            .equalsIgnoreCase(String.valueOf(nodeRelationsByLabel.get("instance"))))
-                .findFirst();
-        PersistenceLabel persistenceLabel = new PersistenceLabel();
-        BeanUtils.populate(persistenceLabel, nodeRelationsByLabel);
-        PersistenceUtils.setValue(persistenceLabel);
-        instanceOption.ifPresent(
-            serviceInstance -> arrays.add(new Tunple(serviceInstance, persistenceLabel)));
-      }
-      return arrays.stream()
-          .collect(Collectors.groupingBy(Tunple::getKey))
-          .entrySet()
-          .stream()
-          .collect(
-              Collectors.toMap(
-                  Map.Entry::getKey,
-                  f -> f.getValue().stream().map(Tunple::getValue).collect(Collectors.toList())));
-    } catch (InvocationTargetException | IllegalAccessException e) {
-      throw new PersistenceWarnException(
-          BEANUTILS_POPULATE_FAILED.getErrorCode(), BEANUTILS_POPULATE_FAILED.getErrorDesc(), e);
-    }
+    Map<ServiceInstance, List<PersistenceLabel>> resultMap = new HashMap<>();
+    List<Map<String, Object>> nodeRelationsByLabels =
+            listLabelRelationByServiceInstance(
+                    serviceInstances, RMConfiguration.LABEL_SERVICE_PARTITION_NUM.getValue());
+    logger.info("list label relation end, with size: {}", nodeRelationsByLabels.size());
+    Map<String, List<Map<String, Object>>> groupByInstanceMap =
+            nodeRelationsByLabels.stream()
+                    .collect(
+                            Collectors.groupingBy(
+                                    nodeRelationsByLabel -> nodeRelationsByLabel.get("instance").toString()));
+    serviceInstances.stream()
+            .filter(serviceInstance -> groupByInstanceMap.containsKey(serviceInstance.getInstance()))
+            .forEach(
+                    serviceInstance -> {
+                      List<PersistenceLabel> persistenceLabelList = new ArrayList<>();
+                      groupByInstanceMap
+                              .get(serviceInstance.getInstance())
+                              .forEach(
+                                      map -> {
+                                        try {
+                                          PersistenceLabel persistenceLabel = new PersistenceLabel();
+                                          BeanUtils.populate(persistenceLabel, map);
+                                          PersistenceUtils.setValue(persistenceLabel);
+                                          persistenceLabelList.add(persistenceLabel);
+                                        } catch (IllegalAccessException | InvocationTargetException e) {
+                                          throw new PersistenceWarnException(
+                                                  BEANUTILS_POPULATE_FAILED.getErrorCode(),
+                                                  BEANUTILS_POPULATE_FAILED.getErrorDesc(),
+                                                  e);
+                                        }
+                                      });
+                      resultMap.put(serviceInstance, persistenceLabelList);
+                    });
+    return resultMap;
   }
 
   @Override
@@ -371,5 +375,14 @@ public class DefaultLabelManagerPersistence implements LabelManagerPersistence {
   @Override
   public List<ServiceInstance> getNodeByLabelKeyValue(String labelKey, String stringValue) {
     return labelManagerMapper.getNodeByLabelKeyValue(labelKey, stringValue);
+  }
+
+  public List<Map<String, Object>> listLabelRelationByServiceInstance(
+          List<ServiceInstance> nodes, int batchSize) {
+
+    return Lists.partition(nodes, batchSize).stream()
+            .map(batch -> labelManagerMapper.listLabelRelationByServiceInstance(batch))
+            .flatMap(List::stream)
+            .collect(Collectors.toList());
   }
 }
